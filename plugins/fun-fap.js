7e1778df37f2965626a728bc159b7d43675abdc5
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-07 01:52:04 -0400

    Update fun-fap.js

--- plugins/fun-fap.js
+++ plugins/fun-fap.js
@@ -8,7 +8,6 @@ let handler = async (m, { conn, usedPrefix }) => {
 
     let user = global.db.data.users[who];
     let name = conn.getName(who);
-    let name2 = conn.getName(m.sender);
     m.react('😛');
     let str = `${name2} le esta lamiendo el coño a`.trim();
     
