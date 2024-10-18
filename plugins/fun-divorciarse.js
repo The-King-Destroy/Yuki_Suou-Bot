b4db7bb647cc4fe6befc65055825895dba01f5bd
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-18 15:11:09 -0400

    Update fun-divorciarse.js

--- plugins/fun-divorciarse.js
+++ plugins/fun-divorciarse.js
@@ -1,47 +1,57 @@
-import fs from 'fs';
-import path from 'path';
+let divorceConfirmation = {};
 
-let handler = async (m, { conn, usedPrefix }) => {
-  if (!m.isGroup) throw 'Este comando solo funciona en grupos';
-
-  let who;
-  who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
+let divorceHandler = async (m, { conn, usedPrefix, isGroup }) => {
+  let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   if (!who) throw 'Etiqueta o menciona a alguien';
 
-  let user = global.db.data.users[who];
-  let name = conn.getName(who);
+  let name1 = conn.getName(who);
   let name2 = conn.getName(m.sender);
-  
 
-  if (global.db.data.users[m.sender].pareja !== who || global.db.data.users[who].pareja !== m.sender) {
-    throw 'No estás casado con esta persona';
-  }
+  if (divorceConfirmation[m.sender]) return m.reply('Ya tienes una solicitud de divorcio pendiente.');
 
-  let str = `${name2} y ${name} se han divorciado.`.trim();
+  let str = `@${m.sender.split('@')[0]} ha solicitado el divorcio de @${who.split('@')[0]}! Responde con "acepto" o "rechazo" en los próximos 5 minutos.`;
+  await conn.sendMessage(m.chat, { text: str, mentions: conn.parseMention(str) }, { quoted: m });
 
-  let imgs = [
-    'https://qu.ax/ChmG.mp4', 
-    'https://qu.ax/yUBa.mp4', 
-    'https://qu.ax/OpVX.mp4'
-  ];
-  let img = imgs[Math.floor(Math.random() * imgs.length)];
-  conn.sendMessage(m.chat, { 
-    video: { url: img }, 
-    gifPlayback: true, 
-    caption: str, 
-    mentions: [m.sender] 
-  }, { quoted: m });
-
-  // Actualiza estado casado
-  global.db.data.users[m.sender].casado = false;
-  global.db.data.users[who].casado = false;
-  global.db.data.users[m.sender].pareja = null;
-  global.db.data.users[who].pareja = null;
+  divorceConfirmation[who] = {
+    sender: m.sender,
+    to: who,
+    message: m,
+    timeout: setTimeout(() => {
+      m.reply(`⏳ Tiempo de respuesta agotado.`);
+      delete divorceConfirmation[m.sender];
+    }, 5 * 60 * 1000) // 5 minutos
+  };
 };
 
-handler.help = ['divorciarse @tag'];
-handler.tags = ['fun'];
-handler.command = ['divorciarse', 'divorce'];
-handler.group = true;
+divorceHandler.before = async (m) => {
+  if (!(m.sender in divorceConfirmation)) return;
+  if (!m.text) return;
 
-export default handler;
+  let { timeout, sender, message, to } = divorceConfirmation[m.sender];
+  if (m.id === message.id) return;
+
+  if (/rechazo|no/i.test(m.text)) {
+    clearTimeout(timeout);
+    delete divorceConfirmation[sender];
+    return m.reply(`❌ La solicitud de divorcio fue rechazada por @${to.split('@')[0]}.`);
+  }
+
+  if (/acepto|si/i.test(m.text)) {
+    let finalStr = `@${sender.split('@')[0]} se ha divorciado de @${to.split('@')[0]}.`;
+    await conn.sendMessage(m.chat, { text: finalStr, mentions: conn.parseMention(finalStr) }, { quoted: message });
+
+    global.db.data.users[sender].casado = false;
+    global.db.data.users[to].casado = false;
+    global.db.data.users[sender].pareja = null;
+    global.db.data.users[to].pareja = null;
+
+    clearTimeout(timeout);
+    delete divorceConfirmation[sender];
+  }
+};
+
+divorceHandler.help = ['divorciarse @tag'];
+divorceHandler.tags = ['fun'];
+divorceHandler.command = ['divorciarse', 'divorce'];
+divorceHandler.group = true;
+export default divorceHandler;
