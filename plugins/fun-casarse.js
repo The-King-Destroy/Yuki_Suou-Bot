09551bcf1db8af2a3ac5dca993a376cf7c0c130e
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-18 14:42:21 -0400

    Update fun-casarse.js

--- plugins/fun-casarse.js
+++ plugins/fun-casarse.js
@@ -2,32 +2,36 @@ import fs from 'fs';
 import path from 'path';
 
 let handler = async (m, { conn, usedPrefix }) => {
+  // Verifica si el comando se está ejecutando en un grupo
   if (!m.isGroup) throw 'Este comando solo funciona en grupos';
 
-  let who;
-  who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
-  if (!who) throw 'Etiqueta o menciona a alguien';
+  let who = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : false);
+  if (!who) throw 'Por favor, etiqueta o menciona a alguien para casarte.';
 
   let user = global.db.data.users[who];
   let name = conn.getName(who);
   let name2 = conn.getName(m.sender);
 
-  let str = `${name2} se ha casado con ${name}! Felicidades!`.trim();
+  // Mensaje de felicitación
+  let str = `${name2} se ha casado con ${name}! ¡Felicidades! 🎉`.trim();
 
+  // Array de videos para elegir aleatoriamente
   let imgs = [
     'https://qu.ax/OpVX.mp4', 
     'https://qu.ax/ChmG.mp4', 
     'https://qu.ax/yUBa.mp4'
   ];
   let img = imgs[Math.floor(Math.random() * imgs.length)];
+
+  // Enviar mensaje de video con felicitaciones
   conn.sendMessage(m.chat, { 
     video: { url: img }, 
     gifPlayback: true, 
     caption: str, 
-    mentions: [m.sender] 
+    mentions: [m.sender, who] // Menciona a ambos en el mensaje
   }, { quoted: m });
-  
-  // Actualiza estado casado
+
+  // Actualiza el estado de casado en la base de datos
   global.db.data.users[m.sender].casado = true;
   global.db.data.users[who].casado = true;
   global.db.data.users[m.sender].pareja = who;
