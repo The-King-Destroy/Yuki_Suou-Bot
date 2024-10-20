df82b33f08f7cf85d9cad2ef0416f54feda84583
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-20 03:52:40 -0400

    Add files via upload

---
+++ plugins/economy-bank.js
@@ -0,0 +1,25 @@
+import db from './lib/database.js';
+
+let img = 'https://files.catbox.moe/xs5a6e.jpg';
+let handler = async (m, { conn, usedPrefix }) => {
+    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender;
+    if (who == conn.user.jid) return m.react('✖️');
+    if (!(who in global.db.data.users)) return m.reply(`*El usuario no se encuentra en mi base de datos*`);
+
+    let user = global.db.data.users[who];
+    const texto = `${who == m.sender 
+        ? `Tienes *${user.bank} Cookies 🍪*, *${user.coin} YukiCoins 🪙* y *${user.diamonds} Diamantes 💎* en el Banco` 
+        : `El usuario @${who.split('@')[0]} tiene *${user.bank} Cookies 🍪*, *${user.coin} YukiCoins 🪙* y *${user.diamonds} Diamantes 💎* en el Banco`}`;
+
+    await conn.sendButton(m.chat, texto, wm, img, 
+        [['Retirar Todo', `${usedPrefix}retirar all`], ['Meter Al Banco Todo', `${usedPrefix}dep all}`]], 
+        rcanal, null, { mentions: [who] });
+}
+
+handler.help = ['bank'];
+handler.tags = ['economy'];
+handler.command = ['bank', 'banco']; 
+handler.register = true; 
+handler.group = true;
+
+export default handler;
