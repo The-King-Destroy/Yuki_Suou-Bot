4cdcc9c824c0a7542a62a4a047e2543032c9966b
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-20 04:06:20 -0400

    Update economy-bank.js

--- plugins/economy-bank.js
+++ plugins/economy-bank.js
@@ -1,18 +1,38 @@
-import db from '../lib/database.js'
-let img 'https://qu.ax/EKcDO.jpg'
-let handler = async (m, {conn, usedPrefix}) => {
-   let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-   if (who == conn.user.jid) return m.react('✖️')
-   if (!(who in global.db.data.users)) return m.reply(`*El usuario no se encuentra en mi base de datos*`)
-   let user = global.db.data.users[who]
-   const texto = `${who == m.sender ? `Tienes *${user.bank} Cookies 🍪* en el Banco` : `El usuario @${who.split('@')[0]} tiene *${user.bank} Cookies 🍪* en el Banco`}`
+import db from '../lib/database.js';
 
-   await conn.sendButton(m.chat, texto, wm, img, [['Retirar Todo', `${usedPrefix}retirar all`], ['Meter Al Banco Todo', `${usedPrefix}dep all`] ], rcanal, null, { mentions: [who] })
+let img = 'https://qu.ax/EKcDO.jpg';
+let handler = async (m, { conn, usedPrefix }) => {
+    try {
+        // Determinar el usuario objetivo
+        let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender;
+        if (who == conn.user.jid) return m.react('✖️');
+
+        // Verificar si el usuario existe en la base de datos
+        if (!(who in global.db.data.users)) {
+            return m.reply(`*El usuario no se encuentra en la base de datos.*`);
+        }
+
+        let user = global.db.data.users[who];
+
+        // Formatear el mensaje con información de cookies, YukiCoins y diamantes
+        const texto = `${who == m.sender 
+            ? `Tienes *${user.bank} Cookies 🍪*, *${user.coin} YukiCoins 🪙* y *${user.diamonds} Diamantes 💎* en el Banco.` 
+            : `El usuario @${who.split('@')[0]} tiene *${user.bank} Cookies 🍪*, *${user.coin} YukiCoins 🪙* y *${user.diamonds} Diamantes 💎* en el Banco.`}`;
+
+        // Enviar el mensaje con botones
+        await conn.sendButton(m.chat, texto, wm, img, 
+            [['Retirar Todo', `${usedPrefix}retirar all`], ['Depositar Todo', `${usedPrefix}dep all`]], 
+            null, null, { mentions: [who] });
+    } catch (error) {
+        console.error(error);
+        m.reply(`*Ocurrió un error inesperado. Por favor, intenta nuevamente más tarde.*`);
+    }
 }
 
-handler.help = ['bank']
-handler.tags = ['economy']
-handler.command = ['bank', 'banco'] 
-handler.register = true 
-handler.group = true
-export default handler 
+handler.help = ['bank'];
+handler.tags = ['economy'];
+handler.command = ['bank', 'banco'];
+handler.register = true;
+handler.group = true;
+
+export default handler;
