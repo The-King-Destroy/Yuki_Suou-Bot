31ec0e19aa8080505b67b43be845013a2531e304
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-20 16:41:17 -0400

    Add files via upload

--- plugins/ai-openai.js
+++ plugins/ai-openai.js
@@ -1,54 +1,48 @@
-import fetch from 'node-fetch'
-import axios from 'axios'
-import translate from '@vitalets/google-translate-api'
-import {Configuration, OpenAIApi} from 'openai'
-
-const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key})
-const openaiii = new OpenAIApi(configuration)
-
-var handler = async (m, {conn, text, usedPrefix, command}) => {
+import Groq from 'groq-sdk';
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+const groq = new Groq({ apiKey: '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' });
+    conn.sylph = conn.sylph ? conn.sylph : {};
+    let ya = text && m.quoted ? (m.quoted.text ? text + '\n\n' + m.quoted.text : text) : text ? text : (m.quoted ? (m.quoted.text ? m.quoted.text : false) : false);
+    if (!ya) throw `\`\`\`[ ✨ ] Por favor ingresa un texto. Ejemplo: ${usedPrefix + command} Hola\`\`\``
+      try {
+        let { key } = await conn.sendMessage(m.chat, { text: wait }, { quoted: m });
+        if (!(m.sender in conn.sylph))
+        conn.sylph[m.sender] = [{
+          role: 'system',
+          content: `Eres Yuki Suou, una inteligencia artificial creada por I'am Destroy, responde de manera clara y concisa para que los usuarios entiendan mejor tus respuestas. El nombre del usuario será: ${conn.getName(m.sender)}`,
+        }];
   
-if (usedPrefix == 'a' || usedPrefix == 'A') return
-if (!text) return conn.reply(m.chat, `🎌 *Ingrese una petición*\n\nEjemplo, !${command} Codigo en JS para un juego de cartas`, m, fake, ) 
-  
-try {
+        if (conn.sylph[m.sender].length > 10) {
+          conn.sylph[m.sender] = conn.sylph[m.sender].slice(-1);
+        }
 
-conn.sendPresenceUpdate('composing', m.chat)
-let sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por Destroy, tu seras Yuki_Suou-Bot`
-async function getOpenAIChatCompletion(texto) {
-const openaiAPIKey = global.openai_key
-let chgptdb = global.chatgpt.data.users[m.sender]
-chgptdb.push({ role: 'user', content: texto })
-let url = 'https://api.openai.com/v1/chat/completions'
-let headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${openaiAPIKey}` }
-let data = { 'model': 'gpt-3.5-turbo', 'messages': [{ 'role': 'system', 'content': sistema1 }, ...chgptdb, ]}
-let response = await fetch(url, {method: 'POST', headers: headers, body: JSON.stringify(data)})
-let result = await response.json()
-let finalResponse = result.choices[0].message.content
-return finalResponse
-}
-let respuesta = await getOpenAIChatCompletion(text)
-if (respuesta == 'error' || respuesta == '' || !respuesta) return XD // causar error undefined para usar otra api
-conn.reply(m.chat, `${respuesta}`, m, fake, )
-} catch {
-try {
-let botIA222 = await openaiii.createCompletion({model: 'text-davinci-003', prompt: text, temperature: 0.3, max_tokens: 4097, stop: ['Ai:', 'Human:'], top_p: 1, frequency_penalty: 0.2, presence_penalty: 0})
-if (botIA222.data.choices[0].text == 'error' || botIA222.data.choices[0].text == '' || !botIA222.data.choices[0].text) return XD // causar error undefined para usar otra api
-conn.reply(m.chat, botIA222.data.choices[0].text, m, fake, )
-} catch {
-try {
-let syms1 = `Actuaras como un Bot de WhatsApp el cual fue creado por Destroy, tu seras Yuki_Suou-Bot`
-let Empireapi1 = await fetch(`https://api.cafirexos.com/api/chatgpt?text=${text}&name=${m.name}&prompt=${syms1}`)
-let empireApijson1 = await Empireapi1.json();
-if (empireApijson1.resultado == 'error' || empireApijson1.resultado == '' || !empireApijson1.resultado) return XD // causar error undefined para lanzar msg de error
-conn.reply(m.chat, `${empireApijson1.resultado}`, m, fake, )
-} catch {
-}}}
+        conn.sylph[m.sender].push({
+          role: 'user',
+          content: ya,
+        });
 
-}
-handler.help = ['openia', 'chatgpt', 'ia']
-handler.tags = ['ai']
-handler.command = /^(openai|chatgpt|ia)$/i
+        let msg = [ ...conn.sylph[m.sender], {
+          role: 'user',
+          content: ya,
+        }];
 
-export default handler
+        const payloads = {
+          messages: msg,
+          model: 'llama-3.1-70b-versatile'
+        };
 
+        const json = await groq.chat.completions.create(payloads)
+        let message = json.choices[0].message.content;
+        conn.sylph[m.sender].push({
+          role: "system",
+          content: message,
+        });
+        await conn.sendMessage(m.chat, { text: message, edit: key }, { quoted: m });
+      } catch (e) {
+        return m.reply(e.message)
+      }
+  }
+handler.command = ['ai2', 'openai', 'ia', 'chatgpt'];
+handler.help = ['openai', 'ia', 'chatgpt'];
+handler.tags = ['ai'];
+export default handler;
