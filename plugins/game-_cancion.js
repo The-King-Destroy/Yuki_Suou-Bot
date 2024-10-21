623b73ccb2506eb0cac0500a6346a519668c63fc
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-21 10:38:05 -0400

    Update game-_cancion.js

--- plugins/game-_cancion.js
+++ plugins/game-_cancion.js
@@ -5,6 +5,7 @@ const answerHandler = {
   async before(m) {
     const id = m.chat;
 
+    // Verifica que el mensaje sea una respuesta a la pregunta de adivinanza
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/ADIVINA EL TITULO DE LA CANCION/i.test(m.quoted.text)) return true;
 
     this.tebaklagu = this.tebaklagu ? this.tebaklagu : {};
@@ -12,15 +13,20 @@ const answerHandler = {
 
     if (m.quoted.id == this.tebaklagu[id][0].id) {
       const json = this.tebaklagu[id][1];
-      if (m.text.toLowerCase() === json.jawaban.toLowerCase().trim()) {
+      
+      // Compara la respuesta del usuario con la respuesta correcta
+      const userAnswer = m.text.toLowerCase().trim();
+      const correctAnswer = json.jawaban.toLowerCase().trim();
+
+      if (userAnswer === correctAnswer) {
         global.db.data.users[m.sender].cookies += this.tebaklagu[id][2];
-        m.reply(`✅Correcto!\n+${this.tebaklagu[id][2]} Cookies`);
+        m.reply(`✅ Correcto!\n+${this.tebaklagu[id][2]} Cookies`);
         clearTimeout(this.tebaklagu[id][3]);
         delete this.tebaklagu[id];
-      } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) {
+      } else if (similarity(userAnswer, correctAnswer) >= threshold) {
         m.reply(`Casi!`);
       } else {
-        m.reply(`❌Incorrecto!`);
+        m.reply(`❌ Incorrecto!`);
       }
     }
     return true;
