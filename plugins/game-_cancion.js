e3cc0e331edcce55f55eed5db6d286048ac73ab5
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-21 10:31:50 -0400

    Update game-_cancion.js

--- plugins/game-_cancion.js
+++ plugins/game-_cancion.js
@@ -1,23 +1,27 @@
-import similarity from 'similarity';
-const threshold = 0.72;
-const handler = {
+const answerHandler = {
   async before(m) {
     const id = m.chat;
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/ADIVINA EL TITULO DE LA CANCION/i.test(m.quoted.text)) return !0;
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/ADIVINA EL TITULO DE LA CANCION/i.test(m.quoted.text)) return true;
+
     this.tebaklagu = this.tebaklagu ? this.tebaklagu : {};
     if (!(id in this.tebaklagu)) return m.reply('El juego ha terminado');
+
     if (m.quoted.id == this.tebaklagu[id][0].id) {
-      const json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]));
-      if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-        global.db.data.users[m.sender].exp += this.tebaklagu[id][2];
-        m.reply(`✅Correcto!\n+${this.tebaklagu[id][2]} XP`);
+      const json = this.tebaklagu[id][1];
+      if (m.text.toLowerCase() === json.jawaban.toLowerCase().trim()) {
+        global.db.data.users[m.sender].cookies += this.tebaklagu[id][2];
+        m.reply(`✅Correcto!\n+${this.tebaklagu[id][2]} Cookies`);
         clearTimeout(this.tebaklagu[id][3]);
         delete this.tebaklagu[id];
-      } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Casii!`);
-      else m.reply(`❌Incorrecto!`);
+      } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= 0.72) {
+        m.reply(`Casi!`);
+      } else {
+        m.reply(`❌Incorrecto!`);
+      }
     }
-    return !0;
+    return true;
   },
   exp: 0,
 };
-export default handler;
+
+export { handler, answerHandler };
