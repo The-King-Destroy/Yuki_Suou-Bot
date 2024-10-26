10a8458dff0eee317803e525fe8abd66cf595956
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-26 05:27:56 -0400

    Update emox-penetrar.js

--- plugins/emox-penetrar.js
+++ plugins/emox-penetrar.js
@@ -1,24 +1,14 @@
 let handler = async (m, { conn, command, text }) => {
-    // Verifica si se proporcionó un texto
-    if (!text) {
-        return conn.reply(m.chat, `*Por favor, menciona al usuario para saber si puedes ${command.replace('how', '')}*`, m);
-    }
-
     // Obtiene el usuario mencionado o el que respondió al mensaje
-    let user = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : null);
-    
-    // Verifica si se encontró al usuario
-    if (!user) {
-        return conn.reply(m.chat, `*No se encontró al usuario mencionado o citado.*`, m);
-    }
+    let user = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : m.sender);
 
     // Mensaje de respuesta
     const responseMessage = `
 *TE HAN LLENADO LA CARA DE SEMEN POR PUTA Y ZORRA!*
 
-*Le ha metido el pene a ${text}* con todo y condón hasta quedar seco, has dicho "por favor más duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tú!" mientras te penetraba y luego te ha dejado en silla de ruedas!
+*Le ha metido el pene a ${text || 'tú'}* con todo y condón hasta quedar seco, has dicho "por favor más duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tú!" mientras te penetraba y luego te ha dejado en silla de ruedas!
 
-*${text}* 
+*${text || 'tú'}* 
 🔥 *YA TE HAN PENETRADO!*`;
 
     // Envía la respuesta al chat
