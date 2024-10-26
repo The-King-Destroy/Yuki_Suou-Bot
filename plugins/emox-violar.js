d2ff6e9f6363b9a9045459065083302c681b7291
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-26 07:31:32 -0400

    Update emox-violar.js

--- plugins/emox-violar.js
+++ plugins/emox-violar.js
@@ -22,11 +22,11 @@ if (!db.data.chats[m.chat].nsfw && m.isGroup) return m.reply('🚩 *¡Estos coma
     // Construimos el mensaje dependiendo de si hay una mención o no
     let str;
     if (m.mentionedJid.length > 0) {
-        str = `${name2} Acabas de Violar a la putita de ${name || who}mientras te decía metemela durooo más durooo que rico pitote
-tenemos que volver a sudar juntos.`; // Usamos nombre agendado o número si no está agendado
+        str = `${name2} Acabas de Violar a la putita de ${name || who}. mientras te decía metemela durooo más durooo que rico pitote
+tenemos que volver a sudar juntos`; // Usamos nombre agendado o número si no está agendado
     } else if (m.quoted) {
-        str = `${name2} Acabas de Violar a la putita de ${name || who}mientras te decía metemela durooo más durooo que rico pitote
-tenemos que volver a sudar juntos.`; // Mensaje cuando se cita a otro usuario
+        str = `${name2} Acabas de Violar a la putita de ${name || who}. mientras te decía metemela durooo más durooo que rico pitote
+tenemos que volver a sudar juntos`; // Mensaje cuando se cita a otro usuario
     } else {
         str = `${name2} violo a alguien random del grupo por puta`.trim();
     }<
