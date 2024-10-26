3a5634f362b30ff51a7eb35bbaab4128c1d3fdbe
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-26 05:31:44 -0400

    Update emox-penetrar.js

--- plugins/emox-penetrar.js
+++ plugins/emox-penetrar.js
@@ -1,14 +1,15 @@
 let handler = async (m, { conn, command, text }) => {
     // Obtiene el usuario mencionado o el que respondió al mensaje
     let user = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : m.sender);
+    let userName = user === m.sender ? 'tú' : `@${user.split('@')[0]}`;
 
     // Mensaje de respuesta
     const responseMessage = `
 *TE HAN LLENADO LA CARA DE SEMEN POR PUTA Y ZORRA!*
 
-*Le ha metido el pene a ${text || 'tú'}* con todo y condón hasta quedar seco, has dicho "por favor más duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tú!" mientras te penetraba y luego te ha dejado en silla de ruedas!
+*Le ha metido el pene a ${text || userName}* con todo y condón hasta quedar seco, has dicho "por favor más duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tú!" mientras te penetraba y luego te ha dejado en silla de ruedas!
 
-*${text || 'tú'}* 
+*${text || userName}* 
 🔥 *YA TE HAN PENETRADO!*`;
 
     // Envía la respuesta al chat
@@ -23,4 +24,4 @@ handler.register = true;
 handler.group = true;
 handler.fail = null;
 
-export default handler;
+export default handler
