adab72aaaa19fb6eb18ecfbd219070aa1b000a78
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-27 02:40:47 -0400

    Add files via upload

--- plugins/emox-hello.js
+++ plugins/emox-hello.js
@@ -17,27 +17,29 @@ let handler = async (m, { conn, usedPrefix }) => {
 
     let name = conn.getName(who); // Nombre de la persona mencionada o del emisor
     let name2 = conn.getName(m.sender); // Nombre del usuario que envía el comando
-    m.react('😏');
+    m.react('👋');
 
     // Construimos el mensaje dependiendo de si hay una mención o no
     let str;
     if (m.mentionedJid.length > 0) {
-        str = `${name2} le está manoseando a ${name || who} estas muy excitante hoy.`; // Usamos nombre agendado o número si no está agendado
+        str = `${name2} hola ${name || who} como estas?.`; // Usamos nombre agendado o número si no está agendado
     } else if (m.quoted) {
-        str = `${name2} está manoseando a ${name || who} estás muy sensual hoy.`; // Mensaje cuando se cita a otro usuario
+        str = `${name2} hola ${name || who} como te encuentras hoy?.`; // Mensaje cuando se cita a otro usuario
     } else {
-        str = `${name2} está manoseando! >.<`.trim();
+        str = `${name2} saludos para todos los del grupo, como se encuentran?`.trim();
     }
     
     if (m.isGroup) {
-        let pp = 'https://qu.ax/RdBAy.mp4'; 
-        let pp2 = 'https://qu.ax/DolnW.mp4'; 
-        let pp3 = 'https://qu.ax/OydCh.mp4';
-        let pp4 = 'https://qu.ax/CDrZm.mp4';
-        let pp5 = 'https://qu.ax/BRmBk.mp4';
-        let pp6 = 'https://qu.ax/AFzqg.mp4';
+        let pp = 'https://qu.ax/EcRBE.mp4'; 
+        let pp2 = 'https://qu.ax/oARle.mp4'; 
+        let pp3 = 'https://qu.ax/eQXQh.mp4';
+        let pp4 = 'https://qu.ax/ddLrC.mp4';
+        let pp5 = 'https://qu.ax/oalOG.mp4';
+        let pp6 = 'https://qu.ax/nYJ.mp4';
+        let pp7 = 'https://qu.ax/bkcz.mp4';
+        let pp8 = 'https://qu.ax/oARle.mp4';
         
-        const videos = [pp, pp2, pp3, pp4, pp5, pp6];
+        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8];
         const video = videos[Math.floor(Math.random() * videos.length)];
         
         // Enviamos el mensaje con el video y el mensaje correspondiente
@@ -46,9 +48,9 @@ let handler = async (m, { conn, usedPrefix }) => {
     }
 }
 
-handler.help = ['grop/manosear @tag'];
+handler.help = ['hello/hola @tag'];
 handler.tags = ['emox'];
-handler.command = ['grop', 'manosear'];
+handler.command = ['hello','hola'];
 handler.group = true;
 
 export default handler;
