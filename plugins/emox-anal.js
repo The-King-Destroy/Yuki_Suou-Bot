f1e3cb6e35d8d5aecb487625254b2ce9db34a5ef
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-28 16:04:34 -0400

    Update emox-anal.js

--- plugins/emox-anal.js
+++ plugins/emox-anal.js
@@ -22,11 +22,11 @@ if (!db.data.chats[m.chat].nsfw && m.isGroup) return m.reply('🚩 *¡Estos coma
     // Construimos el mensaje dependiendo de si hay una mención o no
     let str;
     if (m.mentionedJid.length > 0) {
-        str = `/`${name2}`/ le partio el culo a la puta de ${name || who}.`; // Usamos nombre agendado o número si no está agendado
+        str = `\`${name2}\` le partio el culo a la puta de ${name || who}.`; // Usamos nombre agendado o número si no está agendado
     } else if (m.quoted) {
-        str = `/`${name2}`/ se la metio en el ano a ${name || who}.`; // Mensaje cuando se cita a otro usuario
+        str = `\`${name2}\` se la metio en el ano a ${name || who}.`; // Mensaje cuando se cita a otro usuario
     } else {
-        str = `/`${name2}`/ esta haciendo un anal`.trim();
+        str = `\`${name2}\` esta haciendo un anal`.trim();
     }
     
     if (m.isGroup) {
