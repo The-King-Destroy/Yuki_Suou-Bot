1c49cd55f91accd00a152e7668bac5b94e1be558
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-29 00:07:57 -0400

    Update convertidor-tourl3.js

--- plugins/convertidor-tourl3.js
+++ plugins/convertidor-tourl3.js
@@ -1,47 +1,56 @@
+
 import axios from 'axios';
 import FormData from 'form-data';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
 
+const IMGBB_API_KEY = '1f55ea75f24df783643940f3eacbbc96'; // Reemplaza con tu propia API key si es necesario
+
 let handler = async (m, { args, command, usedPrefix }) => {
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || '';
-  if (!mime) throw `✳️ ${mssg.replyImg}`;
-  if (!args[0]) throw ` \`\`\`[ 🌺 ] Ingresa un texto para guardar la imagen. Ejemplo:\n${usedPrefix + command} Yuki\`\`\``
+  
+  // Verificar si el tipo de archivo es una imagen
+  if (!mime.startsWith('image/')) {
+    throw `✳️ Por favor, envía una imagen.`;
+  }
 
-  let media = await q.download();
-  let tempFilePath = path.join(os.tmpdir(), 'Yuki');
-  fs.writeFileSync(tempFilePath, media);
-
-  let form = new FormData();
-  form.append('image', fs.createReadStream(tempFilePath));
+  // Verificar que se haya proporcionado un texto
+  if (!args[0]) {
+    throw `\`\`\`[ 🌺 ] Ingresa un texto para guardar la imagen. Ejemplo:\n${usedPrefix + command} Yuki\`\`\``;
+  }
 
   try {
-    let response = await axios.post('https://api.imgbb.com/1/upload?key=1f55ea75f24df783643940f3eacbbc96', form, {
+    let media = await q.download();
+    let tempFilePath = path.join(os.tmpdir(), `${args[0]}.jpg`); // Guardar con el nombre proporcionado
+    fs.writeFileSync(tempFilePath, media);
+
+    let form = new FormData();
+    form.append('image', fs.createReadStream(tempFilePath));
+
+    let response = await axios.post(`https://api.imgbb.com/1/upload?key=${IMGBB_API_KEY}`, form, {
       headers: {
         ...form.getHeaders()
       }
     });
 
-    if (!response.data || !response.data.data || !response.data.data.url) throw '❌ Error al subir el archivo';
+    if (!response.data || !response.data.data || !response.data.data.url) {
+      throw '❌ Error al subir el archivo a ImgBB';
+    }
     
     let link = response.data.data.url;
-    fs.unlinkSync(tempFilePath);
+    fs.unlinkSync(tempFilePath); // Eliminar archivo temporal
 
-    m.reply(`❖ ${media.length} Byte(s)
-
-❖ (Archivo subido a ImgBB)
-❖ *URL:* ${link}
-    `);
+    m.reply(`❖ *${args[0]}* ha sido guardado como imagen.\n❖ (Archivo subido a ImgBB)\n❖ *URL:* ${link}`);
   } catch (error) {
     console.error('Error al subir el archivo:', error.message);
-    throw '❌ Error al subir el archivo a ImgBB';
+    throw '❌ Ocurrió un error al procesar la imagen.';
   }
 }
 
-handler.help = ['tourl3'];
+handler.help = ['to/up'];
 handler.tags = ['transformador'];
-handler.command = ['up', 'tourl3'];
+handler.command = ['up', 'to'];
 
 export default handler;
