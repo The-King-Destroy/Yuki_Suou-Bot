8a39c1b1743c0dbd3c409d652951fe4268b0241b
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-30 15:20:54 -0400

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,31 +1,29 @@
-import { WAMessageStubType } from '@whiskeysockets/baileys';
-import fetch from 'node-fetch';
+import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
 
-export async function before(m, { conn, participants, groupMetadata }) {
+export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
+    let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => welcome)
+    let pp2 = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => adios)
+  let img = await (await fetch(`${pp}`)).buffer()
+  let img2 = await (await fetch(`${pp2}`)).buffer()
 
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => welcome);
-  let pp2 = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => adios);
-  let img = await (await fetch(`${pp}`)).buffer();
-  let img2 = await (await fetch(`${pp2}`)).buffer();
-
-  let chat = global.db.data.chats[m.chat];
+  let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let wel = chat.welcome ? chat.welcome : ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨️ \n │「 𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎 😁 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │💖  𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎/𝐀\n   │🤍  ${groupMetadata.subject}\n   └───────────────┈ ⳹`;
-    await conn.sendMini(m.chat, packname, dev, wel, img, img, channel, fkontak);
+    let wel = ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨️ \n │「 𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎 😁 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🌹  𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎/𝐀\n   │🤍  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, wel, img, img, channel, fkontak)
   }
 
-  if (chat.bye && m.messageStubType == 28) {
-    let bye = chat.bye ? chat.bye : ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨️ \n │「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │😒  𝐒𝐄 𝐅𝐔𝐄\n   │💥 𝐍𝐮𝐧𝐜𝐚 𝐓𝐞 𝐐𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐀𝐪𝐮í\n   └───────────────┈ ⳹`;
-    await conn.sendMini(m.chat, packname, dev, bye, img2, img2, channel, fkontak);
+  if (chat.welcome && m.messageStubType == 28) {
+   let bye = ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨️ \n │「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │😒  𝐒𝐄 𝐅𝐔𝐄\n   │🥀 𝐍𝐮𝐧𝐜𝐚 𝐓𝐞 𝐐𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐀𝐪𝐮í\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, bye, img2, img2, channel, fkontak)
   }
 
-  if (chat.bye && m.messageStubType == 32) {
-    let kick = chat.bye ? chat.bye : ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨\n │「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │😒  𝐒𝐄 𝐅𝐔𝐄\n   │💥 𝐍𝐮𝐧𝐜𝐚 𝐓𝐞 𝐐𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐀𝐪𝐮í\n   └───────────────┈ ⳹`;
-    await conn.sendMini(m.chat, packname, dev, kick, img2, img2, channel, fkontak);
-  }
-}
+  if (chat.welcome && m.messageStubType == 32) {
+    let kick = ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨\n │「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │😒  𝐒𝐄 𝐅𝐔𝐄\n   │🥀 𝐍𝐮𝐧𝐜𝐚 𝐓𝐞 𝐐𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐀𝐪𝐮í\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, kick, img2, img2, channel, fkontak)
+}}
 
 /*import { WAMessageStubType } from '@whiskeysockets/baileys';
 import fetch from 'node-fetch';
