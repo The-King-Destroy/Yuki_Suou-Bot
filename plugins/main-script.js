14cbee7362987b565ac49ba0fa4c5d94faa07685
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-30 02:12:28 -0400

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args }) => {
   try {
-    let res = await fetch('https://api.github.com/The-King-Destroy/Yuki_Suou-Bot');
+    let res = await fetch('https://github.com/The-King-Destroy/Yuki_Suou-Bot');
     if (!res.ok) throw new Error('Error al obtener datos del repositorio');
     let json = await res.json();
 
