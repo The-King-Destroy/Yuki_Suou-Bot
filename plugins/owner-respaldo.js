49e72365d2334c416964c6071f40d41eae70c6e1
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-30 08:55:27 -0400

    Add files via upload

---
+++ plugins/owner-respaldo.js
@@ -0,0 +1,52 @@
+import AdmZip from 'adm-zip';
+import fs from 'fs';
+import path from 'path';
+/************
+https://Github.com/FzTeis
+************/
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+    function ZipFiles(directorio, name) {
+        const zip = new AdmZip();
+        function addFiles(directorio) {
+            const archivos = fs.readdirSync(directorio);
+            archivos.forEach((archivo) => {
+                const rutaCompleta = path.join(directorio, archivo);
+                if (archivo.startsWith('.') || archivo === 'node_modules') {
+                    return;
+                }
+                const stats = fs.statSync(rutaCompleta);
+
+                if (stats.isDirectory()) {
+                    addFiles(rutaCompleta);
+                } else {
+                    zip.addLocalFile(rutaCompleta, path.relative('.', directorio));
+                }
+            });
+        }
+        addFiles(directorio);
+        const zipPath = `${name}.zip`;
+        zip.writeZip(zipPath);
+        return zipPath;
+    }
+    try {
+        const Base = '.'; // Directorio actual
+        const name = 'Sylphiette_Respaldo';
+        const rutaZip = ZipFiles(Base, name);
+        const Sylphiette = fs.readFileSync(rutaZip);
+        await conn.sendMessage(m.chat, { 
+            document: Sylphiette, 
+            mimetype: 'application/zip', 
+            fileName: `${botName}.zip`, 
+            caption: "*[ 🌹 ] Successful.*"
+        }, { quoted: m });
+        fs.unlinkSync(rutaZip);
+    } catch (error) {
+        console.error(error);
+        await conn.sendMessage(m.chat, { text: 'Hubo un error al intentar generar o enviar el archivo zip.' }, { quoted: m });
+    }
+}
+
+handler.help = ['respaldo'];
+handler.command = ['getfiless','respaldo''compress'];
+handler.owner = true;
+export default handler;
