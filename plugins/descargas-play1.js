40d6ce3bddc89d6292591f4824c210ff095c2ecd
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-30 01:54:25 -0400

    Update descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -1,10 +1,28 @@
-import { yt-search } from 'ruhend-scraper';
+
+import { ytmp3, ytmp4 } from 'ruhend-scraper';
+import yts from 'yt-search'; // Asegúrate de que este paquete esté instalado
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-    const fkontak = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-    if (!text) throw `*[🌹] Complementa tu peticion con alguna canción o video (Se recomienda especificar al autor)*.\n\n _🥀.- Ejemplo_ *${usedPrefix + command} Enemy tommoee profitt.*`;
-    const randomReduction = Math.floor(Math.random() * 5) + 1;
+    const fkontak = {
+        'key': {
+            'participants': '0@s.whatsapp.net',
+            'remoteJid': 'status@broadcast',
+            'fromMe': false,
+            'id': 'Halo'
+        },
+        'message': {
+            'contactMessage': {
+                'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+            }
+        },
+        'participant': '0@s.whatsapp.net'
+    };
+
+    if (!text) throw `*[🌹] Complementa tu petición con alguna canción o video (Se recomienda especificar al autor)*.\n\n _🥀.- Ejemplo_ *${usedPrefix + command} Enemy tommoee profitt.*`;
+
     let search = await yts(text);
+    if (!search.all.length) throw `*[❌] No se encontraron resultados para: ${text}*`;
+
     let isVideo = /vid$/.test(command);
     let urls = search.all[0].url;
     let body = `*『  𝐘 𝐮 𝐤 𝐢 _ 𝐒 𝐮 𝐨 𝐮 - 𝐁 𝐨 𝐭  』*
@@ -16,39 +34,33 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
  *🝤.- 𝙻𝚒𝚗𝚔* ${urls}
 
 *🝩.- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 ${isVideo ? '𝚟𝚒𝚍𝚎𝚘' : '𝚊𝚞𝚍𝚒𝚘'}, 𝚊𝚐𝚞𝚊𝚛𝚍𝚊 𝚞𝚗 𝚖𝚘𝚖𝚎𝚗𝚝𝚘...*`;
-    
-    conn.sendMessage(m.chat, { 
-        image: { url: search.all[0].thumbnail }, 
-        caption: body 
+
+    conn.sendMessage(m.chat, {
+        image: { url: search.all[0].thumbnail },
+        caption: body
     }, { quoted: fkontak });
-    let res = await DOWNLOAD_YT(urls)
+
+    let res = await DOWNLOAD_YT(urls);
     let type = isVideo ? 'video' : 'audio';
     let video = res.video.dl_link;
     let audio = res.audio.dl_link;
-    conn.sendMessage(m.chat, { 
-        [type]: { url: isVideo ? video : audio }, 
-        gifPlayback: false, 
-        mimetype: isVideo ? "video/mp4" : "audio/mpeg" 
+
+    conn.sendMessage(m.chat, {
+        [type]: { url: isVideo ? video : audio },
+        gifPlayback: false,
+        mimetype: isVideo ? "video/mp4" : "audio/mpeg"
     }, { quoted: m });
-    
-    if (type = video) {
-        handler.limit = 10;
-    }
-    if (type = audio) {
-        handler.limit = 5;
-    }
 }
 
 handler.command = ['play', 'play2'];
 export default handler;
 
 async function DOWNLOAD_YT(input) {
-    let ytSearch = await yts(input)
-    let { title, url, thumbnail, description, views, ago, duration } = ytSearch.videos[0]
+    let ytSearch = await yts(input);
+    let { title, url, thumbnail, description, views, ago, duration } = ytSearch.videos[0];
 
-    let { video, quality, size } = await ytmp4(url)
-
-    let { audio } = await ytmp3(url)
+    let { video, quality, size } = await ytmp4(url);
+    let { audio } = await ytmp3(url);
 
     let resultados = {
         Status: true,
@@ -67,9 +79,7 @@ async function DOWNLOAD_YT(input) {
         audio: {
             dl_link: audio
         }
+    };
 
-    }
-
-    return resultados
-
+    return resultados;
 }
