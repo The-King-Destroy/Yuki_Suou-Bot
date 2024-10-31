6698df2ae1d9f3ebc4375b885f3d23f0229627e3
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-31 02:08:33 -0400

    Add files via upload

--- plugins/descargas-play2.js
+++ plugins/descargas-play2.js
@@ -1,28 +1,28 @@
-import yts from 'yt-search';
+import yts from 'yt-search' 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `\`\`\`[ 🌹 ] Por favor ingresa un texto. Ejemplo:\n${usedPrefix + command} Did i tell u that i miss you\`\`\``;
+    if (!text) throw `Ejemplo: ${usedPrefix + command} diles`,m ,rcanal;
 
     const randomReduction = Math.floor(Math.random() * 5) + 1;
     let search = await yts(text);
-    let f = `\n\n${String.fromCharCode(68,101,118,101,108,111,112,101,100,32,98,121,32,73,39,109,32,70,122,32,126)}`;
     let isVideo = /vid$/.test(command);
     let urls = search.all[0].url;
-    let body = `\`\`\`⊜─⌈ 📻 ◜YouTube Play◞ 📻 ⌋─⊜
+    let body = `\`YouTube Play\`
 
-    ≡ Título : » ${search.all[0].title}
-    ≡ Views : » ${search.all[0].views}
-    ≡ Duration : » ${search.all[0].timestamp}
-    ≡ Uploaded : » ${search.all[0].ago}
-    ≡ URL : » ${urls}
+    *Título:* ${search.all[0].title}
+    *Vistas:* ${search.all[0].views}
+    *Duración:* ${search.all[0].timestamp}
+    *Subido:* ${search.all[0].ago}
+    *Url:* ${urls}
 
-# 🌷 Su ${isVideo ? 'Video' : 'Audio'} se está enviando, espere un momento...\`\`\``;
-
+🕒 *Su ${isVideo ? 'Video' : 'Audio'} se está enviando, espere un momento...*`;
+    
     conn.sendMessage(m.chat, { 
         image: { url: search.all[0].thumbnail }, 
-        caption: body + f
-    }, { quoted: m });
+        caption: body
+    }, { quoted: m,rcanal });
+    m.react('react1')
 
-    let res = await dl_vid(urls);
+    let res = await dl_vid(urls)
     let type = isVideo ? 'video' : 'audio';
     let video = res.data.mp4;
     let audio = res.data.mp3;
@@ -31,15 +31,10 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
         gifPlayback: false, 
         mimetype: isVideo ? "video/mp4" : "audio/mpeg" 
     }, { quoted: m });
-
-    // Agregar la firma al final del mensaje
-    conn.sendMessage(m.chat, { 
-        text: `\n\n♡⃝𝒴𝓊𝓀𝒾_𝒮𝓊𝑜𝓊-𝐵𝑜𝓉ᚐ҉ᚐ`
-    }, { quoted: m });
 }
 
-handler.command = ['play1', 'playvid'];
-handler.help = ['play1', 'playvid'];
+handler.command = ['musica', 'video'];
+handler.help = ['musica', 'video'];
 handler.tags = ['descargas'];
 export default handler;
 
