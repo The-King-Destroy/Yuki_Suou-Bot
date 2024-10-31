76cdbcba95159f0ed4695d97896803527c98c5fb
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-30 23:00:27 -0400

    Update descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,3 +1,4 @@
+
 import yts from 'yt-search';
 
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
@@ -24,18 +25,13 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
     const downloadUrl = delius.data.download.url;
 
     // Crear el mensaje informativo del video/audio
-    let body = `*『 𝐘 𝐮 𝐤 𝐢 _ 𝐒 𝐮 𝐨 𝐮 - 𝐁 𝐨 𝐭  』*
-
- *☊.- 𝚃𝚒́𝚝𝚞𝚕𝚘:* ${vid.title}
- *♕.- 𝙰𝚞𝚝𝚘𝚛:* ${vid.author || 'Desconocido'}  // Manejo de casos donde no haya autor
- *⛨.- 𝙲𝚊𝚗𝚊𝚕* ${vid.channel}
- *🝓.- 𝙵𝚎𝚌𝚑𝚊 𝚍𝚎 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚌𝚒𝚘́𝚗:* ${vid.ago}
- *🜵.- 𝙳𝚞𝚛𝚊𝚌𝚒𝚘́𝚗:* ${vid.timestamp}
- *🜚.- 𝚅𝚒𝚜𝚝𝚊𝚜:* ${vid.views}
- *🝤.- 𝙻𝚒𝚗𝚔* ${videoUrl}
-
-*🝩.- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 𝚊𝚞𝚍𝚒𝚘, 𝚊𝚐𝚞𝚊𝚛𝚍𝚊 𝚞𝚗 𝚖𝚘𝚖𝚎𝚗𝚝𝚘...*
-
+    let body = `*☊.- 𝚃𝚒́𝚝𝚞𝚖𝚘:* ${vid.title}
+*🝓.- 𝙵𝚎𝚌𝚑𝚊 𝚍𝚎 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚌𝚒𝚘́𝚗:* ${vid.ago}
+*🜵.- 𝙳𝚞𝚛𝚊𝚌𝚒𝚘́𝚗:* ${vid.timestamp}  // Asegúrate de que `timestamp` sea la duración en el formato correcto
+*🜚.- 𝚅𝚒𝚜𝚝𝚊𝚜:* ${`${vid.views}`}
+*♕.- 𝙰𝚞𝚝𝚘𝚛:* ${vid.author || 'Desconocido'}  // Manejo de casos donde no haya autor
+*⛨.- 𝙲𝚊𝚗𝚊𝚕:* ${vid.channel}
+*🝤.- 𝙻𝚒𝚗𝚔:* ${videoUrl}\n
 > ৎ୭࠭͢𝒴𝓊𝓀𝒾_𝒮𝓊𝑜𝓊-𝐵𝑜𝓉𝐭ⷭ𓆪͟͞ `;
 
     // Enviar el mensaje informativo con la imagen
