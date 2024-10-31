ee83ffc5eb276201dd5296ff6c010957a3804431
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-10-30 23:06:32 -0400

    Update descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -29,8 +29,8 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
 
  *☊.- 𝚃𝚒́𝚝𝚞𝚕𝚘:* ${vid.title || 'Desconocido'}
  *♕.- 𝙰𝚞𝚝𝚘𝚛:* ${vid.author?.name || 'Desconocido'}  // Manejo de casos donde no haya autor
- *⛨.- 𝙲𝚊𝚗𝚊𝚕:* ${vid.author?.url ? `[${vid.author.url}](${vid.author.url})` : 'Desconocido'}  // Manejo de casos donde no haya canal
- *🝓.- 𝙵𝚎𝚌𝚑𝚊 𝚍𝚎 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚌𝚘́𝚗:* ${vid.ago || 'Desconocido'}
+ *⛨.- 𝙲𝚊𝚗𝚊𝚕:* ${vid.author?.url ? `[${vid.author.name}](${vid.author.url})` : 'Desconocido'}  // Manejo de casos donde no haya canal
+ *🝓.- 𝙵𝚎𝚌𝚑𝚊 𝚍𝚎 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚌𝚒𝚘́𝚗:* ${vid.ago || 'Desconocido'}
  *🜵.- 𝙳𝚞𝚛𝚊𝚌𝚒𝚘́𝚗:* ${vid.timestamp || 'Desconocido'}
  *🜚.- 𝚅𝚒𝚜𝚝𝚊𝚜:* ${`${vid.views || 'Desconocido'}`}
  *🝤.- 𝙻𝚒𝚗𝚔:* ${videoUrl}\n
