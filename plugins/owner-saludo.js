df734a98b824c0d428df13040a4e8825be58df73
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-02 17:21:37 -0400

    Delete plugins/owner-saludo.js

--- plugins/owner-saludo.js
+++
@@ -1,7 +0,0 @@
-const handler = async (m, {conn, command}) => {
-  console.log(`/////////////////////////////////////////////////////////////////\n\nEl único reporte con fallos en este comando, no se presenta aquí.\n\n/////////////////////////////////////////////////////////////////`);
-  m.reply('*[ 🌹 ] Hmm, parece que me necesitas. Qué inesperado... 😒.*');
-};
-handler.command = /^(saludo)$/i;
-handler.owner = true;
-export default handler;
