01fd183060b0843e6a97e8d31b8b56d6d9303fe9
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-02 14:18:48 -0400

    Update main-hornymenu.js

--- plugins/main-hornymenu.js
+++ plugins/main-hornymenu.js
@@ -1,6 +1,9 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
-  if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `⚠️ *¡Estos comandos están desactivados!*`; 
+  let who;
+if (!db.data.chats[m.chat].nsfw && m.isGroup) {
+    return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
+}
   try {
     const pp = imagen5
     // let vn = './media/menu.mp3'
