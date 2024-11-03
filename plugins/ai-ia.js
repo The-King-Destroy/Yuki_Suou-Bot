64b2436c72dfa9734a38c86fd60be9f1add12de5
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-03 19:05:03 -0400

    Update ai-ia.js

--- plugins/ai-ia.js
+++ plugins/ai-ia.js
@@ -1,72 +1,16 @@
-import axios from 'axios'
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
-const username = `${conn.getName(m.sender)}`
-const basePrompt = `Tu nombre es Yuki_Suou-Bot y parece haber sido creado por ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, te encanta aprender y sobre todo los juegos. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
-if (isQuotedImage) {
-const q = m.quoted
-const img = await q.download?.()
-if (!img) {
-console.error('🚩 Error: No image buffer available')
-return conn.reply(m.chat, '🚩 Error: No se pudo descargar la imagen.', m, fake)}
-const content = '🚩 ¿Qué se observa en la imagen?'
-try {
-const imageAnalysis = await fetchImageBuffer(content, img)
-const query = '😊 Descríbeme la imagen y detalla por qué actúan así. También dime quién eres'
-const prompt = `${basePrompt}. La imagen que se analiza es: ${imageAnalysis.result}`
-const description = await luminsesi(query, username, prompt)
-await conn.reply(m.chat, description, m, fake)
-} catch (error) {
-console.error('🚩 Error al analizar la imagen:', error)
-await conn.reply(m.chat, '🚩 Error al analizar la imagen.', m, fake)}
-} else {
-if (!text) { return conn.reply(m.chat, `🍟 *Ingrese su petición*\n🚩 *Ejemplo de uso:* ${usedPrefix + command} Como hacer un avión de papel`, m, rcanal)}
-await m.react('💬')
-try {
-const query = text
-const prompt = `${basePrompt}. Responde lo siguiente: ${query}`
-const response = await luminsesi(query, username, prompt)
-await conn.reply(m.chat, response, m, fake)
-} catch (error) {
-console.error('🚩 Error al obtener la respuesta:', error)
-await conn.reply(m.chat, 'Error: intenta más tarde.', m, fake)}}}
+let handler = async (m, { conn, text }) => {
+if (!text) return m.reply('🌹 Ingresa un texto para hablar con la Bot')
 
-handler.help = ['chatgpt <texto>', 'ia <texto>']
-handler.tags = ['ai']
-handler.group = true;
-handler.register = true
+try {
+let api = await fetch(`https://deliriussapi-oficial.vercel.app/ia/chatgpt?q=${text}`)
+let json = await api.json()
+m.reply(json.data)
+} catch (error) {
+console.error(error)
+}}
 
-// handler.estrellas = 1
-handler.command = ['ia', 'chatgpt']
+handler.command = ['chatgpt']
 
 export default handler
-
-// Función para enviar una imagen y obtener el análisis
-async function fetchImageBuffer(content, imageBuffer) {
-try {
-const response = await axios.post('https://Luminai.my.id', {
-content: content,
-imageBuffer: imageBuffer 
-}, {
-headers: {
-'Content-Type': 'application/json' 
-}})
-return response.data
-} catch (error) {
-console.error('Error:', error)
-throw error }}
-// Función para interactuar con la IA usando prompts
-async function luminsesi(q, username, logic) {
-try {
-const response = await axios.post("https://Luminai.my.id", {
-content: q,
-user: username,
-prompt: logic,
-webSearchMode: false
-})
-return response.data.result
-} catch (error) {
-console.error('🚩 Error al obtener:', error)
-throw error }}
