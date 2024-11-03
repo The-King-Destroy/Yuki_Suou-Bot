506a14d41ea069805577866b34b645611d60a7b3
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-02 20:06:26 -0400

    Add files via upload

--- handler.js
+++ handler.js
@@ -166,7 +166,7 @@ if (!('sDemote' in chat)) chat.sDemote = ''
 if (!('sCondition' in chat)) chat.sCondition = JSON.stringify([{ grupo: { usuario: [], condicion: [], admin: '' }, prefijos: []}])
 if (!('delete' in chat)) chat.delete = false                   
 if (!('nsfw' in chat)) chat.nsfw = false
-if (!('autoAceptar' in chat)) chat.autoAceptar = false
+if (!('autoAceptar' in chat)) chat.autoAceptar = false                   
 if (!('autosticker' in chat)) chat.autosticker = false      
 if (!('audios' in chat)) chat.audios = false
 if (!('antiBot' in chat)) chat.antiBot = false 
@@ -204,7 +204,8 @@ sPromote: '',
 sDemote: '', 
 sCondition: JSON.stringify([{ grupo: { usuario: [], condicion: [], admin: '' }, prefijos: []}]), 
 delete: false,
-modohorny: false,
+autoAceptar: false,
+nsfw: false,
 autosticker: false,
 audios: false,
 antiBot: false,
@@ -460,7 +461,7 @@ if (xp > 2000)
 m.reply('Chirrido -_-') 
 else               
 if (!isPrems && plugin.money && global.db.data.users[m.sender].money < plugin.money * 1) {
-conn.reply(m.chat, `❮💰❯ 𝗡𝗼 𝘁𝗶𝗲𝗻𝗲𝘀 𝘀𝘂𝗳𝗶𝗰𝗶𝗲𝗻𝘁𝗲𝘀 𝐘𝐮𝐤𝐢𝐂𝐨𝐢𝐧𝐬 𝗽𝗮𝗿𝗮 𝘂𝘀𝗮𝗿 𝗲𝘀𝘁𝗲 𝗰𝗼𝗺𝗮𝗻𝗱𝗼.`, m, rcanal)       
+conn.reply(m.chat, `❮💰❯ 𝗡𝗼 𝘁𝗶𝗲𝗻𝗲𝘀 𝘀𝘂𝗳𝗶𝗰𝗶𝗲𝗻𝘁𝗲𝘀 𝙔𝙪𝙠𝙞𝗖𝗼𝗶𝗻𝘀 𝗽𝗮𝗿𝗮 𝘂𝘀𝗮𝗿 𝗲𝘀𝘁𝗲 𝗰𝗼𝗺𝗮𝗻𝗱𝗼.`, m, rcanal)       
 continue     
 }
 
@@ -743,14 +744,14 @@ console.error(e)
 global.dfail = (type, m, conn) => {
 const msg = {
 rowner: '「👑」 *Esta función solo puede ser usada por mi creador*\n\n> Destroy.', 
-owner: '「👑」 *Esta función solo puede ser usada por mi desarrollador.', 
+owner: '「👨‍💻」 *Esta función solo puede ser usada por mi desarrollador.', 
 mods: '「👨‍💻」 *Esta función solo puede ser usada por mis desarrolladores.*', 
 premium: '「⚜️」 *Esta función solo es para usuarios Premium.', 
-group: '「⭐」 *Esta funcion solo puede ser ejecutada en grupos.*', 
+group: '「✨」 *Esta funcion solo puede ser ejecutada en grupos.*', 
 private: '「🌹」 *Esta función solo puede ser usada en chat privado.*', 
-admin: '「⚜️」 *Este comando solo puede ser usado por admins.*', 
-botAdmin: '「✨」 *Para usar esta función debo ser admin.*', 
-unreg: '「🌹」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg Destroy.17*',
+admin: '「🌟」 *Este comando solo puede ser usado por admins.*', 
+botAdmin: '「🌟」 *Para usar esta función debo ser admin.*', 
+unreg: '「🌹」 *¡Hey! no estas registrado, registrese para usar esta función*\n\n*/reg nombre.edad*\n\n*_❕ Ejemplo_* : */reg Destroy.18*',
 restrict: '「💫」 *Esta característica esta desactivada.*'
 }[type];
 if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
