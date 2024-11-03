2a5d8944763198e564d4f953d70fb211adc29e51
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-03 18:53:51 -0400

    Update +18-porno.js

--- plugins/+18-porno.js
+++ plugins/+18-porno.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const icons = 'https://qu.ax/tHKwZ.jpg'; // URL de tu ícono
 const handler = async (m, {command, conn, usedPrefix}) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
