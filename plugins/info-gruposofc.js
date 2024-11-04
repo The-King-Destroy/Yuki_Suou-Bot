d42de20425098598c739b84c7e06ba20597555e9
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-04 10:30:45 -0400

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,53 +1,40 @@
-let media = './src/Grupo.mp4'; // Ruta del archivo de video
+let media = 'https://files.catbox.moe/9b4u20.mp4'; // URL del video
 
 let handler = async (m, { conn, command }) => {
-    // Asumimos que m.chat es el chat correcto
-
-    let fkontak = {
-        "key": {
-            "participants": "0@s.whatsapp.net",
-            "remoteJid": "status@broadcast",
-            "fromMe": false,
-            "id": "Halo"
-        },
-        "message": {
-            "contactMessage": {
-                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-            }
-        },
-        "participant": "0@s.whatsapp.net"
-    };
-
     await m.react('🌷'); // Reacción al mensaje
 
     let str = `*📍 GRUPOS OFICIALES*
 
-   *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙🌹̶̩ܻᝒ̷̸꯭͙𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭𓆩֟֯፝𓆪𝝣⃯ᵴͦ𝛒⃨ᷫ𝛆ͨ🄲⃪⃯𝛊ᷨ𝛂⃨ͦꝆ᷽ͭ🍁⃝⃙̻⃮̋⃛⃰⁌̷̸̊͟⿻᳔̶̷̸_*
-  ┃🪷❏ ${gp4}
+   *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙🌹̶̩ܻᝒ̷̸꯭͙𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭𓆩֟֯፝⃝⃙̻⃮̋⃛⃰⁌̷̸̊͟⿻᳔̶̷̸_*
+   ┃🪷❏ ${gp4}
 
-   *_ ͟͞〾⃝̵͡♡⃝𝓨𝓾𝓴𝓲 𝓢𝓾𝓸𝓾 𝓑𝓸𝓣 𝒩𝐹𝒮𝒲☙_*
-┃🥀❏ https://chat.whatsapp.com/E78uEs2qJIE0apCLB7rSQZ
+   *_ ͟͞〾⃝̵͡♡⃝𝓨𝓾𝓴𝓲 𝓢𝓾𝓸𝓾 𝓑𝓸𝓣 𝓞𝓯𝓲𝓬𝓲𝓪𝓵ᚐ҉ᚐ_* 
+   ┃🌸❏ https://chat.whatsapp.com/E78uEs2qJIE0apCLB7rSQZ
    
-   *_❦𝒴𝓊𝓚𝒾 𝒮𝓊𝒪𝒰 𝐵𝒪𝒯  𝒩𝐹𝒮𝒲☙_*
-┃🍒❏ https://chat.whatsapp.com/BELmEmNjNlv36w7ElRrOKa
+   *_❦𝒴𝓊𝓀𝒾 𝒮𝓊𝑜𝓊 𝐵𝑜𝓣  𝒩𝐹𝒮𝒲☙_*
+   ┃🥀❏ https://chat.whatsapp.com/BELmEmNjNlv36w7ElRrOKa
 
    *_✿:･ﾟ✧ 𝒴𝒰𝒦𝐼 𝒮𝒰𝒪𝒰 𝐵𝒪𝒯 ✧ﾟ･:✿_*
-┃🌹❏ https://chat.whatsapp.com/BuLovToIxdiLeycG2d3xJN
+   ┃🍒❏ https://chat.whatsapp.com/BuLovToIxdiLeycG2d3xJN
 
    *_♡⃝𝒞𝐻𝒜𝒩𝒩𝐸𝐿 𝒴𝒰𝒦𝐼 𝒮𝒰𝒪𝒰ᚐ҉ᚐ_*
-┃🌹❏ https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T
-*_╰━━━━━━━━━━━━━━━━⊜_*`;
+   ┃🌹❏ https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T
+   *_╰━━━━━━━━━━━━━━━━⊜_*`;
 
     try {
-        // Enviar video directamente sin botones
-        await conn.sendMessage(m.chat, { video: { url: media }, caption: str });
+        // Enviar el mensaje con botones
+        await conn.sendButton(m.chat, str, `͟͞ 𓆩ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜ৎ୭࠱࠭ ͟͞\n` + wm, media, [
+            ['MENU 🌹', '#menu']
+        ], null, [
+            ['⏤͟͞ू⃪ ፝͜⁞𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭✰⃔࿐', `${md}`]
+        ], fkontak);
     } catch (error) {
-        console.error('Error al enviar el video:', error); // Mensaje de error
+        console.error('Error al enviar el mensaje:', error); // Mensaje de error
     }
 };
 
 handler.command = ['grupos', 'links', 'gruposofc', 'gruposoficiales'];
 handler.register = true;
-handler.exp = 33; // Puedes cambiar esto a 0 si lo deseas
+handler.exp = 10; // Puedes cambiar esto a 0 si lo deseas
 
 export default handler;
