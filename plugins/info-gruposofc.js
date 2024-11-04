87bf3eb805b15392cdfaf686ae4bba8c864e6a43
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-04 10:02:22 -0400

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,62 +1,31 @@
-let media = 'https://i.ibb.co/Kzp1rHt/Yuki.jpg'; // URL de la imagen que se enviará
-
-// Definiciones de variables adicionales
-let gp4 = "Grupo de Yuki Suou"; // Un nombre o descripción del grupo
-let wm = "Yuki Suou Bot"; // Marca de agua o nombre del bot
-
+let media = './src/Grupo.jpg'
 let handler = async (m, { conn, command }) => {
-    console.log(`Comando recibido: ${command}`); // Mensaje de depuración
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+   // await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
+ await m.react('⚡️')
+let str = `*📍 GRUPO OFICIAL*
 
-    let fkontak = {
-        "key": {
-            "participants": "0@s.whatsapp.net",
-            "remoteJid": "status@broadcast",
-            "fromMe": false,
-            "id": "Halo"
-        },
-        "message": {
-            "contactMessage": {
-                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-            }
-        },
-        "participant": "0@s.whatsapp.net"
-    };
+   *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙🌸̶̩ܻᝒ̷̸꯭͙𝝡꯭𝝣꯭𝗚꯭𝗨꯭𝗠꯭𝗜꯭𝗡᭄𓆩֟֯፝𓆪𝝣⃯ᵴͦ𝛒⃨ᷫ𝛆ͨ🄲⃪⃯𝛊ᷨ𝛂⃨ͦꝆ᷽ͭ🍁⃝⃙̻⃮̋⃛⃰⁌̷̸̊͟⿻᳔̶̷̸_*
+  ┃🧸❏ ${gp4}
 
-    // Reacción al mensaje
-    await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } });
-
-    let str = `*📍 GRUPO OFICIAL*
-
-   *_〾̷̸‣⃝⃨⃛⃰⁝̵̓ᝒ̷̸͙✨̶̩ܻᝒ̷̸꯭͙𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭𓆩֟֯፝𓆪𝝣⃯ᵴͦ𝛒⃨ᷫ𝛆ͨ🄲⃪⃯𝛊ᷨ𝛂⃨ͦꝆ᷽ͭ🌸⃙̻⃮⃛⃰⁌̷̸̊͟⿻᳔̶̷̸_*
-  ┃🪷❏ ${gp4}
-
-   *_♡⃝𝓨𝓾𝓴𝓲 𝓢𝓾𝓸𝓾 𝓑𝓸𝓽 𝓞𝓯𝓲𝓬𝓲𝓪𝓵ᚐ҉_*
-┃🌹❏ https://chat.whatsapp.com/E78uEs2qJIE0apCLB7rSQZ
+   *_ ͟͞〾⃝̵͡𝑬⃗ꭙȾ⃯𝑟𝘦𝑚⃖𝚎֟֯፝͜❀̵⃕ ̤🄷𝕖ᷨɳͦᵵͭ𝐚͢𝖏♡̵̭̭̎͟͞ ₂₀፝֟֯࣪࣪࣪₁₂_*
+┃🧸❏ https://chat.whatsapp.com/J9gyFJLbhVIJXaUZlpo8Xt
    
-   *_❦𝒴𝓊𝓀𝒾 𝒮𝓊𝑜𝓊 𝐵𝑜𝓣  𝒩𝐹𝒮𝒲 ☙_*
-┃🍒❏ https://chat.whatsapp.com/BELmEmNjNlv36w7ElRrOKa
+   *_Grupo de antojar 3.0_*
+┃🧸❏ https://chat.whatsapp.com/LJKcR8QBJgu37bVFWuhRVn
 
-   *_✿:･✧ 𝒴𝒰𝒦𝐼 𝒮𝒰𝒪𝒰 𝐵𝒪𝒯 ✧･:✿_*
-┃🥀❏ https://chat.whatsapp.com/BuLovToIxdiLeycG2d3xJN
+   *_Grupo Colaboración Yaemori_*
+┃❤️‍🔥❏ https://chat.whatsapp.com/HjVwC835OWgERQYhspnMW8
 
-   *_♡⃝𝒞𝐻𝒜𝒩𝒩𝐸𝐿 𝒴𝒰𝒦𝐼 𝒮𝒰𝒪𝒰ᚐ҉ᚐ_*
-┃🌸❏ https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T
-*_╰━━━━━━━━━━━━━━━━⊜_*`;
+   *_Canal Oficial_*
+┃❤️‍🔥❏ https://whatsapp.com/channel/0029VacDy0R6hENqnTKnG820
+`
+await conn.sendButton(m.chat, str, `͟͞ 𓆩ꪶꪾ𝘿᪶𝙞ᷨ𝙖ᷞ𝙢ͣ𝙤᪶ͨ𝙣ᷜ𝙙ꫂৎ୭࠱࠭ ͟͞\n` + wm, media, [
+['Menu Lista 💖', '/lista']], null, [
+['⏤͟͞ू⃪ ፝͜⁞M͢ᴇɢ፝֟ᴜᴍ⃨ɪɴ⃜✰⃔࿐', `${md}`]], fkontak)}
+                      
+handler.command = ['grupos','linksk','','gruposoficiales']
+handler.register = true
+handler.exp = 33
 
-    try {
-        // Enviar el mensaje con botones
-        await conn.sendButton(m.chat, str, `͟͞ 𓆩ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜ৎ୭࠱࠭ ͟͞\n` + wm, media, [
-            ['MENU 🌹', '#menu']
-        ], null, [
-            ['⏤͟͞ू⃪ ፝͜⁞𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭✰⃔࿐', `${md}`]
-        ], fkontak);
-    } catch (error) {
-        console.error('Error al enviar el mensaje:', error); // Mensaje de error
-    }
-};
-
-handler.command = ['grupos', 'links', 'gruposofc', 'gruposoficiales'];
-handler.register = true;
-handler.exp = 0; // Establecer exp en cero
-
-export default handler;
+export default handler
