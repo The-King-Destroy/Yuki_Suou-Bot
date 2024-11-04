bd0b9dd9b908f43b07086973aebc7539b7179c9f
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-04 11:23:40 -0400

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,12 +1,14 @@
-import yts from 'yt-search'; 
+import yts from 'yt-search';
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `\`\`\`[ 🌹 ] Por favor ingresa un texto. Ejemplo:\n${usedPrefix + command} Did i tell u that i miss you\`\`\``;
+    if (!text) throw `\`\`\`[ 🌹 ] Por favor ingresa un texto. Ejemplo:\n${usedPrefix + command} Did I tell you that I miss you\`\`\``;
 
     let search = await yts(text);
+    if (!search.all.length) throw `\`\`\`[ 🌹 ] No se encontraron resultados para "${text}".\`\`\``;
+
     let isVideo = /vid$/.test(command);
     let urls = search.all[0].url;
-    
+
     let body = `\`\`\`⊜─⌈ 📻 ◜YouTube Play◞ 📻 ⌋─⊜
 
     ≡ Título : » ${search.all[0].title}
@@ -16,25 +18,30 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     ≡ URL : » ${urls}
 
 # 🌹 Su ${isVideo ? 'Video' : 'Audio'} se está enviando, espere un momento...\`\`\``;
-    
+
     await conn.sendMessage(m.chat, { 
         image: { url: search.all[0].thumbnail }, 
-        caption: body + `\n\n> ♡⃝𝒴𝓊𝓀𝒾_𝒮𝓊𝑜𝓊-𝐵𝑜𝓉ᚐ҉ᚐ`
+        caption: body + `\n\n> ♡⃝𝒴𝓊𝓀𝒾_𝒮𝓊𝓸𝓊-𝐵𝑜𝓉ᚐ҉ᚐ`
     }, { quoted: m });
 
-    let res = await dl_vid(urls);
-    let type = isVideo ? 'video' : 'audio';
-    let mediaUrl = isVideo ? res.data.mp4 : res.data.mp3;
+    try {
+        let res = await dl_vid(urls);
+        let type = isVideo ? 'video' : 'audio';
+        let mediaUrl = isVideo ? res.data.mp4 : res.data.mp3;
 
-    await conn.sendMessage(m.chat, { 
-        [type]: { url: mediaUrl }, 
-        gifPlayback: false, 
-        mimetype: isVideo ? "video/mp4" : "audio/mpeg" 
-    }, { quoted: m });
+        await conn.sendMessage(m.chat, { 
+            [type]: { url: mediaUrl }, 
+            gifPlayback: false, 
+            mimetype: isVideo ? "video/mp4" : "audio/mpeg" 
+        }, { quoted: m });
+    } catch (error) {
+        console.error('Error al descargar el video/audio:', error);
+        await conn.sendMessage(m.chat, { text: 'Hubo un error al intentar descargar el video/audio. Intente más tarde.' }, { quoted: m });
+    }
 }
 
-handler.command = ['play1', 'playvid']; // Cambiado 'playvid' a 'play2'
-handler.help = ['play1', 'playvid']; // Cambiado 'playvid' a 'play2'
+handler.command = ['play1', 'playvid']; 
+handler.help = ['play1', 'playvid']; 
 handler.tags = ['descargas'];
 export default handler;
 
