08aa8d5db4a31265bf4ceb32a05be1d94f6f3638
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-05 01:19:40 -0400

    Update descargas-docmp3.js

--- plugins/descargas-docmp3.js
+++ plugins/descargas-docmp3.js
@@ -2,8 +2,9 @@ import fetch from 'node-fetch'
 import yts from 'yt-search'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw m.reply(`Ingresa un link de YouTube\n*✧ Ejemplo:* ${usedPrefix}${command} https://youtu.be/oGmW2CF001I`);
-    conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
+    if (!text) throw `🌹 Te Faltó Un Link De Un Video De Youtube.\n_(Puedes hacer una búsqueda utilizando el comando ${usedPrefix}yts)_\n _🌷.- Ejemplo:_ *${usedPrefix + command}* https://youtu.be/sBKR6aUorzA?si=TmC01EGbXUx2DUca`;
+
+    await conn.sendMessage(m.chat, { react: { text: '🥀', key: m.key } });
 
     let d2 = await fetch(`https://exonity.tech/api/ytdlp2-faster?apikey=adminsepuh&url=${text}`)
     let dp = await d2.json()
@@ -31,12 +32,25 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         audioUrl: dp.result.media.mp3,
     };
 
-    // Enviar el mensaje con la información y la imagen del audio solicitado
-    let messageCaption = `*『 𝐘 𝐮 𝐤 𝐢 _ 𝐒 𝐮 𝐨 𝐮 - 𝐁 𝐨 𝐭 』*\n\n *☊.- 𝚃𝚒́𝚝𝚞𝚕𝚘:* ${vid.title}\n *♕.- 𝙰𝚞𝚝𝚘𝚛:* ${vid.author}\n *⛨.- 𝙲𝚊𝚗𝚊𝚕:* ${vid.authorUrl}\n *🝓.- 𝙵𝚎𝚌𝚑𝚊 𝚍𝚎 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚌𝚒𝚘́𝚗:* ${vid.ago}\n *🜵.- 𝙳𝚞𝚛𝚊𝚌𝚘́𝚗:* ${vid.timestamp}\n *🜚.- 𝚅𝚒𝚜𝚝𝚊𝚜:* ${vid.views}\n *🝤.- 𝙻𝚒𝚗𝚔:* ${text}\n\n*🝩.- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 𝚊𝚞𝚍𝚒𝚘, 𝚊𝚐𝚞𝚊𝚝𝚊 𝚞𝚗 𝚖𝚘𝚖𝚎𝚗𝚝𝚘...*\n\n> ♡⃝𝒴𝓊𝓀𝒾_𝒮𝓊𝓸𝓊-𝐵𝑜𝓉ᚐ҉ᚐ`;
+    // Crear el mensaje informativo del video/audio
+    let body = `*『 𝐘 𝐮 𝐤 𝐢 _ 𝐒 𝐮 𝐨 𝐮 - 𝐁 𝐨 𝐭 』*\n\n` +
+               `*☊.- 𝚃𝚒́𝚝𝚞𝚕𝚘:* ${vid.title}\n` +
+               `*♕.- 𝙰𝚞𝚝𝚘𝚛:* ${vid.author}\n` +
+               `*⛨.- 𝙲𝚊𝚗𝚊𝚕:* ${vid.authorUrl}\n` +
+               `*🝓.- 𝙵𝚎𝚌𝚑𝚊 𝚍𝚎 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚌𝚒𝚘́𝚗:* ${vid.ago}\n` +
+               `*🜵.- 𝙳𝚞𝚛𝚊𝚌𝚘́𝚗:* ${vid.timestamp}\n` +
+               `*🜚.- 𝚅𝚒𝚜𝚝𝚊𝚜:* ${vid.views}\n` +
+               `*🝤.- 𝙻𝚒𝚗𝚔:* ${text}\n\n` +
+               `*🝩.- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 𝚊𝚞𝚍𝚒𝚘, 𝚊𝚐𝚞𝚊𝚝𝚊 𝚞𝚗 𝚖𝚘𝚖𝚎𝚗𝚝𝚘...*\n\n` +
+               `> ♡⃝𝒴𝓊𝓀𝒾_𝒮𝓊𝓸𝓊-𝐵𝑜𝓉ᚐ҉ᚐ`;
 
+    // Enviar el mensaje informativo con la imagen del audio solicitado
     if (vid.thumbnail) {
         const imgBuffer = await getBuffer(vid.thumbnail);
-        await conn.sendMessage(m.chat, { image: imgBuffer, caption: messageCaption }, { quoted: m });
+        await conn.sendMessage(m.chat, { 
+            image: imgBuffer, 
+            caption: body 
+        }, { quoted: m });
     }
 
     // Enviar el audio por separado
