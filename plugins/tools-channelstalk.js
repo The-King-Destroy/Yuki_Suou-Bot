2e0f84581fc1cc75b882857110c07e4d6baea88a
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-07 18:14:53 -0400

    Update tools-channelstalk.js

--- plugins/tools-channelstalk.js
+++ plugins/tools-channelstalk.js
@@ -3,7 +3,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw m.reply(`Ingresa un link de un canal\n*🌷 Ejemplo:* ${usedPrefix}${command} https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T`);
+if (!text) throw m.reply(`🥀 Ingresa un link de un canal\n*🌷 Ejemplo:* ${usedPrefix}${command} https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T`);
 conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
 
   let d2 = await fetch(`https://itzpire.com/stalk/whatsapp-channel?url=${text}`)
