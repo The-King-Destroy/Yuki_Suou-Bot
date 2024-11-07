4ad4285f27c67f23d599f2263f65e0d7fb5a62e5
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-07 01:37:43 -0400

    Update descargas-play_6.js

--- plugins/descargas-play_6.js
+++ plugins/descargas-play_6.js
@@ -1,7 +1,7 @@
 import yts from 'yt-search';
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-    if (!text) throw `_𝐄𝐬𝐜𝐫𝐢𝐛𝐞 𝐮𝐧𝐚 𝐩𝐞𝐭𝐢𝐜𝐢𝐨́𝐧 𝐥𝐮𝐞𝐠𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐞𝐣𝐞𝐦𝐩𝐥𝐨:_ \n*${usedPrefix + command} Billie Eilish - Bellyache*`
+    if (!text) throw `_𝐄𝐬𝐜𝐫𝐢𝐛𝐞 𝐮𝐧𝐚 𝐩𝐞𝐭𝐢𝐜𝐢𝐨́𝐧 𝐥𝐮𝐞𝐠𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐞𝐣𝐞𝐦𝐩𝐥𝐨:_ \n*${usedPrefix + command} Billie Eilish - Bellyache*`;
 
     await m.react('⏳');
     let res = await yts(text);
@@ -13,29 +13,34 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
 
     let { title, thumbnail, ago, timestamp, views, videoId, url } = play;
 
-    let txt =   '⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝔻𝔼𝕊ℂ𝔸ℝ𝔾𝔸𝕊໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪\n';
+    let txt = '⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝔻𝔼𝕊ℂ𝔸ℝ𝔾𝔸𝕊໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪\n';
     txt += '🍂⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝕐𝕦𝕜𝕚 𝕊𝕦𝕠𝕦໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪̇ \n';
     txt += `> *𝕋í𝕥𝕦𝕝𝕠* : _${title}_\n`;
     txt += `> *𝔸𝕦𝕥𝕠𝕣(𝕒)* : _${ago}_\n`;
     txt += `> *𝔻𝕦𝕣𝕒𝕔𝕚ó𝕟* : _${timestamp}_\n`;
     txt += `> *𝕍𝕚𝕤𝕥𝕒𝕤* : _${views.toLocaleString()}_\n`;
     txt += `> *𝔼𝕟𝕝𝕒𝕔𝕖* : _https://www.youtube.com/watch?v=${videoId}_\n`;
-    txt += '\n\n> ♡⃝𝒴𝓊𝓀𝒾_𝒮𝓊𝑜𝓊-𝐵𝑜𝓉ᚐ҉ᚐ \n';
+    txt += '\n\n> ♡⃝𝒴𝓊𝓚𝒾_𝒮𝓊𝑜𝓊-𝐵𝑜𝓉ᚐ҉ᚐ \n';
 
-    await conn.sendButton2(m.chat, txt, '. ', thumbnail, [
-        ['MP3🎵', `${usedPrefix}ytmp3 ${url}`],      
-        ['MP4📹', `${usedPrefix}ytmp4 ${url}`], 
-        ['MP4DOC📹📄', `${usedPrefix}ytmp4doc ${url}`], 
-        ['MÁS VÍDEOS', `${usedPrefix}ytsearch ${url}`]
-        ], null, [['ℂ𝕒𝕟𝕒𝕝', 'https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T']], m);
+    await conn.sendMessage(m.chat, {
+        text: txt,
+        buttons: [
+            { buttonId: `${usedPrefix}ytmp3 ${url}`, buttonText: { displayText: 'MP3🎵' }, type: 1 },
+            { buttonId: `${usedPrefix}ytmp4 ${url}`, buttonText: { displayText: 'MP4📹' }, type: 1 },
+            { buttonId: `${usedPrefix}ytmp4doc ${url}`, buttonText: { displayText: 'MP4DOC📹📄' }, type: 1 },
+            { buttonId: `${usedPrefix}ytsearch ${url}`, buttonText: { displayText: 'MÁS VÍDEOS' }, type: 1 }
+        ],
+        footer: 'ℂ𝕒𝕟𝕒𝕝',
+        header: { text: 'Título' },
+        image: { url: thumbnail }
+    });
 
     await m.react('✅');
 };
 
 handler.help = ['play6'];
-handler.tags = ['descargas'] 
-handler.group = false
-//handler.limit = 2
-handler.command = ['play6',];
+handler.tags = ['descargas'];
+handler.group = false;
+handler.command = ['play6'];
 
 export default handler;
