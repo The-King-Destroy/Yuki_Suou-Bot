73fc4b47c15b3c30ec03013b919d9f2baa0fb448
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-07 01:39:32 -0400

    Update descargas-play_6.js

--- plugins/descargas-play_6.js
+++ plugins/descargas-play_6.js
@@ -13,25 +13,27 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
 
     let { title, thumbnail, ago, timestamp, views, videoId, url } = play;
 
-    let txt = '⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝔻𝔼𝕊ℂ𝔸ℝ𝔾𝔸𝕊໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪\n';
-    txt += '🍂⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝕐𝕦𝕜𝕚 𝕊𝕦𝕠𝕦໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪̇ \n';
+    let txt = `⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝔻𝔼𝕊ℂ𝔸ℝ𝔾𝔸𝕊໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪\n`;
+    txt += `🍂⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝕐𝕦𝕜𝕚 𝕊𝕦𝕠𝕦໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪̇ \n`;
     txt += `> *𝕋í𝕥𝕦𝕝𝕠* : _${title}_\n`;
     txt += `> *𝔸𝕦𝕥𝕠𝕣(𝕒)* : _${ago}_\n`;
     txt += `> *𝔻𝕦𝕣𝕒𝕔𝕚ó𝕟* : _${timestamp}_\n`;
     txt += `> *𝕍𝕚𝕤𝕥𝕒𝕤* : _${views.toLocaleString()}_\n`;
     txt += `> *𝔼𝕟𝕝𝕒𝕔𝕖* : _https://www.youtube.com/watch?v=${videoId}_\n`;
-    txt += '\n\n> ♡⃝𝒴𝓊𝓚𝒾_𝒮𝓊𝑜𝓊-𝐵𝑜𝓉ᚐ҉ᚐ \n';
+    txt += `\n\n> ♡⃝𝒴𝓊𝓚𝒾_𝒮𝓊𝑜𝓊-𝐵𝑜𝓉ᚐ҉ᚐ \n`;
+
+    const buttons = [
+        { buttonId: `${usedPrefix}ytmp3 ${url}`, buttonText: { displayText: 'MP3🎵' }, type: 1 },
+        { buttonId: `${usedPrefix}ytmp4 ${url}`, buttonText: { displayText: 'MP4📹' }, type: 1 },
+        { buttonId: `${usedPrefix}ytmp4doc ${url}`, buttonText: { displayText: 'MP4DOC📹📄' }, type: 1 },
+        { buttonId: `${usedPrefix}ytsearch ${url}`, buttonText: { displayText: 'MÁS VÍDEOS' }, type: 1 }
+    ];
 
     await conn.sendMessage(m.chat, {
         text: txt,
-        buttons: [
-            { buttonId: `${usedPrefix}ytmp3 ${url}`, buttonText: { displayText: 'MP3🎵' }, type: 1 },
-            { buttonId: `${usedPrefix}ytmp4 ${url}`, buttonText: { displayText: 'MP4📹' }, type: 1 },
-            { buttonId: `${usedPrefix}ytmp4doc ${url}`, buttonText: { displayText: 'MP4DOC📹📄' }, type: 1 },
-            { buttonId: `${usedPrefix}ytsearch ${url}`, buttonText: { displayText: 'MÁS VÍDEOS' }, type: 1 }
-        ],
         footer: 'ℂ𝕒𝕟𝕒𝕝',
-        header: { text: 'Título' },
+        buttons: buttons,
+        headerType: 1,
         image: { url: thumbnail }
     });
 
