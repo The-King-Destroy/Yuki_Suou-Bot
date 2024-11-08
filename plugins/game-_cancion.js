02a903b43bdee24305c852f81e951666cf9c8935
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-08 01:27:09 -0400

    Update game-_cancion.js

--- plugins/game-_cancion.js
+++ plugins/game-_cancion.js
@@ -1,37 +1,23 @@
 import similarity from 'similarity';
 const threshold = 0.72;
-
-const answerHandler = {
+const handler = {
   async before(m) {
     const id = m.chat;
-
-    // Verifica que el mensaje sea una respuesta a la pregunta de adivinanza
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/ADIVINA EL TITULO DE LA CANCION/i.test(m.quoted.text)) return true;
-
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/ADIVINA EL TITULO DE LA CANCION/i.test(m.quoted.text)) return !0;
     this.tebaklagu = this.tebaklagu ? this.tebaklagu : {};
     if (!(id in this.tebaklagu)) return m.reply('El juego ha terminado');
-
     if (m.quoted.id == this.tebaklagu[id][0].id) {
-      const json = this.tebaklagu[id][1];
-      
-      // Compara la respuesta del usuario con la respuesta correcta
-      const userAnswer = m.text.toLowerCase().trim();
-      const correctAnswer = json.jawaban.toLowerCase().trim();
-
-      if (userAnswer === correctAnswer) {
+      const json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]));
+      if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
         global.db.data.users[m.sender].cookies += this.tebaklagu[id][2];
-        m.reply(`✅ Correcto!\n+${this.tebaklagu[id][2]} Cookies`);
+        m.reply(`✅Correcto!\n+${this.tebaklagu[id][2]} Cookies`);
         clearTimeout(this.tebaklagu[id][3]);
         delete this.tebaklagu[id];
-      } else if (similarity(userAnswer, correctAnswer) >= threshold) {
-        m.reply(`Casi!`);
-      } else {
-        m.reply(`❌ Incorrecto!`);
-      }
+      } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Casii!`);
+      else m.reply(`❌Incorrecto!`);
     }
-    return true;
+    return !0;
   },
   exp: 0,
 };
-
-export default answerHandler;
+export default handler;
