cc9448fd673ac737b4f5daa10810ce711035ec24
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-09 11:30:50 -0400

    Update +18-porno.js

--- plugins/+18-porno.js
+++ plugins/+18-porno.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-const icons = 'https://qu.ax/tHKwZ.jpg'; // URL de tu ícono
 const handler = async (m, {command, conn, usedPrefix}) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
     }
-conn.reply(m.chat, `✨ *Enviando ${command}...*`, m, {
+conn.reply(m.chat, `🚀 *Enviando ${command}...*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
@@ -12,11 +11,11 @@ previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
 const res = (await axios.get(`https://raw.githubusercontent.com/DevDiegoxyz/YaemoriBot-MD/master/src/JSON/${command}.json`)).data;
 const haha = await res[Math.floor(res.length * Math.random())];
-conn.sendFile(m.chat, haha, 'error.jpg', `🌟 *${command}*`, m, null, rcanal);
+conn.sendFile(m.chat, haha, 'error.jpg', `🔥 *${command}*`, m, null, rcanal);
 };
 handler.help = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwglass', 'nsfworgy', 'yuri', 'yuri2', 'yaoi', 'yaoi2', 'panties', 'tetas', 'booty', 'ecchi', 'furro', 'hentai', 'trapito', 'imagenlesbians', 'pene', 'porno', 'randomxxx', 'pechos'];
 handler.command = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwglass', 'nsfworgy', 'yuri', 'yuri2', 'yaoi', 'yaoi2', 'panties', 'tetas', 'booty', 'ecchi', 'furro', 'hentai', 'trapito', 'imagenlesbians', 'pene', 'porno', 'randomxxx', 'pechos'];
 handler.tags = ['nsfw'];
-handler.group = false;
+//handler.group = true;
 handler.register = true
 export default handler;
