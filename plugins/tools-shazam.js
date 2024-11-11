f6d0869cf0d0114beeff3aa75ff64719c24eaeb8
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-11 16:11:37 -0400

    Update tools-shazam.js

--- plugins/tools-shazam.js
+++ plugins/tools-shazam.js
@@ -30,5 +30,5 @@ fs.unlinkSync(`./tmp/${m.sender}.${ext}`)
 m.reply(txt)
 } else throw '💭 Responda A Un Audio O Video'
 }
-handler.command = ['shazam', 'whatmusic']
+handler.command = ['quemusica', 'quemusicaes']
 export default handler
