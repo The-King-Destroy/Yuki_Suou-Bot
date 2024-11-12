a975d273d468402bf0f33cbfdb436a0bb7343ec1
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-11 21:20:00 -0400

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -6,7 +6,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
     let userId = m.messageStubParameters[0];
 
     const welcomeImage = 'https://files.catbox.moe/ibij1z.jpg'; // Imagen de bienvenida
-    const goodbyeImage = 'https://qu.ax/iSUCQ.jpg'; // Imagen de despedida
+    const goodbyeImage = 'https://files.catbox.moe/r44rha.jpg'; // Imagen de despedida
 
     let pp;
     try {
