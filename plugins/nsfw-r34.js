aeb071ab4cb9cc1ce709338ade3770fa71cd15c9
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-20 13:42:50 -0400

    Update nsfw-r34.js

--- plugins/nsfw-r34.js
+++ plugins/nsfw-r34.js
@@ -3,7 +3,7 @@ const handler = async (m, { conn, args, usedPrefix }) => {
 if (!args[0]) {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
-    }
+}
 await conn.reply(m.chat, '🌹 Ingresa el nombre de la imágen que estas buscando', m, rcanal);
 return;
 }
