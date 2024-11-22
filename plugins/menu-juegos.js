7a882e5c58eb326a8222890abe0dfeb1022a67d9
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-21 21:18:00 -0400

    Update menu-juegos.js

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -54,6 +54,10 @@ const document = doc[Math.floor(Math.random() * doc.length)];
 ┃✧ _!ahorcado_
 ┃✧ _!math <mode>_
 ┃✧ _!mayorque_
+┃✧ _!aplauso_
+┃✧ _!marron_
+┃✧ _!suicide_
+┃✧ _!chupalo_
 ┃✧ _!ppt_
 ┃✧ _!pvp_
 ┃✧ _!reto_
@@ -88,10 +92,9 @@ const document = doc[Math.floor(Math.random() * doc.length)];
 ┃✧ _!estupido_
 ┃✧ _!stupid_
 ┃✧ _!top *<texto>*_
-┃✧ _!formartrio @usuario1 @usuario2_
+┃✧ _!formartrio @usuario|@usuario_
 ┃✧ _!waste @user_
 ┃✧ _!zodiac *2002 02 25*_
-┃✧ _!apostar_
 ┗━━━━━━━━━━━━━━━━━⪩`.trim();     if (m.isGroup) { 
  // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
