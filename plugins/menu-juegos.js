c77259d1042b713090e5dc670b355b5cfe869e9d
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-21 21:12:47 -0400

    Update menu-juegos.js

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -25,76 +25,74 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 
 
 const document = doc[Math.floor(Math.random() * doc.length)];
- const str = `╭━━━━･❪ *DIVERSIONES* ❫ ･━━━━╮
-┃✨ _!rw_
-┃🌟 _!ob_
-┃✨ _!robarp *<nombre / @tag>*_
-┃🌟 _!toprw_
-┃✨ _!darp *<nombre / @tag>*_
-┃🌟 _!ruleta_
-┃✨ _!violar *<responder / @tag>*_
-┃🌟 _!sexo *<responder / @tag>*_
-┃✨ _!nalguear *<responder / @tag>*_
-┃🌟 _!anal *<responder / @tag>*_
-┃✨ _!kiss *<responder / @tag>*_
-┃🌟 _!69 *<responder / @tag>*_
-┃✨ _!agarrartetas *<responder / @tag>*_
-┃🌟 _!blush *<responder / @tag>*_
-┃✨ _!chupartetas *<responder / @tag>*_
-┃🌟 _!nalguear *<responder / @tag>*_
-┃✨ _!dance *<responder / @tag>*_
-┃🌟 _!hug *<responder / @tag>*_
-┃✨ _!laungh *<responder / @tag>*_
-┃🌟 _!love *<responder / @tag>*_
-┃✨ _!mamada *<responder / @tag>*_
-┃🌟 _!pat *<responder / @tag>*_
-┃✨ _!pout *<responder / @tag>*_
-┃🌟 _!punch *<responder / @tag>*_
-┃✨ _!nalguear *<responder / @tag>*_
-┃🌟 _!rusa *<responder / @tag>*_
-┃✨ _!sad *<responder / @tag>*_
-┃🌟 _!asustado *<responder / @tag>*_
-┃✨ _!timido *<responder / @tag>*_
-┃🌟 _!slap *<responder / @tag>*_
-┃✨ _!sleep *<responder / @tag>*_
-┃🌟 _!yuri *<responder / @tag>*_
-┃✨ _!mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
-┃🌟 _!fake *<texto1> <@tag> <texto2>*_
-┃✨ _!ppt *<papel / tijera /piedra>*_
-┃🌟 _!prostituto *<nombre / @tag>*_
-┃✨ _!prostituta *<nombre / @tag>*_
-┃🌟 _!gay2 *<nombre / @tag>*_
-┃✨ _!lesbiana *<nombre / @tag>*_
-┃🌟 _!pajero *<nombre / @tag>*_
-┃✨ _!pajera *<nombre / @tag>*_
-┃🌟 _!follar *<nombre / @tag>*_
-┃✨ _!puto *<nombre / @tag>*_
-┃🌟 _!puta *<nombre / @tag>*_
-┃✨ _!manco *<nombre / @tag>*_
-┃🌟 _!manca *<nombre / @tag>*_
-┃✨ _!rata *<nombre / @tag>*_
-┃🌟 _!love *<nombre / @tag>*_
-┃✨ _!doxear *<nombre / @tag>*_
-┃🌟 _!pregunta *<texto>*_
-┃✨ _!suitpvp *<@tag>*_
-┃🌟 _!slot *<apuesta>*_
-┃✨ _!ttt *<nombre sala>*_
-┃🌟 _!delttt_
-┃✨ _!acertijo_
-┃🌟 _!personalidad *<nombre / @tag>*_
-┃✨ _!simi *<texto>*_
-┃🌟 _!top *<texto>*_
-┃✨ _!topgays_
-┃🌟 _!topotakus_
-┃✨ _!formarpareja_
-┃🌟 _!verdad_
-┃✨ _!crime_
-┃🌟 _!reto_
-┃✨ _!cancion_
-┃🌟 _!pista_
-┃✨ _!akinator_
-┃🌟 _!wordfind_
-╰•°• ✾ •°••°• ✾ •°••°• ✾ •°•°••°• ✾ •°•`.trim();     if (m.isGroup) { 
+ const str = `┏━━⪩「 𝕯𝖎𝖛𝖊𝖗𝖘𝖎ó𝖓 乂 𝕵𝖚𝖊𝖌𝖔𝖘 」⪨
+┃✧ _!amistad_
+┃✧ _!character_
+┃✧ _!darrw_
+┃✧ _!obtenidos_
+┃✧ _!c_
+┃✧ _!robarpersonaje_
+┃✧ _!rw_
+┃✧ _!toprw_
+┃✧ _!gay <@tag> | <nombre>_
+┃✧ _!lesbiana <@tag> | <nombre>_
+┃✧ _!pajero <@tag> | <nombre>_
+┃✧ _!pajera <@tag> | <nombre>_
+┃✧ _!puto <@tag> | <nombre>_
+┃✧ _!puta <@tag> | <nombre>_
+┃✧ _!manco <@tag> | <nombre>_
+┃✧ _!manca <@tag> | <nombre>_
+┃✧ _!rata <@tag> | <nombre>_
+┃✧ _!prostituta <@tag> | <nombre>_
+┃✧ _!prostituto <@tag> | <nombre>_
+┃✧ _!casarse @tag_
+┃✧ _!acertijo_
+┃✧ _!paises_
+┃✧ _!cancion_
+┃✧ _!pista_
+┃✧ _!peliculas_
+┃✧ _!ahorcado_
+┃✧ _!math <mode>_
+┃✧ _!mayorque_
+┃✧ _!ppt_
+┃✧ _!pvp_
+┃✧ _!reto_
+┃✧ _!sopa_
+┃✧ _!buscarpalabras_
+┃✧ _!verdad_
+┃✧ _!consejo_
+┃✧ _!divorciarse @tag_
+┃✧ _!doxear_
+┃✧ _!doxxing <nombre> | <@tag>_
+┃✧ _!formarpareja_
+┃✧ _!formarpareja5_
+┃✧ _!horny_
+┃✧ _!hornycard_
+┃✧ _!huevo @user_
+┃✧ _!iqtest_
+┃✧ _!marica_
+┃✧ _!meme_
+┃✧ _!minovia @user_
+┃✧ _!morse *<encode|decode>*_
+┃✧ _!nombreninja *<texto>*_
+┃✧ _!pajeame_
+┃✧ _!personalidad_
+┃✧ _!piropo_
+┃✧ _!pokedex *<pokemon>*_
+┃✧ _!pregunta_
+┃✧ _!ship_
+┃✧ _!love_
+┃✧ _!simpcard_
+┃✧ _!sorteo_
+┃✧ _!itssostupid_
+┃✧ _!estupido_
+┃✧ _!stupid_
+┃✧ _!top *<texto>*_
+┃✧ _!formartrio @usuario1 @usuario2_
+┃✧ _!waste @user_
+┃✧ _!zodiac *2002 02 25*_
+┃✧ _!apostar_
+┗━━━━━━━━━━━━━━━━━⪩`.trim();     if (m.isGroup) { 
  // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
