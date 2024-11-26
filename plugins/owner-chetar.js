8493da4eb3347190be3ddfcb6a26a4d4bcaf6ef2
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-26 14:05:43 -0400

    Update owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -1,16 +1,14 @@
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `🤟 *@${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
+        conn.sendMessage(m.chat, {text: `🌸 *@${m.sender.split('@')[0]} Pos Ahora tienes recursos ilimitados :v*`, mentions: [m.sender]}, {quoted: fkontak});
       global.db.data.users[m.sender].yukicoins = Infinity;
-    global.db.data.users[m.sender].cookies= Infinity;
+    global.db.data.users[m.sender].cookies = Infinity;
   global.db.data.users[m.sender].level = Infinity;
  global.db.data.users[m.sender].exp = Infinity;
-global.db.data.users[m.sender].diamantes = Infinity;
-global.db.data.users[m.sender].personajes = Infinity;
 };
-handler.help = ['chetar'];
+handler.help = ['cheat'];
 handler.tags = ['owner'];
-handler.command = ['ilimitado','infiniy','chetar'];
+handler.command = ['ilimitado', 'infiniy', 'chetar'];
 handler.rowner = true;
 handler.fail = null;
 export default handler;
