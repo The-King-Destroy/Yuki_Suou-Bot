6fa9de38b0c67325a00eaa46aa518f9171e4c80a
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-26 16:08:36 -0400

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -1,47 +1,27 @@
-import axios from 'axios';
-
-const handler = async (m, { conn, command, args }) => {
-    const text = args.join(' ');
-
-    // Validar que se haya ingresado un texto de búsqueda
-    if (!text) {
-        return conn.reply(m.chat, '🌸 Ingresa lo que deseas buscar junto al comando.', m);
-    }
-
-    await m.react('🕓'); // Reacción de espera
-
-    const img = 'https://i.ibb.co/P5kZNFF/file.jpg'; // Imagen para enviar
-    const url = `https://eliasar-yt-api.vercel.app/api/google?query=colombia ${encodeURIComponent(text)}`;
-
-    try {
-        // Realizar la solicitud a la API de búsqueda
-        const response = await axios.get(url);
-        const results = response.data;
-
-        // Verificar si hay resultados
-        if (!results || results.length === 0) {
-            return conn.reply(m.chat, '🚫 No se encontraron resultados para tu búsqueda.', m);
-        }
-
-        // Construir el texto de respuesta
-        let responseText = `*乂  S E A R C H  -  G O O G L E*\n\n`;
-        results.forEach(result => {
-            responseText += `*${result.titulo}*\n${result.url}\n${result.descripcion}\n\n`;
-        });
-
-        // Enviar la respuesta al chat
-        await conn.sendFile(m.chat, img, 'thumbnail.jpg', responseText, m);
-        await m.react('✅'); // Reacción de éxito
-    } catch (error) {
-        console.error('Error al realizar la búsqueda:', error);
-        conn.reply(m.chat, '🚫 Ocurrió un error al realizar la búsqueda. Intenta de nuevo.', m);
-    }
-};
-
-// Configuración del comando
-handler.help = ['google *<texto>*'];
-handler.tags = ['buscador'];
-handler.command = /^google?$/i;
-handler.register = true;
-
-export default handler;
+import {googleIt} from '@bochilteam/scraper'
+import google from 'google-it'
+import axios from 'axios'
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+const fetch = (await import('node-fetch')).default;
+const text = args.join` `
+if (!text) return conn.reply(m.chat, '🌸 *Ingresa lo que deseas buscar en Google.*', m, rcanal)
+conn.reply(m.chat, `🌸 *Buscando Su Información...*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+const url = 'https://google.com/search?q=' + encodeURIComponent(text)
+google({'query': text}).then(res => {
+let teks = `🌷 *Resultado de* : ${text}\n\n`
+for (let g of res) {
+teks += `⚜️ *Titulo ∙* ${g.title}\n📚 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
+}
+conn.reply(m.chat, teks, m, rcanal)
+})
+}
+handler.help = ['google <búsqueda>']
+handler.tags = ['buscador']
+handler.command = ['google']
+handler.register = true 
+export default handler
