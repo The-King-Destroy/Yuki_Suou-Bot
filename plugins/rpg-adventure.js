b980fd52c1c2cb195b3f893734b4f004c44699c7
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-27 01:48:59 -0400

    Update rpg-adventure.js

--- plugins/rpg-adventure.js
+++ plugins/rpg-adventure.js
@@ -1,124 +1,78 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
+import db from '../lib/database.js';
 
-const cooldown = 1500000 // 25 minutos
+const cooldown = 1500000; // 25 minutos
 let handler = async (m, { usedPrefix, conn }) => {
     try {
-        let fkontak = {
-            "key": {
-                "participants": "0@s.whatsapp.net",
-                "remoteJid": "status@broadcast",
-                "fromMe": false,
-                "id": "Halo"
-            },
-            "message": {
-                "contactMessage": {
-                    "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-                }
-            },
-            "participant": "0@s.whatsapp.net"
+        let user = global.db.data.users[m.sender];
+
+        if (!user) {
+            return conn.reply(m.chat, '👤 El usuario no se encuentra en la base de Datos.', m);
         }
-
-        let ct = ['AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'XK', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'AN', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RS', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'CS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SZ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'XT', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW']
-
-        let ke = await fetch(`https://api.worldbank.org/v2/country/${ct.getRandom()}?format=json`)
-        let kt = await ke.json()
-        let imgr = flaaa.getRandom()
-        let user = global.db.data.users[m.sender]
-        let timers = (cooldown - (new Date - user.lastadventure))
 
         if (user.health < 80) {
-            return conn.sendMessage(m.chat, `${htki} 𝙱𝙰𝙹𝙰 𝚂𝙰𝙻𝚄𝙳 ${htka}\n𝚃𝚄 𝚂𝙰𝙻𝚄𝙳 💔 𝙴𝚂𝚃𝙰 𝙿𝙾𝚁 𝙳𝙴𝙱𝙰𝙹𝙾 𝙳𝙴 *80!!*\n𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙲𝚄𝚁𝙰𝚃𝙴 𝙿𝚁𝙸𝙼𝙴𝚁𝙾 𝙿𝙰𝚁𝙰 𝙰𝚅𝙴𝙽𝚃𝚄𝚁𝙰𝚁 𝙳𝙴 𝙽𝚄𝙴𝚅𝙾.\n\nUsa el comando: .heal`, imgr + 'MALA SALUD', fkontak, m)
+            return conn.reply(m.chat, '💔 No tienes suficiente salud para aventurarte. Usa el comando .heal para curarte.', m);
         }
 
-        if (new Date - user.lastadventure <= cooldown) {
-            return conn.sendMessage(m.chat, `${htki} 𝙳𝙴𝚂𝙲𝙰𝙽𝚂𝙰𝙽𝙳𝙾 ${htka}\n𝚈𝙰 𝙰𝚅𝙴𝙽𝚃𝚄𝚁𝙰𝚂𝚝𝙴 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙴𝚂𝙿𝙴𝚁𝙰 𝙷𝙰𝚂𝚃𝙰 𝚀𝚄𝙴 𝚃𝙴𝚁𝙼𝙸𝙽𝙴 𝙴𝙻 𝚃𝙸𝙴𝙼𝙿𝙾 𝙳𝙴 𝙳𝙴𝚂𝙲𝙰𝙽𝚂𝙾\n\n⏱️ ${timers.toTimeString()}\n\nUsa el comando: .daily`, imgr + 'DESCANSANDO', fkontak, m)
+        if (new Date() - user.lastAdventure <= cooldown) {
+            let timeLeft = cooldown - (new Date() - user.lastAdventure);
+            return conn.reply(m.chat, `⏳ Estás en cooldown. Espera ${Math.ceil(timeLeft / 60000)} minutos antes de aventurarte de nuevo.`, m);
         }
 
-        const rewards = reward(user)
-        let text = `🛫 𝙴𝚂𝚃𝙰𝚜 𝙰𝚅𝙴𝙽𝚃𝚄𝚁𝙰𝙽𝙳𝙾 𝙴𝙽  *» ${kt[1][0].name}*\n\n${cmenut}\n${cmenub} *ID:* ${kt[1][0].id}\n${cmenub} *CIUDAD:* ${kt[1][0].capitalCity}\n${cmenub} *LONGITUD:* ${kt[1][0].longitude}\n${cmenub} *LATITUD:* ${kt[1][0].latitude}\n${cmenuf}\n\n🏞️ 𝙰𝚅𝙴𝙽𝚃𝚄𝚁𝙰 𝙵𝙸𝙽𝙰𝙻𝙸𝚉𝙰𝙳𝙰\n${cmenua}`
+        let ct = ['AF','AX','AL','DZ','AS','AD','AO','AI','AQ','AG','AR','AM','AW','AU','AT','AZ','BS','BH','BD','BB','BY','BE','BZ','BJ','BM','BT','BO','BQ','BA','BW','BV','BR','IO','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CN','CX','CC','CO','KM','CG','CD','CK','CR','CI','HR','CU','CW','CY','CZ','DK','DJ','DM','DO','EC','EG','SV','GQ','ER','EE','ET','FK','FO','FJ','FI','FR','GF','PF','TF','GA','GM','GE','DE','GH','GI','GR','GL','GD','GP','GU','GT','GG','GN','GW','GY','HT','HM','VA','HN','HK','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','JM','JP','JE','JO','KZ','KE','KI','KP','KR','XK','KW','KG','LA','LV','LB','LS','LR','LY','LI','LT','LU','MO','MK','MG','MW','MY','MV','ML','MT','MH','MQ','MR','MU','YT','MX','FM','MD','MC','MN','ME','MS','MA','MZ','MM','NA','NR','NP','NL','AN','NC','NZ','NI','NE','NG','NU','NF','MP','NO','OM','PK','PW','PS','PA','PG','PY','PE','PH','PN','PL','PT','PR','QA','RS','RE','RO','RU','RW','BL','SH','KN','LC','MF','PM','VC','WS','SM','ST','SA','SN','CS','SC','SL','SG','SX','SK','SI','SB','SO','ZA','GS','SS','ES','LK','SD','SR','SJ','SZ','SE','CH','SY','TW','TJ','TZ','TH','TL','TG','TK','TO','TT','TN','TR','XT','TM','TC','TV','UG','UA','AE','GB','US','UM','UY','UZ','VU','VE','VN','VG','VI','WF','EH','YE','ZM','ZW'];
 
-        for (const lost in rewards.lost) if (user[lost]) {
-            const total = rewards.lost[lost].getRandom()
-            user[lost] -= total * 1
-            if (total) text += `\n${global.rpg.emoticon(lost)} ${total}`
+        let randomCountryCode = ct[Math.floor(Math.random() * ct.length)];
+        let response = await fetch(`https://api.worldbank.org/v2/country/${randomCountryCode}?format=json`);
+        let kt = await response.json();
+
+        if (!kt || !kt[1] || !kt[1][0]) {
+            return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝙰𝙻 𝙾𝙱𝚃𝙴𝙽𝙴𝚁 𝙳𝙰𝚃𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙸!*', m);
         }
 
-        text += '\n\n✨ 𝚁𝙴𝙲𝙾𝙼𝙿𝙴𝚂𝙰𝚂 𝙳𝙴 𝙻𝙰 𝙰𝚅𝙴𝙽𝚃𝚄𝚁𝙰`
-        for (const rewardItem in rewards.reward) if (rewardItem in user) {
-            const total = rewards.reward[rewardItem].getRandom()
-            user[rewardItem] += total * 1
-            if (total) text += `\n» ${global.rpg.emoticon(rewardItem)} ${total}`
-        }
+        let city = {
+            id: kt[1][0].id,
+            name: kt[1][0].name,
+            capitalCity: kt[1][0].capitalCity || 'Sin Capital',
+            longitude: kt[1][0].longitude || '0',
+            latitude: kt[1][0].latitude || '0'
+        };
 
-        conn.sendMessage(m.chat, `${htki} 𝙰𝚅𝙴𝙽𝚃𝚄𝚁𝙰 ${htka}`, text.trim(), `https://static-maps.yandex.ru/1.x/?lang=id-ID&ll=${kt[1][0].longitude},${kt[1][0].latitude}&z=12&l=map&size=600,300`, fkontak, m)
-        user.lastadventure = new Date * 1
+        const rewards = {
+            yenes: Math.floor(Math.random() * 16) + 5,
+            exp: Math.floor(Math.random() * 16) + 5,
+            emerald: Math.floor(Math.random() * 16) + 5,
+            diamonds: Math.floor(Math.random() * 16) + 5,
+            potions: Math.floor(Math.random() * 16) + 5,
+            healthLost: Math.floor(Math.random() * 20)
+        };
+
+        user.yenes += rewards.yenes;
+        user.exp += rewards.exp;
+        user.emerald += rewards.emerald;
+        user.diamond += rewards.diamonds;
+        user.potion += rewards.potions;
+        user.health -= rewards.healthLost;
+        user.lastAdventure = new Date();
+
+        let text = `🛫 𝙴𝚂𝚃𝙰𝚜 𝙰𝚅𝙴𝙽𝚃𝚄𝚁𝙰𝙽𝙳𝙾 𝙴𝙽  *» ${city.name}*\n\n` +
+                   `🏞️ *Aventura Finalizada* 🏞️\n` +
+                   `┋ 💰 *Yenes Ganados:* ${rewards.yenes}\n` +
+                   `┋ ✨ *Experiencia Ganada:* ${rewards.exp}\n` +
+                   `┋ 💎 *Esmeraldas Encontradas:* ${rewards.emerald}\n` +
+                   `┋ 💎 *Diamantes Encontrados:* ${rewards.diamonds}\n` +
+                   `┋ 🧪 *Pociones Ganadas:* ${rewards.potions}\n` +
+                   `┋ ❤️ *Salud Perdida:* ${rewards.healthLost}\n` +
+                   `┋ ❤️ *Salud Actual:* ${user.health}`;
+
+        await conn.reply(m.chat, text, m);
     } catch {
-        conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝙻𝙾 𝙳𝙴 𝙽𝚄𝙴𝚅𝙾, 𝚂𝙴𝙶𝚄𝚁𝙾 𝙻𝙰 𝙰𝙿𝙸 𝙽𝙾 𝙶𝙴𝙽𝙴𝚁𝙾 𝙻𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*', m)
+        conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝙻𝙾 𝙳𝙴 𝙽𝚄𝙴𝚅𝙾, 𝚂𝙴𝙶𝚄𝚁𝙾 𝙻𝙰 𝙰𝙿𝙸 𝙽𝙾 𝙶𝙴𝙽𝙴𝚁𝙾 𝙻𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*', m);
     }
-}
-handler.help = ['adventure']
-handler.tags = ['rpg']
-handler.command = /^(adventure|adv|aventura|aventurar)$/i
+};
 
-handler.cooldown = cooldown
-handler.disabled = false
+handler.help = ['aventura', 'adventure'];
+handler.tags = ['rpg'];
+handler.command = /^(aventura|adventure)$/i;
+handler.cooldown = cooldown;
 
-export default handler
-
-function reward(user = {}) {
-    let rewards = {
-        reward: {
-            money: 400,
-            exp: 300,
-            trash: 150,
-            potion: 3,
-            rock: 2,
-            joincount: 2,
-            wood: 3,
-            string: 2,
-            common: 2 * (user.dog && (user.dog > 2 ? 2 : user.dog) * 1.2 || 1),
-            uncoommon: [0, 0, 0, 1, 0].concat(
-                new Array(5 - (
-                    (user.dog > 2 && user.dog < 6 && user.dog) || (user.dog > 5 && 5) || 2
-                )).fill(0)
-            ),
-            mythic: [0, 0, 0, 0, 0, 1, 0, 0, 0].concat(
-                new Array(8 - (
-                    (user.dog > 5 && user.dog < 8 && user.dog) || (user.dog > 7 && 8) || 3
-                )).fill(0)
-            ),
-            legendary: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0].concat(
-                new Array(10 - (
-                    (user.dog > 8 && user.dog) || 4
-                )).fill(0)
-            ),
-            cat: [0, 1, 0, 0, 0],
-            centaur: [0, 1, 0, 0, 0],
-            dog: [0, 1, 0, 0, 0],
-            dragon: [0, 1, 0, 0, 0],
-            emerald: [0, 1, 0, 0, 0],
-            fox: [0, 1, 0, 0, 0],
-            griffin: [0, 1, 0, 0, 0],
-            horse: [0, 1, 0, 0, 0],
-            kyubi: [0, 1, 0, 0, 0],
-            lion: [0, 1, 0, 0, 0],
-            pet: [0, 1, 0, 0, 0],
-            phonix: [0, 1, 0, 0, 0],
-            rhinoceros: [0, 1, 0, 0, 0],
-            robo: [0, 1, 0, 0, 0],
-            wolf: [0, 1, 0, 0, 0],
-            iron: [0, 0, 0, 1, 0, 0],
-            gold: [0, 0, 0, 0, 0, 1, 0],
-            diamond: [0, 0, 0, 0, 0, 0, 1, 0].concat(
-                new Array(5 - (
-                    (user.fox < 6 && user.fox) || (user.fox > 5 && 5) || 0
-                )).fill(0)
-            ),
-        },
-        lost: {
-            health: 101 - user.cat * 4,
-            armordurability: (15 - user.armor) * 7
-        }
-    }
-    return rewards
-}
+export default handler;
