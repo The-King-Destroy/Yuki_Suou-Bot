50ec859ce04bb71403b335efb752c394780ef563
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-28 06:48:25 -0400

    Update game-adivina_paises.js

--- plugins/game-adivina_paises.js
+++ plugins/game-adivina_paises.js
@@ -1,32 +1,43 @@
 import fs from 'fs';
+
 const timeout = 60000;
 const yenes = 10;
-const handler = async (m, {conn, usedPrefix}) => {
-  conn.tekateki = conn.tekateki ? conn.tekateki : {};
+
+const handler = async (m, { conn, usedPrefix }) => {
+  conn.tekateki = conn.tekateki || {};
   const id = m.chat;
+
   if (id in conn.tekateki) {
     conn.reply(m.chat, 'Todavía hay alguien jugando aquí, espera hasta que termine', conn.tekateki[id][0]);
     throw false;
   }
-  const tekateki = JSON.parse(fs.readFileSync(`./src/game/paises.json`));
+
+  const tekateki = JSON.parse(fs.readFileSync('./src/game/paises.json'));
   const json = tekateki[Math.floor(Math.random() * tekateki.length)];
-  const _clue = json.response;
-  const clue = _clue.replace(/[A-Za-z]/g, '_');
+  const clue = json.response.replace(/[A-Za-z]/g, '_');
+
   const caption = `
 ⷮ🌏 *\`ADIVINA EL PAIS\`* 🌎
-que pais es: *${json.question}*
+¿qué país es: *${json.question}*?
 
 ⏱️ *Tiempo:* ${(timeout / 1000).toFixed(2)} Segundos
 🎁 *Premio:* *+${yenes}* Yenes 💴`.trim();
+
   conn.tekateki[id] = [
-    await conn.reply(m.chat, caption, m, fake), json,
+    await conn.reply(m.chat, caption, m),
+    json,
     yenes,
     setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `⌛ Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
-      delete conn.tekateki[id];
-    }, timeout)];
+      if (conn.tekateki[id]) {
+        await conn.reply(m.chat, `⌛ Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
+        delete conn.tekateki[id];
+      }
+    }, timeout)
+  ];
 };
+
 handler.help = ['paises'];
 handler.tags = ['game'];
 handler.command = /^(paises)$/i;
+
 export default handler;
