e0e42e7ae9a4dbb346fe7dfb2bed769e91bce4c2
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-28 18:23:28 -0400

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -1,37 +1,22 @@
-
+import { googleIt } from '@bochilteam/scraper';
 import axios from 'axios';
-
-let handler = async (m, { conn, command, args }) => {
-    const text = args.join(' ');
-    if (!text) return conn.reply(m.chat, '🌸 *Ingresa lo que deseas buscar en Google.*', m);
-
-    conn.reply(m.chat, `🌸 *Buscando Su Información...*`, m);
-
-    const url = `https://api.ryzendesu.vip/api/search/google?query=${encodeURIComponent(text)}`;
-
-    try {
-        const response = await axios.get(url);
-
-        // Verifica que la respuesta contenga los datos esperados
-        if (!response.data || !Array.isArray(response.data) || response.data.length === 0) {
-            return conn.reply(m.chat, '🌸 *No se encontraron resultados.*', m);
-        }
-
-        let teks = `🌷 *Resultado de* : ${text}\n\n`;
-        response.data.forEach(result => {
-            teks += `⚜️ *Título ∙* ${result.title}\n📚 *Info ∙* ${result.description}\n🔗 *Url ∙* ${result.link}\n\n`;
-        });
-
-        conn.reply(m.chat, teks, m);
-    } catch (error) {
-        console.error('Error al obtener resultados de Google:', error);
-        conn.reply(m.chat, '🌸 *Ocurrió un error al buscar la información.*', m);
-    }
-}
-
-handler.help = ['google <búsqueda>'];
+const handler = async (m, {conn, command, args}) => {
+  const fetch = (await import('node-fetch')).default;
+  const text = args.join` `;
+  if (!text) return conn.reply(m.chat, '🌸 *Ingresa lo que deseas buscar junto al comando*', m);
+  const url = 'https://google.com/search?q=' + encodeURIComponent(text);
+  const search = await googleIt(text);
+  const msg = search.articles.map(({title, url, description}) => {
+    return `*${title}*\n_${url}_\n_${description}_`;
+  }).join('\n\n');
+  try {
+    const ss = `https://image.thum.io/get/fullpage/${url}`;
+    await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m);
+  } catch {
+    m.reply(msg);
+  }
+};
+handler.help = ['google'].map((v) => v + ' <pencarian>');
 handler.tags = ['buscador'];
-handler.command = ['google'];
-handler.register = true;
-
+handler.command = /^google?$/i;
 export default handler;
