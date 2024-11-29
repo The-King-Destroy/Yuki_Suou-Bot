73e09b8ff3b29e738b7a0f83d325c3d2bb189ee7
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-29 10:18:46 -0400

    Update descargas-soundCloud.js

--- plugins/descargas-soundCloud.js
+++ plugins/descargas-soundCloud.js
@@ -24,7 +24,7 @@ conn.reply('error :v')
 }
     
 handler.help = ['soundclouddl *<link>*']
-handler.yenes = 3
+//handler.yenes = 3
 handler.tags = ['descargas']
 handler.command = ['soundclouddl'];
 
