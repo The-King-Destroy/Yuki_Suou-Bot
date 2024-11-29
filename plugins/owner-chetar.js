e1bac834ce9d59c6ad61324381ed8d0482415825
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-11-29 17:51:46 -0400

    Update owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `🌸 *@${m.sender.split('@')[0]} Pos Ahora tienes recursos ilimitados :v*`, mentions: [m.sender]}, {quoted: fkontak});
+        conn.sendMessage(m.chat, {text: `🌸 @${m.sender.split('@')[0]} Pos Ahora tienes recursos ilimitados`, mentions: [m.sender]}, {quoted: fkontak});
       global.db.data.users[m.sender].money = Infinity;
     global.db.data.users[m.sender].yenes = Infinity;
   global.db.data.users[m.sender].level = Infinity;
