3321934b3bd85494e8897082e8039679019fa669
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-12-01 11:58:06 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -232,6 +232,7 @@ if (!('autoread2' in settings)) settings.autoread2 = false
 if (!('restrict' in settings)) settings.restrict = false
 if (!('antiPrivate' in settings)) settings.antiPrivate = false
 if (!('antiCall' in settings)) settings.antiCall = true
+if (!('frases' in chat)) chat.frases = false
 if (!('antiSpam' in settings)) settings.antiSpam = true
 if (!('modoia' in settings)) settings.modoia = false
 if (!('jadibotmd' in settings)) settings.jadibotmd = false  
@@ -241,6 +242,7 @@ self: false,
 autoread: false,
 autoread2: false,
 restrict: false,
+frases: false,
 antiPrivate: false,
 antiCall: true,
 antiSpam: true,
