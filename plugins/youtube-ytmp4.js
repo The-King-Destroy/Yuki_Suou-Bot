3a1a5b6f831e2ad0a06963dded3f65d42f2e7df5
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-12-04 01:13:27 -0400

    Update youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let limit = 100
 
 let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!args || !args[0]) return star.reply(m.chat, '🌸 *Ingresa el enlace del vídeo de YouTube junto al comando.*\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtube.com/shorts/Dd7rD680ZUU?si=7Rbfvaj4mGnBKzAv`, m, rcanal)
+if (!args || !args[0]) return star.reply(m.chat, '🌸 _*Ingresa el enlace del vídeo de YouTube junto al comando.*_\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtube.com/watch?v=e-xToC9wNl0`, m, rcanal)
 if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(_ => m.react('✖️'))
 let q = args[1] || '720p'
 
@@ -20,14 +20,11 @@ let thumbnail = await yt.thumbnail
 
 let img = await (await fetch(`${thumbnail}`)).buffer()
 if (sizeMB.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga para descargar use el siguiente comando mp4doc.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`*_𔓕꯭  ꯭ ꯭ ꯭ 𓏲꯭֟፝੭ ꯭⌑𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮⌑꯭ 𓏲꯭֟፝੭ ꯭  ꯭ ꯭ ꯭𔓕_*`\n\n'
-       txt += `	>📚   *Titulo* :
-       » ${title}\n`
-       txt += `	>🎞️   *Calidad* : 
-       » ${q}\n`
-       txt += `	> ☁️  *Tamaño* : 
-       » ${size}\n\n`
-       txt += `> 📽️ *Su video en documento se está enviando, espere un momento...*`
+	let txt = '`𔓕꯭  ꯭ ꯭ ꯭ 𓏲꯭֟፝੭ ꯭⌑𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮⌑꯭ 𓏲꯭֟፝੭ ꯭  ꯭ ꯭ ꯭𔓕`\n\n'
+       txt += `	» 📚   *Titulo* : ${title}\n`
+       txt += `	» 🎞️   *Calidad* : ${q}\n`
+       txt += `	» ☁️  *Tamaño* : ${size}\n\n`
+       txt += `> 📽️ *Su video se está enviando, espere un momento...*`
 await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
@@ -40,14 +37,11 @@ let { thumbnail, url } = vid
 
 let img = await (await fetch(`${vid.thumbnail}`)).buffer()  
 if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`*_𔓕꯭  ꯭ ꯭ ꯭ 𓏲꯭֟፝੭ ꯭⌑𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮⌑꯭ 𓏲꯭֟፝੭ ꯭  ꯭ ꯭ ꯭𔓕_*`\n\n'
-       txt += `	>📚   *Titulo* :
-       » ${title}\n`
-       txt += `	>🎞️   *Calidad* : 
-       » ${q}\n`
-       txt += `	> ☁️  *Tamaño* : 
-       » ${size}\n\n`
-       txt += `> 📽️ *Su video en documento se está enviando, espere un momento...*`
+	let txt = '`𔓕꯭  ꯭ ꯭ ꯭ 𓏲꯭֟፝੭ ꯭⌑𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮⌑꯭ 𓏲꯭֟፝੭ ꯭  ꯭ ꯭ ꯭𔓕`\n\n'
+       txt += `	» 📚   *Titulo* : ${title}\n`
+       txt += `	» 🎞️   *Calidad* : ${q}\n`
+       txt += `	» ☁️  *Tamaño* : ${size}\n\n`
+       txt += `> 📽️ *Su video se está enviando, espere un momento...*`
 await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
@@ -58,14 +52,11 @@ let { title, size, dl_url, thumb } = yt
 
 let img = await (await fetch(`${thumb}`)).buffer()
 if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`*_𔓕꯭  ꯭ ꯭ ꯭ 𓏲꯭֟፝੭ ꯭⌑𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮⌑꯭ 𓏲꯭֟፝੭ ꯭  ꯭ ꯭ ꯭𔓕_*`\n\n'
-       txt += `	>📚   *Titulo* :
-       » ${title}\n`
-       txt += `	>🎞️   *Calidad* : 
-       » ${q}\n`
-       txt += `	> ☁️  *Tamaño* : 
-       » ${size}\n\n`
-       txt += `> 📽️ *Su video en documento se está enviando, espere un momento...*`
+	let txt = '`𔓕꯭  ꯭ ꯭ ꯭ 𓏲꯭֟፝੭ ꯭⌑𝐘𝐮𝐤𝐢 𝐒𝐮𝐨𝐮⌑꯭ 𓏲꯭֟፝੭ ꯭  ꯭ ꯭ ꯭𔓕`\n\n'
+       txt += `	» 📚   *Titulo* : ${title}\n`
+       txt += `	» 🎞️   *Calidad* : ${q}\n`
+       txt += `	» ☁️  *Tamaño* : ${size}\n\n`
+       txt += `> 📽️ *Su video se está enviando, espere un momento...*`
 await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await star.sendMessage(m.chat, { video: { url: dl_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 await m.react('✅')
