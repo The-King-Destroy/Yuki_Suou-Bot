484c1116ff24676c9ef953a5e96b2260af7ec2f4
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-12-03 20:25:59 -0400

    Update config-nable.js

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -21,6 +21,7 @@ ${usedPrefix + command} audios
 ${usedPrefix + command} modoadmin
 ${usedPrefix + command} antifake
 ${usedPrefix + command} antibot
+${usedPrefix + command} antiarabes
 
 🌷 *OPCIONES PARA MI PROPIETARIO*
 
@@ -447,6 +448,14 @@ global.dfail('admin', m, conn)
 throw false
 }}
 chat.antiTraba = isEnable
+break
+case 'antiarabes': case 'antiarabe': case 'antiputos':
+if (m.isGroup) {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}}
+chat.antiArabe = isEnable
 break
 case 'simi': case 'chatbot':
 if (m.isGroup) {
