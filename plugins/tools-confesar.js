5ac1232b73c814a02d8bd7c5a189a01ebc3b4992
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-12-07 18:59:32 -0400

    Update tools-confesar.js

--- plugins/tools-confesar.js
+++ plugins/tools-confesar.js
@@ -1,74 +1,71 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     conn.menfess = conn.menfess ? conn.menfess : {}
-    conn.chatIds = conn.chatIds ? conn.chatIds : {}
+    const user = {};
+    const _user = {}; 
 
-    if (!text) throw m.reply(`*🌸 Ejemplo:*\n\n${usedPrefix + command} numero|nombre anónimo|mensaje\n\n*🌷 Nota:* El nombre del remitente puede ser seudónimo o anónimo.\n\n*🍂 Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonimo|Hola.`);
+    if (command === 'anonimo') {
+        if (!text) throw m.reply(`*🌸 Ejemplo:*\n\n${usedPrefix + command} numero|nombre anónimo|mensaje\n\n*🌷 Nota:* El nombre del remitente puede ser seudónimo o anónimo.\n\n*🍂 Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonimo|Hola.`);
 
-    let [jid, name, pesan] = text.split('|');
-    if ((!jid || !name || !pesan)) throw m.reply(`*🍁 Ejemplo:*\n\n${usedPrefix + command} numero|nombre anónimo|mensaje\n\n*🌹 Nota:* El nombre del remitente puede ser seudónimo o anónimo.\n\n*🍀 Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonimo|Hola.`);
+        let [jid, name, pesan] = text.split('|');
+        if ((!jid || !name || !pesan)) throw m.reply(`*🍁 Ejemplo:*\n\n${usedPrefix + command} numero|nombre anónimo|mensaje\n\n*🌹 Nota:* El nombre del remitente puede ser seudónimo o anónimo.\n\n*🍀 Uso:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonimo|Hola.`);
 
-    jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
-    let data = (await conn.onWhatsApp(jid))[0] || {};
-    if (!data.exists) throw m.reply('🍂 El número no está registrado en WhatsApp.');
-    if (jid == m.sender) throw m.reply('🌸 No puedes mandarte un mensaje a ti mismo.');
+        jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+        let data = (await conn.onWhatsApp(jid))[0] || {};
+        if (!data.exists) throw m.reply('🍂 El número no está registrado en WhatsApp.');
+        if (jid == m.sender) throw m.reply('🌸 No puedes mandarte un mensaje a ti mismo.');
 
-    let chatId = `${m.sender}_${data.jid}`;
-    let id = Math.floor(100 + Math.random() * 900);
+        let id = Math.floor(100 + Math.random() * 900);
 
-    while (conn.menfess[id]) {
-        id = Math.floor(100 + Math.random() * 900);
-    }
+        while (conn.menfess[id]) {
+            id = Math.floor(100 + Math.random() * 900);
+        }
 
-    let teks = `Hola @${data.jid.split("@")[0]}, recibiste un mensaje de confesión.\n\nDe: *${name}*\nMensaje: \n${pesan}\nID: ${id}\n\n¿Quieres responder a este mensaje? ¿Cómo puedes hacerlo? Simplemente escriba .responder ${id} <tu mensaje> y envíelo, para transmitirlo a *${name}*.`;
+        let teks = `Hola @${data.jid.split("@")[0]}, recibiste un mensaje anónimo.\n\nDe: *${name}*\nMensaje: \n${pesan}\nID: ${id}\n\n¿Quieres responder a este mensaje? Simplemente escribe:\n> .responder ${id} <mensaje>\n\n_*El mensaje será enviado 📤*_`;
 
-    await conn.relayMessage(data.jid, {
-        extendedTextMessage: {
-            text: teks,
-            contextInfo: {
-                mentionedJid: [data.jid],
-                externalAdReply: {
-                    title: 'Mensajes Anónimos',
-                    mediaType: 1,
-                    previewType: 0,
-                    renderLargerThumbnail: true,
-                    thumbnailUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIyz1dMPkZuNleUyfXPMsltHwKKdVddTf4-A&usqp=CAU',
-                    sourceUrl: ''
+        await conn.relayMessage(data.jid, {
+            extendedTextMessage: {
+                text: teks,
+                contextInfo: {
+                    mentionedJid: [data.jid],
                 }
             }
-        }
-    }, {}).then(() => {
-        m.reply('*🍂 Mensaje enviado con éxito.*');
-        conn.menfess[id] = {
-            id,
-            dari: m.sender,
-            nama: name,
-            penerima: data.jid,
-            pesan: pesan,
-            status: false,
-            replies: []
-        };
-        return !0;
-    });
+        }, {}).then(() => {
+            m.reply('*🍂 Mensaje enviado con éxito.*');
+            conn.menfess[id] = {
+                id,
+                dari: m.sender,
+                nama: name,
+                penerima: data.jid,
+                pesan: pesan,
+                status: false,
+                replies: []
+            };
+            return !0;
+        });
+    }
 
     if (command === 'responder') {
         let args = text.split(' ');
-        if (args.length < 2) return m.reply('Por favor, especifica la ID y tu respuesta después del comando.');
+        if (args.length < 2) return m.reply('Por favor, especifica la ID y tu mensaje después del comando.\nEjemplo: .responder <ID> <mensaje>');
 
-        let responseId = args[1];
-        let responseMessage = args.slice(2).join(' ');
+        let responseId = args[1]; 
+        let responseMessage = args.slice(2).join(' '); 
 
         let mfToRespond = conn.menfess[responseId];
         if (!mfToRespond) return m.reply('No hay mensajes anónimos con esa ID.');
 
+        const confirm = `¿Confirmas enviar tu respuesta a @${mfToRespond.nama}?*\n\n*—◉ 𝑬𝒔𝒄𝒓𝒊𝒃𝒂:*\n*◉ responder ${responseId} ${responseMessage}*`.trim();
+
         await conn.sendMessage(mfToRespond.dari, {
-            text: `Respuesta de *${mfToRespond.nama}*: ${responseMessage}\n\nID: ${responseId}`
-        });
+            text: confirm,
+            mentions: [mfToRespond.dari]
+        }, { quoted: m });
 
         mfToRespond.replies.push({
             responder: m.sender,
             message: responseMessage
         });
-        m.reply('Tu respuesta ha sido enviada.');
+        m.reply('*El mensaje será enviado en breve*');
     }
 }
 
