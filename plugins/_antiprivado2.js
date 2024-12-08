1c73d774001755adbfa42c4d3fecb1211053b95a
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-12-08 12:20:42 -0400

    Update _antiprivado2.js

--- plugins/_antiprivado2.js
+++ plugins/_antiprivado2.js
@@ -1,44 +1,29 @@
-let antiPrivate2Enabled = false;
 
-const handler = {
-  command: ['antiprivado2', 'antiarabe'],
-  rowner: true,
-  
-  async handle(m, { conn, isROwner }) {
-    const [action] = m.text.split(' ').slice(1);
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
 
-    if (!isROwner) {
-      global.dfail('admin', m, conn);
-      throw false;
-    }
-
-    if (action === 'on') {
-      antiPrivate2Enabled = true;
-      await m.reply('✅ La opción ha sido activada.');
-    } else if (action === 'off') {
-      antiPrivate2Enabled = false;
-      await m.reply('❌ La opción ha sido desactivada.');
-    } else {
-      await m.reply('⚠️ Por favor, usa .antiprivado2 on para activar o .antiprivado2 off para desactivar.');
-    }
-  }
-};
-
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-  if (m.isBaileys && m.fromMe) return true;
-  if (m.isGroup) return false;
-  if (!m.message) return true;
-
+  // Lista de prefijos prohibidos
   let forbidPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "234", "91", "254", "213"];
-  let senderNumber = m.sender.split('@')[0];
-  let isForbidden = forbidPrefixes.some(prefix => senderNumber.startsWith(prefix));
+  
+  // Verificar si el mensaje contiene uno de los prefijos prohibidos
+  const senderPrefix = m.sender.split('@')[0];
+  const hasForbiddenPrefix = forbidPrefixes.some(prefix => senderPrefix.startsWith(prefix));
 
-  if (antiPrivate2Enabled && !isOwner && !isROwner && isForbidden) {
-    await m.reply(`> "🤬 Pinché Árabe de mierda No los quiero ver vivos me tienen la vrg parada ya de tanto\n> .apk WhatsApp\n vas hacer bloqueado por Perra.`, false, { mentions: [m.sender] });
+  if (hasForbiddenPrefix) {
+    await m.reply(`> "🥵 puta @${m.sender.split`@`[0]}, Lo Siento No Esta 🌹Permitido Escribirme Al Privado 🌷Por Lo Cual Seras Bloqueado/A\n\n> *🍒Puedes Unirte Al Grupo Oficial De La Bot🪷* 👇\n\n\n${gp1}`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'block');
+    return !1;
+  }
+
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  if (bot.antiPrivate2 && !isOwner && !isROwner) {
+    await m.reply(`> "🥵 puta @${m.sender.split`@`[0]}, Lo Siento No Esta 🌹Permitido Escribirme Al Privado 🌷Por Lo Cual Seras Bloqueado/A\n\n> *🍒Puedes Unirte Al Grupo Oficial De La Bot🪷* 👇\n\n\n${gp1}`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
-
-  return false;
+  return !1;
 }
-
-export default handler;
