b1e61f9b27bd798ce8e423dbfcf65e835570278c
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-12-08 12:43:44 -0400

    Update _antiarabe.js

--- plugins/_antiarabe.js
+++ plugins/_antiarabe.js
@@ -1,23 +1,25 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
+let handler = m => m
 
-  let forbidPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "91", "254", "213"];
-  
-  const senderNumber = m.sender.split('@')[0];
-  const hasForbiddenPrefix = forbidPrefixes.some(prefix => senderNumber.startsWith(prefix));
-
-  if (hasForbiddenPrefix) {
-    await m.reply(`> "🥵 puta @${senderNumber}, Lo Siento No Esta 🌹Permitido Escribirme Al Privado 🌷Por Lo Cual Seras Bloqueado/A\n\n> *🍒Puedes Unirte Al Grupo Oficial De La Bot🪷* 👇\n\n\n${gp1}`, false, {mentions: [m.sender]});
-    await this.updateBlockStatus(m.chat, 'block');
-    return !1;
-  }
-
+handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner}) {
+  if (m.isGroup) return !1
   const bot = global.db.data.settings[this.user.jid] || {};
-  if (bot.antiarabe && !isOwner && !isROwner) {
-    return !1;
-  }
   
-  return !0; // Permitir otros números
+  let forbidPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "91", "254", "213"]
+
+  for (let prefix of forbidPrefixes) {
+    if (m.sender.startsWith(prefix)) {
+      await m.reply('🚩 Lo siento, no se permite la comunicación desde números con este prefijo.', m.sender)
+      await conn.updateBlockStatus(m.sender, 'block')
+      return false
+    }
+  }
+
+  if (bot.antiarabe && !isOwner) {
+    await m.reply('🚩 Lo siento, no se permite la comunicación desde números con este prefijo.', m.sender)
+    return false;
+  }
+
+  return true
 }
+
+export default handler
