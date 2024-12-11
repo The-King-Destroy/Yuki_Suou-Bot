c010faab563eeed010d5930be84648ceacf7c23e
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-12-10 20:13:41 -0400

    Update descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -2,7 +2,7 @@ import { igdl } from "ruhend-scraper";
 
 let handler = async (m, { args, conn }) => { 
     if (!args[0]) {
-        return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🤍\`*', m, fake);
+        return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🌸\`*', m, fake);
     }
     
     try {
