40f43c1276d3f8aba150c085f699c9b0ab509504
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2024-12-31 00:59:24 -0400

    Update descargas-ytmp3.js

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -1,22 +1,19 @@
 import fetch from 'node-fetch'
 
-let HS = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) {
-return conn.reply(m.chat, `❀Ingresa el link de youtube `, m)
-}
-    
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `《✧》Por favor, envia un link de Youtube para descargar su audio.`, m)
+
 try {
-let calidad = '128' // Calidades disponibles : 32, 64, 128, 192, 320
-let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp3q?apikey=gifted&quality=${calidad}&url=${text}`)
+let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp3?url=${text}`)
 let json = await api.json()
-let { quality, title, download_url, thumbnail } = json.result
+let title = json.result.metadata.title
+let dl_url = json.result.download.url
+await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
 
-
-await conn.sendMessage(m.chat, { audio: { url: download_url }, caption: null, mimetype: "audio/mpeg" }, { quoted: m })
 } catch (error) {
 console.error(error)
 }}
 
-HS.command = ['ytmp3']
+HS.command = ['ytmp3', 'fgmp3', 'yta']
 
 export default HS
