f24703eb514a4b62f9a0ac6ab9b4ea4d1ef46ddf
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-01-01 21:03:58 -0400

    Update descargas-ytpm4.js

--- plugins/descargas-ytpm4.js
+++ plugins/descargas-ytpm4.js
@@ -1,15 +1,18 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
-let HS = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `《✧》Por favor, envia un link de Youtube para descargar el video.`, m)
-
+let HS = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) {
+return conn.reply(m.chat, `《✧》Por favor, envia un link de Youtube para descargar el video.`, m)
+}
+    
 try {
-let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${text}`)
+let calidad = '480' // Calidades disponibles : 144, 240, 360, 480, 720, 1080, 1440, 2160
+let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4q?apikey=gifted&quality=${calidad}&url=${text}`)
 let json = await api.json()
-let title = json.data.metadata.title
-let dl_url = json.data.download.url
-await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${json.data.filename}.mp4`, mimetype: "video/mp4" }, { quoted: m })
+let { quality, title, download_url, thumbnail } = json.result
 
+
+await conn.sendMessage(m.chat, { video: { url: download_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 } catch (error) {
 console.error(error)
 }}
