8b0862bfd084e1672f118d6822c5c3f2685798ca
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-01-05 12:32:23 -0400

    Update _clear.js

--- plugins/_clear.js
+++ plugins/_clear.js
@@ -3,8 +3,8 @@
 
 import fs from 'fs';
 
-const directoryPath = './YukiJadiBot/';
-const sanSessionPath = './YukiSession/';
+const directoryPath = './Sections-SubBots/';
+const sanSessionPath = './Sections-JadiBots/';
 
 function cleanSubbotDirectories() {
   fs.readdir(directoryPath, (err, subbotDirs) => {
@@ -48,7 +48,7 @@ function cleanSessionFiles() {
           if (err && err.code !== 'ENOENT') {
             console.log(`Error al eliminar Session: ${file}: ` + err);
           } else {
-            console.log(`Session: ${file} elimiando.`);
+            console.log(`Session: ${file} eliminado.`);
           }
         });
       }
@@ -56,8 +56,8 @@ function cleanSessionFiles() {
   });
 }
 
-setInterval(cleanSubbotDirectories, 10 * 1000);
-setInterval(cleanSessionFiles, 10 * 1000);
+setInterval(cleanSubbotDirectories, 60 * 1000);
+setInterval(cleanSessionFiles, 60 * 1000);
 
 cleanSubbotDirectories();
 cleanSessionFiles();
