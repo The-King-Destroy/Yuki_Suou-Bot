ffcdfa2a2274098f5b5a8cf0034fd99102b56ec3
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-01-16 17:57:54 -0400

    Create _antitrabas.js

---
+++ plugins/_antitrabas.js
@@ -0,0 +1,28 @@
+import * as fs from 'fs';
+
+export async function before(m, {conn, isAdmin, isBotAdmin, usedPrefix}) {
+  if (m.isBaileys && m.fromMe) {
+    return !0;
+  }
+  if (!m.isGroup) return !1;
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  const delet = m.key.participant;
+  const bang = m.key.id;
+  const name = await conn.getName(m.sender);
+  const fakemek = {'key': {'participant': '0@s.whatsapp.net', 'remoteJid': '0@s.whatsapp.net'}, 'message': {'groupInviteMessage': {'groupJid': '51995386439-1616969743@g.us', 'inviteCode': 'm', 'groupName': 'P', 'caption': 'Yuki-Suou-Bot', 'jpegThumbnail': null}}};
+  if (chat.antiTraba && m.text.length > 5000) { 
+    if (isAdmin) return conn.sendMessage(m.chat, {text: `El administrador @${m.sender.split('@')[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`, mentions: [m.sender]}, {quoted: fakemek});
+    conn.sendMessage(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, m);
+    if (isBotAdmin && bot.restrict) {
+      conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
+        	setTimeout(() => {
+        	conn.sendMessage(m.chat, {text: `Marcar el chat como leido ✓\n${'\n'.repeat(400)}\n=> El número : wa.me/${m.sender.split('@')[0]}\n=> Alias : ${name}\n[ ! ] Acaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender]}, {quoted: fakemek});
+      }, 0);
+      setTimeout(() => {
+        	conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      }, 1000);
+    } else if (!bot.restrict) return m.reply('[ ! ] Para realizar acciones de eliminación, mi dueño tiene que encender el modo restringido!');
+  }
+  return !0;
+}
