9b07069c85c9389faa4e7dd8c4009d5e72968983
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-01-17 02:23:10 -0400

    Update descargas-mediafire.js

--- plugins/descargas-mediafire.js
+++ plugins/descargas-mediafire.js
@@ -1,31 +1,38 @@
 import axios from 'axios'
 import fetch from 'node-fetch'
 import cheerio from 'cheerio'
-import {mediafiredl} from '@bochilteam/scraper'
-
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '🍬 Por favor, envia un link de MediaFire para descargar el archivo.', m, rcanal)
-if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, '🍭 El enlace deve ser de un archivo de Mediafire.', m, rcanal)
-try {
-await m.react(rwait)
-let { title, ext, aploud, size, dl_url } = await mediafiredl(args[0])
-let txt = `乂  *¡MEDIAFIRE - DESCARGAS!*  乂\n\n`
-    txt += `✩ *Nombre* : ${title}\n`
-    txt += `✩ *Peso* : ${size}\n`
-    txt += `✩ *Publicado* : ${aploud}\n`
-    txt += `✩ *MimeType* : ${ext}\n\n`
-    txt += `*- ↻ El archivo se esta enviando espera un momento, soy lento. . .*`
-let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, fkontak, null, rcanal)
-await conn.sendFile(m.chat, dl_url, title, null, fkontak, null, { mimetype: ext, asDocument: true })
-await m.react(done)
+if (!args[0]) throw m.reply(`🍬 Por favor, envia un link de MediaFire para descargar el archivo.`m, rcanal)
+try {  
+let res = await mediafireDl(args[0])
+let { name, size, date, mime, link } = res
+let caption = `🍬 *Nombre:* ${name}
+☁️ *Peso:* ${size}
+🚩 *Tipo:* ${mime}`.trim()
+conn.reply(m.chat, caption, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: packname, body: dev, previewType: 0, thumbnail: banner, sourceUrl: channel}}})
+await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: mime, asDocument: true })
 } catch (e) {
-await m.reply('⚠️ Ocurrió un error inesperado\n\n' + e)
-await m.react(error)
+await conn.reply(m.chat, `⚠️ Error, pruebe nuevamente con ${usedPrefix + command}`)
+console.log(e)
+handler.limit = false      
 }}
 handler.help = ['mediafire']
 handler.tags = ['descargas']
-handler.command = ['mediafire', 'mdfire', 'mf']
-handler.premium = false
-
+handler.command = ['mediafire', 'mf']
+handler.register = true
+//handler.group = true
 export default handler
+
+async function mediafireDl(url) {
+const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/','')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`)
+const $ = cheerio.load(res.data)
+const link = $('#downloadButton').attr('href')
+const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ','').replaceAll('\n','')
+const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text()
+const size = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ','')
+let mime = ''
+let rese = await axios.head(link)
+mime = rese.headers['content-type']
+return { name, size, date, mime, link }
+}
