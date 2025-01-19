63b31863382bbe41cf976d38018e68be1737984e
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-01-19 19:25:16 -0400

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -4,12 +4,10 @@ let handler = async (m, { conn, args }) => {
   let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
   let user = global.db.data.users[userId];
   let name = conn.getName(userId);
-  let cumpleanos = user.birth || 'No especificado';
-  let genero = user.genre || 'No especificado';
   let exp = user.exp || 0;
   let nivel = user.level || 0;
   let coins = user.coin || 0;
-  let role = user.role || '';
+  let role = user.role || 'Sin Rango';
 
   let perfil = await conn.profilePictureUrl(userId, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg');
 
