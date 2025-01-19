77d1f20e29f9eb5430a1e0f0ca71f42b291d2173
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-01-19 01:43:01 -0600

    Actualizar _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -2,9 +2,9 @@ export async function before(m) {
   if (!m.text || !global.prefix.test(m.text)) {
     return;
   }
-/*if (m.key.fromMe) {
+if (m.key.fromMe) {
      return;
-}*/
+}
 
   const usedPrefix = global.prefix.exec(m.text)[0];
   const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
