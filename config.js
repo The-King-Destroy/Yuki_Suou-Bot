aa78ab04c9b7be3b9fa665547e80f59679caade4
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-01-21 08:23:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -64,6 +64,7 @@ global.welcom1 = 'Edita Con #setwelcome'
 global.welcom2 = 'Edita Con #setbye'
 global.banner = 'https://tinyurl.com/27d7rnhu'
 global.catalogo = 'https://tinyurl.com/25ru293g'
+global.test = 'https://tinyurl.com/25ru293g'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
