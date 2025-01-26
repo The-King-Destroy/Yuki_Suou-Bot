2949bca5a9190db5a659eb288064b69d5439fad0
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-01-26 11:43:09 -0400

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,64 +1,55 @@
 import moment from 'moment-timezone';
 
 let handler = async (m, { conn, args }) => {
+    let staff = `
+    ✨ *EQUIPO DE AYUDANTES* ✨
+    👑 *Dueño* ${creador}
+    🍬 *Bot:* ${botname}
+    ⚜️ *Versión:* ${vs}
+    📚 *Libreria:* ${libreria} ${baileys}
 
-  let staff = `
-✨ *EQUIPO DE AYUDANTES* ✨
-👑 *Dueño* ${creador}
-🍰 *Bot:* ${botname}
-⚜️ *Versión:* ${vs}
-📚 *Libreria:* ${libreria} ${baileys}
+    🪐 *Creador:*
 
-🪐 *Creador:*
+    ☁️ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜
+    🔖 *Rol:* Creador
+    👾 *GitHub:* https://github.com/The-King-Destroy
 
-☁️ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜
-🔖 *Rol:* Creador
-👾 *GitHub:* https://github.com/The-King-Destroy
+    🍃 *Colaboradores:*
 
-🍃 *Colaboradores:*
+    🫧 𝓔𝓶𝓶𝓪 𝓥𝓲𝓸𝓵𝓮𝓽𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷 
+    🔖 *Rol:* Developer
+    👾 *GitHub:* https://github.com/Elpapiema
 
-🫧 𝓔𝓶𝓶𝓪 𝓥𝓲𝓸𝓵𝓮𝓽𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷 
-🔖 *Rol:* Developer
-👾 *GitHub:* https://github.com/Elpapiema
+    🍍 Niño Piña
+    🔖 *Rol:* Developer
+    👾 *GitHub:* https://github.com/WillZek
 
-🍍 Niño Piña
-🔖 *Rol:* Developer
-👾 *GitHub:* https://github.com/WillZek
+    ⚡ ☆꧁༒ĹєǤ𝒆𝐧𝐃༒꧂☆
+    🔖 *Rol:* Developer
+    👾 *GitHub:* https://github.com/Diomar-s
 
-⚡ ☆꧁༒ĹєǤ𝒆𝐧𝐃༒꧂☆
-🔖 *Rol:* Developer
-👾 *GitHub:* https://github.com/Diomar-s
+    ☘️ I'm Fz' (Tesis)
+    🔖 *Rol:* Developer
+    👾 *GitHub:* https://github.com/FzTeis
 
-☘️ I'm Fz' (Tesis)
-🔖 *Rol:* Developer
-👾 *GitHub:* https://github.com/FzTeis
+    🌪️ 𝓛𝓮𝓰𝓷𝓪
+    🔖 *Rol:* Moderador 
+    👾 *GitHub:* https://github.com/Legna-chan
+    `.trim();
 
-🌪️ 𝓛𝓮𝓰𝓷𝓪
-🔖 *Rol:* Moderador 
-👾 *GitHub:* https://github.com/Legna-chan
-  `.trim();
-
-  await conn.sendMessage(m.chat, { 
-      text: staff,
-      contextInfo: {
-          isForwarded: true,
-          forwardedNewsletterMessageInfo: {
-              newsletterJid: channelRD.id,
-              newsletterName: channelRD.name,
-              serverMessageId: -1,
-          },
-          forwardingScore: 999,
-          externalAdReply: {
-              title: `✨ Developers`,
-              body: dev,
-              thumbnailUrl: banner,
-              mediaType: 1,
-              showAdAttribution: true,
-              renderLargerThumbnail: true,
-          },
-      },
-  }, { quoted: m });
-
+    await conn.sendMessage(m.chat, {
+        text: staff,
+        contextInfo: {
+            externalAdReply: {
+                title: '✨ Developers',
+                body: dev,
+                thumbnailUrl: catalogo,
+                mediaType: 1,
+                showAdAttribution: true,
+                renderLargerThumbnail: true
+            }
+        }
+    }, { quoted: m });
 };
 
 handler.help = ['staff'];
