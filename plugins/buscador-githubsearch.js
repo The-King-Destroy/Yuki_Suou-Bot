420a2aee39be497781c7c5340abea52e883f5228
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-01-31 12:45:02 -0400

    Update buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -9,7 +9,7 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return m.reply(m.chat, '🍬 Por favor ingresa un nombre de un repositorio o usuario de GitHub.', m);
+if (!text) return m.reply(m.chat, '🍬 Por favor ingresa un nombre de un repositorio GitHub.', m);
 
 try {
 let api = `https://dark-core-api.vercel.app/api/search/github?key=api&text=${text}`;
@@ -18,7 +18,7 @@ let response = await fetch(api);
 let json = await response.json();
 let result = json.results[0];
 
-let txt = `*Nombre:* ${result.name}\n*Owner:* ${result.creator}\n*Estrellas:* ${result.stars}\n*Bifurcaciones:* ${result.forks}\n*Descripcion:* ${result.description}\n*Creado:* ${result.createdAt}\n*Link:* ${result.cloneUrl}`;
+let txt = `🍬 *Nombre:* ${result.name}\n👑 *Owner:* ${result.creator}\n🌟 *Estrellas:* ${result.stars}\n🔖 *Bifurcaciones:* ${result.forks}\n📜 *Descripcion:* ${result.description}\n📆 *Creado:* ${result.createdAt}\n🔗 *Link:* ${result.cloneUrl}`;
 
 let img = 'https://qu.ax/trzJV.jpg';
 
