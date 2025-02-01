26a75e2c0833b77b3d71c001584c76449caf61c5
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-01 12:46:22 -0400

    Update and rename Test-codigo.js to owner-codigo.js

--- plugins/owner-codigo.js
+++ plugins/owner-codigo.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let amount = parseInt(text.trim());
 
     if (isNaN(amount) || amount <= 0) {
-        return conn.reply(m.chat, `Por favor, ingrese una cantidad válida de ${moneda}.`, m);
+        return conn.reply(m.chat, `🍬 Por favor, ingrese una cantidad válida de ${moneda}.`, m);
     }
 
     let code = Math.random().toString(36).substring(2, 10).toUpperCase();
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text }) => {
     if (!global.db.data.codes) global.db.data.codes = {};
     global.db.data.codes[code] = { coin: amount, claimedBy: [] };
 
-    conn.reply(m.chat, `Código generado: ${code}\nEste código puede ser canjeado por ${amount} ${moneda} y puede ser utilizado por 5 personas.`, m);
+    conn.reply(m.chat, `🍬 Código generado: ${code}\nEste código puede ser canjeado por ${amount} ${moneda} y puede ser utilizado por 5 personas.`, m);
 }
 
 handler.help = ['codigo <cantidad de coins>'];
