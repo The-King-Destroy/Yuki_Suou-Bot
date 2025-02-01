e7604bd118ab5b6c6bb0beb014d5bf198dc84876
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-01 10:56:29 -0400

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,72 +1,27 @@
-import fetch from "node-fetch";
+import axios from 'axios'
+import cheerio from 'cheerio'
 
-async function fetchTruecallerData(phoneNumber) {
-    const installationId = "a1i0D--jTBiKAks-Y9FHnPk_XG-YIsKEIa_eWiBwjH68LKn-zKRx9vaZq731KL0x";
-    const apiUrl = `https://truecaller-api.vercel.app/search?phone=${encodeURIComponent(phoneNumber)}&id=${installationId}`;
-    
+
+let handler = async (m, { text }) => {
+	if (!text) throw `✳️ Ingresa lo que quieres buscar en Wikipedia` 
+	
     try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`Error al obtener datos de Truecaller. Código de estado: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        throw new Error(`Error al obtener datos de Truecaller: ${error.message}`);
-    }
+	const link =  await axios.get(`https://es.wikipedia.org/wiki/${text}`)
+	const $ = cheerio.load(link.data)
+	let wik = $('#firstHeading').text().trim()
+	let resulw = $('#mw-content-text > div.mw-parser-output').find('p').text().trim()
+	m.reply(`▢ *Wikipedia*
+
+‣ Buscado : ${wik}
+
+${resulw}`)
+} catch (e) {
+  m.reply('⚠️ NINGUN RESULTADO ')
 }
-
-function formatTruecallerData(data) {
-    let formattedData = "";
-    for (const prop in data) {
-        if (prop === "flagURL") continue;
-        
-        formattedData += `⚝ *${prop}:* ${formatPropertyValue(data[prop])}\n`;
-    }
-    return formattedData;
 }
+handler.help = ['wikipedia']
+handler.tags = ['tools']
+handler.command = ['wiki','wikipedia'] 
 
-function formatPropertyValue(value) {
-    if (Array.isArray(value)) {
-        return value.join(", ");
-    } else if (typeof value === "object") {
-        return formatObjectPropertyValue(value);
-    }
-    return value;
-}
 
-function formatObjectPropertyValue(obj) {
-    let formattedValue = "";
-    for (const key in obj) {
-        formattedValue += `${key}: ${obj[key]}\n`;
-    }
-    return formattedValue;
-}
-
-async function handler(m, { text, quoted, mentionedJid }) {
-    let phoneNumber = "";
-    if (text) {
-        phoneNumber = text.replace(/[^0-9]/g, "");
-    } else if (quoted) {
-        phoneNumber = quoted.sender.replace(/[^0-9]/g, "");
-    } else if (mentionedJid && mentionedJid[0]) {
-        phoneNumber = mentionedJid[0].replace(/[^0-9]/g, "");
-    } else {
-        throw `Proporcione un número en formato internacional sin +, cite un usuario o mencione un usuario`;
-    }
-
-    try {
-        const truecallerData = await fetchTruecallerData(phoneNumber);
-        truecallerData.creator = "Gabriel Curi BY REM-BOT";
-        const formattedData = formatTruecallerData(truecallerData);
-        m.reply(formattedData);
-    } catch (error) {
-        console.error(error);
-        throw "Ocurrió un error al obtener los datos de Truecaller";
-    }
-}
-
-handler.help = ["true"];
-handler.tags = ["tools"];
-handler.command = ['truecaller'];
-
-export default handler;
+export default handler
