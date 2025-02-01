48db3a199390cb4a4ecb39efdbceb9e9c7916c02
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-01 11:40:52 -0400

    Update Test-codigo.js

--- plugins/Test-codigo.js
+++ plugins/Test-codigo.js
@@ -2,18 +2,18 @@ let handler = async (m, { conn, text }) => {
     let amount = parseInt(text.trim());
 
     if (isNaN(amount) || amount <= 0) {
-        return conn.reply(m.chat, 'Por favor, ingrese una cantidad válida de créditos.', m);
+        return conn.reply(m.chat, `Por favor, ingrese una cantidad válida de ${moneda}.`, m);
     }
 
     let code = Math.random().toString(36).substring(2, 10).toUpperCase();
 
     if (!global.db.data.codes) global.db.data.codes = {};
-    global.db.data.codes[code] = { credits: amount, claimedBy: [] };
+    global.db.data.codes[code] = { coin: amount, claimedBy: [] };
 
-    conn.reply(m.chat, `Código generado: ${code}\nEste código puede ser canjeado por ${amount} créditos y puede ser utilizado por 5 personas.`, m);
+    conn.reply(m.chat, `Código generado: ${code}\nEste código puede ser canjeado por ${amount} ${moneda} y puede ser utilizado por 5 personas.`, m);
 }
 
-handler.help = ['codigo <cantidad de créditos>'];
+handler.help = ['codigo <cantidad de coins>'];
 handler.tags = ['owner'];
 handler.command = ['codigo']
 handler.rowner = true;
