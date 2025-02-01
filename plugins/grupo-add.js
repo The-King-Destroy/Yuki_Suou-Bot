dbfea475f34d6b8fd1f2c83d8d639fa66871d695
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-01 19:17:59 -0400

    Update grupo-add.js

--- plugins/grupo-add.js
+++ plugins/grupo-add.js
@@ -1,27 +1,19 @@
-var handler = async (m, { conn, args, text, usedPrefix, command }) => {
-
-let who 
-if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-else who = m.chat
-let name = await conn.getName(m.sender)        
-let user = global.db.data.users[who]
-let nom = conn.getName(m.sender)
-if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🍬 Este comando está deshabilitado por mi creador.`, m, rcanal) 
-if (!text) await m.reply(`🍬 Por favor, ingrese el número de la persona que quieres añadir a este grupo.`)
-if (text.includes('+')) await m.reply(`🍭 Ingrese el número todo junto sin el *(+)*`)
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `🍬 Por favor, ingrese el número al que quiere enviar una invitación al grupo.`, m)
+if (text.includes('+')) return conn.reply(`🍭 Ingrese el número todo junto sin el *+*`, m)
+if (isNaN(text)) return conn.reply(m.chat, `🍭 Ingrese sólo números más su código de país sin espacios*`, m)
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-
-await conn.reply(text+'@s.whatsapp.net', `🍬 Hola! soy ${botname}, Una persona te a invitado a su grupo.*\n\n🍭 *Link:*\n${link}`, m, {mentions: [m.sender]})
-await m.reply(`🍬 *Enviando la invitación al privado de ${nom}*\n\n*📅 ${fecha}*\n⏰ *${tiempo}*`) 
+ 
+      await conn.reply(text+'@s.whatsapp.net', `🍬 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
+        m.reply(`🍭 Se envió un enlace de invitación al usuario.`) 
 
 }
-handler.help = ['add']
-handler.tags = ['grupo']
+handler.help = ['invite *<521>*']
+handler.tags = ['group']
 handler.command = ['add', 'agregar', 'añadir']
 handler.group = true
-handler.admin = true
+handler.admin = false
 handler.botAdmin = true
-handler.fail = null
 
 export default handler
