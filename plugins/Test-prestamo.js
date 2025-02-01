bff91be8da7e27a39b9748e3ce1db1d16625c573
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-01 13:06:19 -0400

    Update Test-prestamo.js

--- plugins/Test-prestamo.js
+++ plugins/Test-prestamo.js
@@ -6,15 +6,15 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     const prestamos = {
         100: 2 * 60 * 60 * 1000,
         300: 3 * 60 * 60 * 1000,
-        600: 4 * 60 * 60 * 1000,
+        600: 4 * 60 * 1000,
         1200: 5 * 60 * 60 * 1000,
         2400: 6 * 60 * 60 * 1000,
         4800: 7 * 60 * 60 * 1000,
     };
 
     if (command === 'prestamo') {
-        if (user.limit < 20 || user.limit >= 300) {
-            return conn.reply(m.chat, 'Este comando puede ser usado pasando los 20 creditos.', m);
+        if (user.coin < 20 || user.coin >= 300) {
+            return conn.reply(m.chat, `Este comando puede ser usado pasando los 20 ${moneda}.`, m);
         }
 
         let monto = parseInt(text);
@@ -34,18 +34,18 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
             return conn.reply(m.chat, 'Ya tienes un préstamo pendiente. Debes pagarlo antes de solicitar otro.', m);
         }
 
-        user.limit += monto;
+        user.coin += monto;
         user.prestamo = {
             monto: monto,
             fechaPago: Date.now() + prestamos[monto],
             descuentoActivo: false
         };
 
-        conn.reply(m.chat, `Has recibido un préstamo de ${monto} créditos. Debes pagarlo en ${prestamos[monto] / (60 * 60 * 1000)} horas.\n\n.pagar < cantidad >  para pagar el prestamo`, m);
+        conn.reply(m.chat, `Has recibido un préstamo de ${monto} ${moneda}. Debes pagarlo en ${prestamos[monto] / (60 * 60 * 1000)} horas.\n\n.pagar < cantidad >  para pagar el prestamo`, m);
 
         setTimeout(() => {
             if (user.prestamo.monto > 0) {
-                conn.reply(m.chat, `@${m.sender.split('@')[0]} usted no pagó su préstamo en el tiempo indicado, los créditos que gane se les descontará gracias...`, null, { mentions: [m.sender] });
+                conn.reply(m.chat, `@${m.sender.split('@')[0]} usted no pagó su préstamo en el tiempo indicado, los ${moneda} que gane se les descontará gracias...`, null, { mentions: [m.sender] });
                 user.prestamo.descuentoActivo = true;
             }
         }, prestamos[monto]);
@@ -57,35 +57,35 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
         }
 
         if (isNaN(cantidad) || cantidad <= 0) {
-            return conn.reply(m.chat, `Uso: ${usedPrefix}${command} <cantidad>\n\nTu deuda actual es de ${user.prestamo.monto} créditos.`, m);
+            return conn.reply(m.chat, `Uso: ${usedPrefix}${command} <cantidad>\n\nTu deuda actual es de ${user.prestamo.monto} ${moneda}.`, m);
         }
 
-        if (user.limit < cantidad) {
-            return conn.reply(m.chat, `No tienes suficientes créditos para pagar esa cantidad. Necesitas ${user.prestamo.monto} créditos para saldar tu deuda.`, m);
+        if (user.coin < cantidad) {
+            return conn.reply(m.chat, `No tienes suficientes ${moneda} para pagar esa cantidad. Necesitas ${user.prestamo.monto} ${moneda} para saldar tu deuda.`, m);
         }
 
         if (cantidad >= user.prestamo.monto) {
-            user.limit -= user.prestamo.monto;
+            user.coin -= user.prestamo.monto;
             user.prestamo = {};
             conn.reply(m.chat, 'Su préstamo fue pagado con éxito. No tiene deudas.', m);
         } else {
-            user.limit -= cantidad;
+            user.coin -= cantidad;
             user.prestamo.monto -= cantidad;
-            conn.reply(m.chat, `Has pagado ${cantidad} créditos. Tu deuda restante es de ${user.prestamo.monto} créditos.`, m);
+            conn.reply(m.chat, `Has pagado ${cantidad} ${moneda}. Tu deuda restante es de ${user.prestamo.monto} ${moneda}.`, m);
         }
     }
 }
 
-let handlerCreditInterceptor = async (user, creditsGained) => {
+let handlerCreditInterceptor = async (user, coinGained) => {
     if (user.prestamo && user.prestamo.descuentoActivo) {
-        if (creditsGained >= user.prestamo.monto) {
-            user.limit += (creditsGained - user.prestamo.monto);
+        if (coinGained >= user.prestamo.monto) {
+            user.coin += (coinGained - user.prestamo.monto);
             user.prestamo = {};
         } else {
-            user.prestamo.monto -= creditsGained;
+            user.prestamo.monto -= coinGained;
         }
     } else {
-        user.limit += creditsGained;
+        user.coin += coinGained;
     }
 };
 
