ffcaee30b90515359fa4514833a8f0ccd6d06e51
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-02 16:28:02 -0400

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -15,7 +15,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
         const nametag = await conn.getName(user);
         const nn = conn.getName(m.sender);
         await conn.reply(m.chat, `✅️ El usuario *${nametag}* ha sido desbaneado.`, m, { mentionedJid: [user] });
-        conn.reply('584120346669@s.whatsapp.net', `🍬 El usuario *${nametag}* ha sido desbaneado por *${nn}*.`, m, rcanal, );
+        conn.reply(`${suittag}@s.whatsapp.net`, `🍬 El usuario *${nametag}* ha sido desbaneado por *${nn}*.`, m, rcanal, );
     } else {
         await conn.reply(m.chat, `🍭 El usuario no está registrado.`, m);
     }
