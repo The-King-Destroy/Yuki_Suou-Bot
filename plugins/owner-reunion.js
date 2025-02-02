f0a1f3aed80eafd660d2b05d7af37aab1c6ac6a2
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-02 09:35:32 -0400

    Update owner-reunion.js

--- plugins/owner-reunion.js
+++ plugins/owner-reunion.js
@@ -2,21 +2,14 @@ let handler = async (m, { conn, command, text }) => {
   if (!text) return m.reply(`🍬 Por favor, ingresa el motivo de la reunión.`);
   if (text.length < 11) return m.reply(`🍭 Por favor, ingresa al menos 11 caracteres.`);
 
-  let texto = `🍨 El Owner @${m.sender.split`@`[0]} ha comenzado una reunión. \n*➪ Motivo: ${text}*`;
+  let texto = `🍨 El Owner @${m.sender.split`@`[0]} ha empezado una reunión. \n*➪ Motivo: ${text}*`;
 
-  m.reply('🍭 Enviando mensaje de reunión a todos los owners y mods.');
-
-  const sendMessageToGlobal = async (group) => {
-    for (let [jid] of global[group]) {
-      let data = (await conn.onWhatsApp(jid))[0] || {};
-      if (data.exists) {
-        conn.sendMessage(data.jid, texto, { quoted: m });
-      }
+  for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
+    let data = (await conn.onWhatsApp(jid))[0] || {};
+    if (data.exists) {
+      conn.sendMessage(data.jid, texto, { quoted: m });
     }
-  };
-
-  await sendMessageToGlobal('owner');
-  await sendMessageToGlobal('mods');
+  }
 };
 
 handler.tags = ['owner'];
