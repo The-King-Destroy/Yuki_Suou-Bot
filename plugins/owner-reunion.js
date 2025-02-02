28569e94215cc1ca3a521b4af25f26c8aa383bd6
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-02 09:45:05 -0400

    Update owner-reunion.js

--- plugins/owner-reunion.js
+++ plugins/owner-reunion.js
@@ -1,19 +1,20 @@
-let handler = async (m, { conn, command, text }) => {
-  if (!text) return m.reply(`🍬 Por favor, ingresa el motivo de la reunión.`);
-  if (text.length < 11) return m.reply(`🍭 Por favor, ingresa al menos 11 caracteres.`);
-
-  let texto = `🍨 El Owner @${m.sender.split`@`[0]} ha empezado una reunión. \n*➪ Motivo: ${text}*`;
-
+let handler = async(m, { conn, command, text }) => {
+  if (!text) return m.reply(`🍬 Por favor, ingresa el motivo de la reunión.`)
+  if (text.length < 11) return m.reply(`🍭 Por favor, ingresa al menos 11 caracteres.`)
+  
+  let texto = `🍬 El Owner @${m.sender.split`@`[0]} ha empezado una reunión. Entra lo más pronto al grupo del staff...\n*➪ Motivo: ${text}*`
+  m.reply('🍭 Enviando mensaje de reunión a todos los owners.')
+  
   for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
-    let data = (await conn.onWhatsApp(jid))[0] || {};
+    let data = (await conn.onWhatsApp(jid))[0] || {}
     if (data.exists) {
-      conn.sendMessage(data.jid, texto, { quoted: m });
+      conn.sendMessage(data.jid, texto, m)
     }
   }
-};
+}
 
-handler.tags = ['owner'];
-handler.command = handler.help = ['reunion', 'reunionstaff'];
-handler.rowner = true;
+handler.tags = ['owner']
+handler.command = handler.help = ['reunion', 'reunionstaff']
+handler.rowner = true
 
-export default handler;
+export default handler
