802090d62612d90e84a3b0956fbcb154a04e5361
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-02 09:51:31 -0400

    Update owner-reunion.js

--- plugins/owner-reunion.js
+++ plugins/owner-reunion.js
@@ -2,8 +2,10 @@ let handler = async(m, { conn, command, text }) => {
   if (!text) return m.reply(`🍬 Por favor, ingresa el motivo de la reunión.`)
   if (text.length < 11) return m.reply(`🍭 Por favor, ingresa al menos 11 caracteres.`)
   
-  let texto = `🍬 El Owner @${m.sender.split`@`[0]} ha empezado una reunión. Entra lo más pronto al grupo del staff...\n*➪ Motivo: ${text}*`
+  let texto = `🍰 El Owner @${m.sender.split`@`[0]} ha empezado una reunión. Entra lo más pronto al grupo del staff...\n*➪ Motivo: ${text}*`
   m.reply('🍭 Enviando mensaje de reunión a todos los owners.')
+  
+  let mentions = [m.sender]
   
   for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
     let data = (await conn.onWhatsApp(jid))[0] || {}
