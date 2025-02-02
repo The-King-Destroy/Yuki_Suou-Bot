2ac0fa328ca97e674b46d247598d763fbebe143c
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-02 12:04:57 -0400

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -100,8 +100,9 @@ global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
-global.packsticker = `°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.\n\n✦ Usuario: ${nombre}\n✦ Creador: ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜\n✦ Fecha:${fecha}\n✦ Hora: ${tiempo}\n`;
-global.packsticker2 = `✦ Bot: ${botname}\n\n°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.`
+global.packsticker = `°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.\n\n✦ Bot: ${botname}\n✦ Usuario: ${nombre}
+✦ Creador: ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜\n✦ Fecha:${fecha}\n✦ Hora: ${tiempo}\n\n`;
+global.packsticker2 = `°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.\n\nѕτιϲκєя ϐγ: Yυƙι-Sυσυ-Bσƚ`
   
 //Fakes
 global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
