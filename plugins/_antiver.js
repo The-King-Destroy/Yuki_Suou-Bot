5d86b341030fb728815140ce70c90cc66aafce2d
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-03 16:36:48 -0400

    Update _antiver.js

--- plugins/_antiver.js
+++ plugins/_antiver.js
@@ -1,32 +1,21 @@
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'))
 
-let handler = m => m
-handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
-let media, msg, type
+export async function before(m, { v, isAdmin, isBotAdmin }) {
 const { antiver, isBanned } = global.db.data.chats[m.chat]
-if (!antiver || isBanned || !(m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension')) return
-if (m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension') {
-msg = m.mtype == 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message 
-type = Object.keys(msg)[0]
-if (m.mtype == 'viewOnceMessageV2') {
-media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'videoMessage' ? 'video' : 'audio')
-} else {
-media = await downloadContentFromMessage(msg[type], 'audio')
-}
-let buffer = Buffer.from([])
-for await (const chunk of media) {
-buffer = Buffer.concat([buffer, chunk])}
-const fileSize = formatFileSize(msg[type].fileLength)
+if (!antiver || isBanned) return
+if (m.viewOnce) {
+let buffer = await m.download(false);
+let type = m.mtype.replace('Message', '');
+const fileSize = formatFileSize(Buffer.byteLength(buffer));
 const description = `
 ✅️ *ANTI VER UNA VEZ* ✅️\n\n💭 *No ocultes* ${type === 'imageMessage' ? '`Imagen` 📷' : type === 'videoMessage' ? '`Vídeo` 🎥' : type === 'audioMessage' ? '`Mensaje de voz` 🎤' : 'este mensaje'}\n- ✨️ *Usuario:* *@${m.sender.split('@')[0]}*
-${msg[type].caption ? `- *Texto:* ${msg[type].caption}` : ''}`.trim()
+${m.caption ? `- *Texto:* ${m.caption}` : ''}`.trim()
 if (/image|video/.test(type)) return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] })
 if (/audio/.test(type)) { 
 await conn.reply(m.chat, description, m, { mentions: [m.sender] }) 
 await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
 }
 }}
-export default handler
 
 function formatFileSize(bytes) {
 const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY']
