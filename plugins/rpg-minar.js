3c325aba90e1bc859526dec5a13110fc57452be5
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-06 09:11:49 -0400

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -29,7 +29,7 @@ let info = `⛏️ *Te has adentrando en lo profundo de las cuevas*\n\n` +
 `🕋 *Carbón*: ${coal}\n` +
 `🪨 *Piedra*: ${stone}`;
 
-await conn.sendFile(m.chat, img, 'yuki.jpg', info, m);
+await conn.sendFile(m.chat, img, 'yuki.jpg', info, fkontak);
 await m.react('⛏️');
 
 user.health -= 50;
