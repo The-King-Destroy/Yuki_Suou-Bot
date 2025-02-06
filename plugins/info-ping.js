3a226ed1e29be87ed4296c8a4df0f5f16e610e20
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-06 19:37:54 -0400

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          conn.reply(m.chat, `✰ *¡Pong!*\n> ${latensi.toFixed(4)}ms`, m);
+          conn.reply(m.chat, `✰ *¡Pong!*\n> Tiempo ⴵ ${latensi.toFixed(4)}ms`, m);
             });
 }
 handler.help = ['ping']
