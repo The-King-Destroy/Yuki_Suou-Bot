a8178c284e038070202beaeb9c1d6da55d4a90a1
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-06 09:02:32 -0400

    Update rpg-heal.js

--- plugins/rpg-heal.js
+++ plugins/rpg-heal.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn }) => {
     if (!user) {
         return conn.reply(m.chat, `${emoji} El usuario no se encuentra en la base de Datos.`, m);
     }
-    if (user.coin < 30) {
+    if (user.coin < 50) {
         return conn.reply(m.chat, `💔 Su saldó fue insuficiente para curarte. Necesitas al menos 20.`, m);
     }
     let healAmount = 50; 
     user.health += healAmount;
-    user.coin -= 30; 
+    user.coin -= 50; 
     if (user.health > 100) {
         user.health = 100; 
     }
