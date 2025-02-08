099ba6f49b03d192d4b8aa150ec055196386f3f2
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-08 10:22:29 -0400

    Update descargas-twitter.js

--- plugins/descargas-twitter.js
+++ plugins/descargas-twitter.js
@@ -1,48 +1,35 @@
 import axios from 'axios';
 let enviando = false;
 
-const handler = async (m, {conn, text, usedPrefix, command, args}) => {
-    if (!args || !args[0]) return conn.reply(m.chat, `${emoji} Te faltó el link de una imagen/video de twitter.`, m)
+const handler = async (m, { conn, text, usedPrefix, command, args }) => {
+    if (!args || !args[0]) return conn.reply(m.chat, `${emoji} Te faltó el link de una imagen/video de twitter.`, m);
     if (enviando) return; 
     enviando = true;
 
     try {
- 
         const apiResponse = await axios.get(`https://delirius-apiofc.vercel.app/download/twitterdl?url=${args[0]}`);
         const res = apiResponse.data;
 
+        const caption = res.caption ? res.caption : `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`;
+
         if (res?.type === 'video') {
-            
-            const caption = res.caption ? res.caption : `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`;
-            for (let i = 0; i < res.media.length; i++) {
-
-                await conn.sendMessage(m.chat, { video: { url: res.media[i].url }, caption: caption }, { quoted: m });
-            }
-
-            enviando = false;
-            return;
-        
+            await conn.sendMessage(m.chat, { video: { url: res.media[0].url }, caption: caption }, { quoted: m });
         } else if (res?.type === 'image') {
-            
-            const caption = res.caption ? res.caption : `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`;
-            for (let i = 0; i < res.media.length; i++) {
-                await conn.sendMessage(m.chat, { image: { url: res.media[i].url }, caption: caption }, { quoted: m });
-            }
-
-            enviando = false;
-            return;
+            await conn.sendMessage(m.chat, { image: { url: res.media[0].url }, caption: caption }, { quoted: m });
         }
 
+        enviando = false;
+        return;
+
     } catch (error) {
-        
         enviando = false;
         console.error(error);         
         conn.reply(m.chat, `${msm} Error al descargar su archivo`, m);
     }
 };
 
-handler.help = ['twitter <url>']
-handler.tags = ['dl']
-handler.command = ['x', 'xdl', 'dlx', 'twdl', 'tw', 'twt', 'twitter']
+handler.help = ['twitter <url>'];
+handler.tags = ['dl'];
+handler.command = ['x', 'xdl', 'dlx', 'twdl', 'tw', 'twt', 'twitter'];
 
 export default handler;
