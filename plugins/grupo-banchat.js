cc491f3ddab7b342eb1e8de6e559bf30d6a79325
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-08 16:04:17 -0400

    Update grupo-banchat.js

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -7,5 +7,6 @@ conn.reply(m.chat, `${emoji} Has *desactivado* a *${botname}*!`, m)
 handler.help = ['banchat']
 handler.tags = ['grupo']
 handler.command = ['banchat', 'off']
+handler.admin = true
 
 export default handler
