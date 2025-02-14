9ef9a10cd9fe94b35b100ee8652598e42b266adc
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-14 10:54:13 -0400

    Update rpg-canjear.js

--- plugins/rpg-canjear.js
+++ plugins/rpg-canjear.js
@@ -30,6 +30,8 @@ let handler = async (m, { conn, text }) => {
 
 handler.help = ['canjear <código>'];
 handler.tags = ['economia'];
-handler.command = ['canjear']
+handler.command = ['canjear'];
+handler.group = true;
+handler.register = true;
 
 export default handler;
