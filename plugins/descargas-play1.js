2b1d0aeb0344c137ec8883ed32f49950dc9280a3
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-14 09:27:30 -0400

    Update descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+/*import fetch from "node-fetch";
 import yts from 'yt-search';
 import axios from "axios";
 
@@ -158,4 +158,4 @@ function formatViews(views) {
   } else {
     return views.toString();
   }
-}
+}*/
