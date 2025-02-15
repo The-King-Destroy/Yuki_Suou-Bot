fd497efdcefb3aa98a681fcaaddc5aaa7a5c4d62
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-15 16:51:08 -0400

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -39,7 +39,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     
     } else if (command === 'yta' || command === 'ytmp3dl') {
     m.react(rwait)
-      let audio = await (await fetch(`https://api.botcahx.eu.org/api/download/get-YoutubeResult?url=${videoInfo.url}&type=video&xky=sxadCfpz`)).buffer()
+      let audio = await (await fetch(`https://api.botcahx.eu.org/api/download/get-YoutubeResult?url=${videoInfo.url}&type=audio&xky=sxadCfpz`)).buffer()
       conn.sendFile(m.chat, audio, videoInfo.title, '', m, null, { mimetype: "audio/mpeg", asDocument: false })
     m.react(done)
     } else if (command === 'ytv' || command === 'ytmp4dl') {
