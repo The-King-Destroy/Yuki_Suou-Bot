1a71289f9344674db3801b083c6c9a9b7073534d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-02-16 00:31:49 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -59,8 +59,8 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.command = ['play1', 'ytmp3dl', 'ytmp4dl']
 //handler.command = handler.help = ['play', 'playvid', 'ytv', 'ytmp4', 'yta', 'play2', 'ytmp3'];
 handler.tags = ['dl'];
-handler.group = true;
-handler.register = true;
+//handler.group = true;
+//handler.register = true;
 
 export default handler;
 
