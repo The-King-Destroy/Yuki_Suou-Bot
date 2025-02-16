35b8a48ab7311b8002d62a077389a0c795072d81
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-15 23:24:34 -0400

    Create stickers-ttp_attp.js

---
+++ plugins/stickers-ttp_attp.js
@@ -0,0 +1,32 @@
+import { sticker } from '../lib/sticker.js'
+import fetch from 'node-fetch'
+import axios from 'axios';
+
+let handler = async(m, { conn, text, args, usedPrefix, command }) => {
+    if (!text) return m.reply(`${emoji} Por favor, ingresa Un texto para crear un sticker.`)
+    let teks = encodeURI(text)
+    if (command == 'attp') {
+        const data = {
+            text: `${text}`
+        };
+        const response = await axios.post('https://salism3api.pythonanywhere.com/text2gif', data);
+        const x=response.data.image;
+        let stiker = await sticker(null,x,global.packsticker, global.packsticker2)
+        conn.sendFile(m.chat, stiker, null, { asSticker: true })
+    }
+    if (command == 'ttp') {
+        const data = {
+            text: `${text}`,"outlineColor":"255,0,0,255", "textColor":"0,0,0,255"
+        };
+        const response = await axios.post('https://salism3api.pythonanywhere.com/text2img', data);
+        const x=response.data.image;
+        let stiker = await sticker(null,x,global.packsticker, global.packsticker2)
+        conn.sendFile(m.chat, stiker, null, { asSticker: true })
+    }
+}
+
+handler.tags = ['sticker']
+handler.help = ['ttp', 'attp']
+handler.command = ['ttp', 'attp']
+
+export default handler
