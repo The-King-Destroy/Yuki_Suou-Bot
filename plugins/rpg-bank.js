4cb4f2f37568ed3e742c7aaa28ffe4855aead46b
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 14:44:12 -0400

    Update rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -5,14 +5,14 @@
 import fetch from 'node-fetch'
 import db from '../lib/database.js'
 
-let img = 'https://files.catbox.moe/al4kc8.jpg'
 let handler = async (m, {conn, usedPrefix}) => {
    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
+   let img = 'https://files.catbox.moe/al4kc8.jpg'
    if (who == conn.user.jid) return m.react('✖️')
    if (!(who in global.db.data.users)) return m.reply(`${emoji4} El usuario no se encuentra en mi base de datos.`)
    let user = global.db.data.users[who]
    let name = conn.getName(who);
-   let txt = (`${who == m.sender ? `╭━〔  ⪛ ʙᴀɴᴄᴏ ᴄᴇɴᴛʀᴀʟ ⪜  〕⬣\n┋ 👤 *Usuario:* ${name}\n┋ 💸 *${moneda} En Cartera*: ${user.coin}\n┋ 🏦 *${moneda} En Banco*: ${user.bank}\n┋ ✨ *Experiencia:* ${user.exp}\n┋ 📅 *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣`}`)
+   let txt = (`${who == m.sender ? `╭━〔  ⪛ ʙᴀɴᴄᴏ ᴄᴇɴᴛʀᴀʟ ⪜  〕⬣\n┋ 👤 *Cliente:* ${name}\n┋ 💸 *${moneda} En Cartera*: ${user.coin}\n┋ 🏦 *${moneda} En Banco*: ${user.bank}\n┋ ✨ *Experiencia:* ${user.exp}\n┋ 📅 *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣` : `╭━〔  ⪛ ʙᴀɴᴄᴏ ᴄᴇɴᴛʀᴀʟ ⪜  〕⬣\n┋ 👤 *Cliente:* @${who.split('@')[0]}\n┋ 💸 *${moneda} En Cartera*: ${user.coin}\n┋ 🏦 *${moneda} En Banco*: ${user.bank}\n┋ *✨ Experiencia:* ${user.exp}\n┋ 📅 *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣`}`)
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, fkontak, null, {mentions: [who] })
 }
 
@@ -22,4 +22,4 @@ handler.command = ['bank', 'banco']
 handler.register = true 
 handler.group = true
 
-export default handler 
+export default handler
