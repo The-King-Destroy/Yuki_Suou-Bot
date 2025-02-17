dbd3b85a807c940327b54ab2f4d39d48d6791ceb
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 01:30:56 -0400

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -11,7 +11,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
 handler.tags = ['owner'];
 handler.help = ['autoadmin'];
 handler.command = ['autoadmin'];
-handler.mods = true;
+handler.rowner = true;
 handler.group = true;
 handler.botAdmin = true;
 
