7d97ebdd8b0f63fbe19e2e50b56cafc4a3753780
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 01:36:23 -0400

    Update owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -43,6 +43,6 @@ await conn.reply(m.chat, `${msm} Ocurrió un fallo.`, m)
 handler.help = ['dsowner']
 handler.tags = ['owner']
 handler.command = ['delai', 'dsowner', 'clearallsession']
-handler.mods = true
+handler.rowner = true;
 
 export default handler
