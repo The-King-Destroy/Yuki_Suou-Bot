5ed66458db812569dc65f45a734a50208c58e215
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 10:01:50 -0400

    Update grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command, args }) => {
   if (!(m.chat in global.db.data.chats)) {
     return conn.reply(m.chat, `《✦》¡Este chat no está registrado!.`, m);
   }
@@ -6,19 +6,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let chat = global.db.data.chats[m.chat];
 
   if (command === 'bot') {
-    if (m.args[0] === 'off') {
-      if (chat.isBanned) {
-        return conn.reply(m.chat, `《✧》*${botname}* ya estaba desactivado.`, m);
-      }
-      chat.isBanned = true;
-      await conn.reply(m.chat, `✐ Has *desactivado* a *${botname}*!`, m);
-    } else if (m.args[0] === 'on') {
-      if (!chat.isBanned) {
-        return conn.reply(m.chat, `《✧》*${botname}* ya estaba activado.`, m);
-      }
-      chat.isBanned = false;
-      await conn.reply(m.chat, `✐ Has *activado* a *${botname}*!`, m);
-    } else {
+    if (args.length === 0) {
       const estado = chat.isBanned ? '✗ Desactivado' : '✓ Activado';
       const info = `
 「✦」Un administrador puede activar o desactivar a *${botname}* utilizando:
@@ -29,6 +17,20 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 ✧ Estado actual » *${estado}*
 `;
       return conn.reply(m.chat, info, m);
+    }
+
+    if (args[0] === 'off') {
+      if (chat.isBanned) {
+        return conn.reply(m.chat, `《✧》*${botname}* ya estaba desactivado.`, m);
+      }
+      chat.isBanned = true;
+      return conn.reply(m.chat, `✐ Has *desactivado* a *${botname}*!`, m);
+    } else if (args[0] === 'on') {
+      if (!chat.isBanned) {
+        return conn.reply(m.chat, `《✧》*${botname}* ya estaba activado.`, m);
+      }
+      chat.isBanned = false;
+      return conn.reply(m.chat, `✐ Has *activado* a *${botname}*!`, m);
     }
   }
 };
