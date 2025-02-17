ed47d8ed688de4e73098026e45c9962e279a3765
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 10:35:54 -0400

    Update _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -1,4 +1,4 @@
-/*export async function before(m) {
+export async function before(m) {
   if (!m.text || !global.prefix.test(m.text)) {
     return;
   }
@@ -8,7 +8,7 @@
 
   const validCommand = (command, plugins) => {
     for (let plugin of Object.values(plugins)) {
-     if (plugin.command && (Array.isArray(plugin.command) ? plugin.command : [plugin.command]).includes(command)) {
+      if (plugin.command && (Array.isArray(plugin.command) ? plugin.command : [plugin.command]).includes(command)) {
         return true;
       }
     }
@@ -18,14 +18,19 @@
   if (validCommand(command, global.plugins)) {
     let chat = global.db.data.chats[m.chat];
     let user = global.db.data.users[m.sender];
-    if (chat.isBanned) return;
+    
+    if (chat.isBanned) {
+      const avisoDesactivado = `《✧》El bot *${botname}* está desactivado en este grupo.\n\n> ✦ Un *administrador* puede activarlo con el comando:\n> » *${usedPrefix}bot on*`;
+      await m.reply(avisoDesactivado);
+      return;
+    }
+    
     if (!user.commands) {
       user.commands = 0;
     }
     user.commands += 1;
-   // await conn.sendPresenceUpdate('composing', m.chat);
   } else {
-   const comando = m.text.trim().split(' ')[0];
-   await m.reply(`《✧》El comando *${comando}* no existe.\nPara ver la lista de comandos usa:\n» *#help*`);
+    const comando = m.text.trim().split(' ')[0];
+    await m.reply(`《✧》El comando *${comando}* no existe.\nPara ver la lista de comandos usa:\n» *#help*`);
   }
-}*/
+}
