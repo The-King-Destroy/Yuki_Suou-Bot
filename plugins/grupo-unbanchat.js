d3b14464fe53270919df963e78d537da3395c16c
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 09:51:16 -0400

    Update grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,13 +1,39 @@
-let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, `${emoji} ¡Este chat no está registrado!.`, m)
-let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, `${emoji} *${botname}* ya estába activado.`, m)
-chat.isBanned = false
-await conn.reply(m.chat, `${emoji} Has *activado* a *${botname}*!`, m)
-}
-handler.help = ['unbanchat'];
-handler.tags = ['grupo'];
-handler.command = ['unbanchat', 'on']
-handler.admin = true
+let handler = async (m, { conn, usedPrefix, command }) => {
+  if (!(m.chat in global.db.data.chats)) {
+    return conn.reply(m.chat, `《✦》¡Este chat no está registrado!.`, m);
+  }
 
-export default handler
+  let chat = global.db.data.chats[m.chat];
+
+  if (command === 'off') {
+    if (chat.isBanned) {
+      return conn.reply(m.chat, `《✧》*${botname}* ya estaba desactivado.`, m);
+    }
+    chat.isBanned = true;
+    await conn.reply(m.chat, `✐ Has *desactivado* a *${botname}*!`, m);
+  } else if (command === 'on') {
+    if (!chat.isBanned) {
+      return conn.reply(m.chat, `《✧》*${botname}* ya estaba activado.`, m);
+    }
+    chat.isBanned = false;
+    await conn.reply(m.chat, `✐ Has *activado* a *${botname}*!`, m);
+  } else {
+    const estado = chat.isBanned ? '✗ Desactivado' : '✓ Activado';
+    const instrucciones = `
+「✦」Un administrador puede activar o desactivar a *${botname}* utilizando:
+
+> ✐ *${usedPrefix}bot on* para activar
+> ✐ *${usedPrefix}bot off* para desactivar
+
+✧ Estado actual » *${estado}*
+`;
+    return conn.reply(m.chat, instrucciones, m);
+  }
+};
+
+handler.help = ['bot'];
+handler.tags = ['grupo'];
+handler.command = ['bot'];
+handler.admin = true;
+
+export default handler;
