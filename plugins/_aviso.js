5fb4107cf1be2fac880901041ba7981db5086ad8
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 10:10:59 -0400

    Update _aviso.js

--- plugins/_aviso.js
+++ plugins/_aviso.js
@@ -6,14 +6,19 @@ export async function before(m) {
   const usedPrefix = global.prefix.exec(m.text)[0];
   const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
 
-  if (command === "bot" || command === "unbanchat") {
+  if (command === "bot") {
     return;
   }
 
   let chat = global.db.data.chats[m.chat];
 
   if (chat.isBanned) {
-    const avisoDesactivado = `《✧》El bot *${botname}* está desactivado en este grupo.\n\n> ✦ Un *administrador* puede activarlo con el comando:\n> » *#on* o *#unbanchat*`;
+    const avisoDesactivado = `《✧》El bot *${botname}* está desactivado en este grupo.\n\n> ✦ Un *administrador* puede activarlo con el comando:\n> » *${usedPrefix}bot on*`;
+    
+    if (!global.validCommands.includes(command)) {
+      return;
+    }
+    
     await m.reply(avisoDesactivado);
     return;
   }
