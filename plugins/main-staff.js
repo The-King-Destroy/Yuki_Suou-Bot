5763384dfb6f81c4274fc559714e9e0139621c54
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 00:49:34 -0400

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -3,35 +3,35 @@ let img = './src/catalogo.jpg'
 let staff = `ᥫ᭡ *EQUIPO DE AYUDANTES* ❀
 ✰ *Dueño* ${creador}
 ✦ *Bot:* ${botname}
-🜸 *Versión:* ${vs}
+⚘ *Versión:* ${vs}
 ❖ *Libreria:* ${libreria} ${baileys}
 
 ❍ *Creador:*
 
 ᰔᩚ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜
-> ✏ Rol » *Creador*
+> 🜸 Rol » *Creador*
 > ✧ GitHub » https://github.com/The-King-Destroy
 
 ❒ *Colaboradores:*
 
 ᰔᩚ 𝓔𝓶𝓶𝓪 𝓥𝓲𝓸𝓵𝓮𝓽𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷 
-> ✏ Rol » *Developer*
+> 🜸 Rol » *Developer*
 > ✧ GitHub » https://github.com/Elpapiema
 
 ᰔᩚ Niño Piña
-> ✏ Rol » *Developer*
+> 🜸 Rol » *Developer*
 > ✧ GitHub » https://github.com/WillZek
 
 ✧ ☆꧁༒ĹєǤ𝒆𝐧𝐃༒꧂☆
-> ✏ Rol » *Developer*
+> 🜸 Rol » *Developer*
 > ✧ GitHub » https://github.com/Diomar-s
 
 ᰔᩚ I'm Fz' (Tesis)
-> ✏ Rol » *Developer*
+> 🜸 Rol » *Developer*
 > ✧ GitHub » https://github.com/FzTeis
 
 ᰔᩚ Legna
-> ✏ Rol » *Mini-Dev* 
+> 🜸 Rol » *Mini-Dev* 
 > ✧ GitHub » https://github.com/Legna-chan
 `
 await conn.sendFile(m.chat, img, 'yuki.jpg', staff.trim(), fkontak)
