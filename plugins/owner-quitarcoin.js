9447cc90f63e1a4ca4925997e9022161c6d2002f
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-17 01:33:26 -0400

    Update owner-quitarcoin.js

--- plugins/owner-quitarcoin.js
+++ plugins/owner-quitarcoin.js
@@ -45,6 +45,6 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['quitarcoin *<@user>*', 'quitarcoin all'];
 handler.tags = ['owner'];
 handler.command = ['quitarcoin', 'removecoin', 'removecoins']; 
-handler.mods = true;
+handler.rowner = true;
 
 export default handler;
