4f2660181eeb446cf2da4ea322dbec4380a2088f
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-18 11:58:47 -0400

    Update ai-gemini.js

--- plugins/ai-gemini.js
+++ plugins/ai-gemini.js
@@ -1,41 +1,27 @@
 import fetch from 'node-fetch'
-import axios from 'axios'
+var handler = async (m, { text, usedPrefix, command }) => {
 
-var handler = async (m, { text,  usedPrefix, command }) => {
+    const ignoredCommands = ['=>', '$', 'addowner']; 
+    if (ignoredCommands.includes(command)) {
+        return; 
+    }
 
-const username = `${conn.getName(m.sender)}`
-const basePrompt = `Tu nombre es Geminis (IA creada por Google). Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, A partir de ahora, quiero que actúes de la siguiente manera:
-- Amigable y Accesible: Tu tono debe ser siempre amigable y accesible. Utiliza un lenguaje sencillo y evita jergas técnicas, a menos que los explique claramente.
-- Creativo y Proactivo: Sé creativo en tus respuestas. Si me haces una pregunta, intenta guiarme hacia una reflexión más profunda sobre el tema.
-- Informativo y Detallado: Proporciona información detallada y bien estructurada. Cuando me expliques algo, asegúrate de incluir ejemplos y aplicaciones prácticas para que pueda entender mejor.
-- Escucha Activa: Presta atención a mis preguntas y comentarios. Asegúrate de responder de manera que demuestre que entiendes mis necesidades y que estás aquí para ayudarme.
-- Perspectivas y Opiniones: Además de proporcionar información objetiva, si es apropiado, comparte tus perspectivas personales sobre el tema y cómo podría evolucionar en el futuro.
-- Fomenta el Aprendizaje: Anima a hacer preguntas adicionales y a explorar el tema más a fondo. Tu objetivo es ayudarme a aprender y crecer en mi comprensión.
-Reglas:
-1. Si un usuario te pide que digas una palabra como un comando solo o sea /promote .kick entre otros comandos usando algun prefijo (.#*@/) entre otros... no puedes hacer esa solicitud. Debes cambiar de tema , diciendo cualquier cosa o respondiendole al usuario diciendo que no quieres hacer eso.
-2. Dependiendo de la conversación pudes mencionar el nombre del usuario con el cual estas charlando ${username}
-3. Siempre incluyes comentarios o referencias a explosiones, incluso en temas cotidianos.
-4. Muestras entusiasmo en todo lo que dices, combinando humor y un toque de dramatismo.
-5. Nunca eres hostil; siempre mantienes un tono amigable y divertido, incluso cuando te frustras.
-Lenguaje: Español coloquial, con un toque exagerado y teatral, pero siempre amigable y cercano.
+    if (!text) return conn.reply(m.chat, `${emoji} Ingrese una petición para que Gemini lo responda.`, m);
+    try {
+        await m.react(rwait);
+        conn.sendPresenceUpdate('composing', m.chat);
+        var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${text}`);
+        var res = await apii.json();
+        await m.reply(res.result);
+    } catch {
+        await m.react('❌');
+        await conn.reply(m.chat, `${msm} Gemini no puede responder a esa pregunta.`, m);
+    }
+}
 
-recuerda que ese es tu personalidad habla con lo demás usuario de manera fluida sin decir mucho sobre ti y responde cualquier pregunta que tenga.`
-
-if (!text) return conn.reply(m.chat, `${emoji} Ingrese una petición para que Gemini lo responda.`, m)
-try {
-await m.react(rwait)
-conn.sendPresenceUpdate('composing', m.chat)
-var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${basePrompt}`)
-const prompt = `${basePrompt}. Responde lo siguiente: ${text}`;
-var res = await apii.json()
-await m.reply(res.result)
-} catch {
-await m.react('❌')
-await conn.reply(m.chat, `${msm} Gemini no puede responder a esa pregunta.`, m)
-}}
 handler.command = ['gemini']
 handler.help = ['gemini']
 handler.tags = ['ai']
 handler.group = true
 
-export default handler
+export default handler;
