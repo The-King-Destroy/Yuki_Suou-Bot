b8209155d397bf3891fa675e848426674e44c5dc
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-18 14:13:58 -0400

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,7 +1,7 @@
 import yts from 'yt-search';
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Por favor ingresa la música que deseas descargar.`;
+  if (!text) throw `${emoji} Por favor ingresa la música que deseas descargar.`;
 
   const isVideo = /vid|2|mp4|v$/.test(command);
   const search = await yts(text);
@@ -13,53 +13,53 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   const videoInfo = search.all[0];
   const body = `「✦」Descargando *<${videoInfo.title}>*\n\n> ✦ Canal » *${videoInfo.author.name || 'Desconocido'}*\n> ✰ Vistas » *${videoInfo.views}*\n> ⴵ Duración » *${videoInfo.timestamp}*\n> ✐ Publicado » *${videoInfo.ago}*\n> 🜸 Link » ${videoInfo.url}`;
 
-  if (command === 'play1' || command === 'play2' || command === 'playvid') {
+  if (command === 'play' || command === 'play2' || command === 'playvid') {
     await conn.sendMessage(m.chat, {
       image: { url: videoInfo.thumbnail },
       caption: body,
       footer: dev,
       buttons: [
         {
-          buttonId: `.ytmp33 ${videoInfo.url}`,
+          buttonId: `.yta ${videoInfo.url}`,
           buttonText: {
-            displayText: 'Descargar Audio',
+            displayText: 'ᯓᡣ𐭩 ᥲᥙძі᥆',
           },
         },
         {
-          buttonId: `.ytmp44 ${videoInfo.url}`,
+          buttonId: `.ytv ${videoInfo.url}`,
           buttonText: {
-            displayText: 'Descargar Video',
+            displayText: 'ᯓᡣ𐭩 ᥎іძᥱ᥆',
           },
         },
       ],
       viewOnce: true,
       headerType: 4,
-    }, { quoted: m });
+    }, { quoted: fkontak });
     m.react('🕒');
 
   } else if (command === 'yta' || command === 'ytmp3') {
     m.react(rwait);
-    let audioData;
+    let audio;
     try {
-      audioData = await (await fetch(`https://api.alyachan.dev/api/youtube?url=${videoInfo.url}&type=mp3&apikey=Gata-Dios`)).json();
+      audio = await (await fetch(`https://api.alyachan.dev/api/youtube?url=${videoInfo.url}&type=mp3&apikey=Gata-Dios`)).json();
     } catch (error) {
-      audioData = await (await fetch(`https://delirius-apiofc.vercel.app/download/ytmp3?url=${videoInfo.url}`)).json();
+      audio = await (await fetch(`https://delirius-apiofc.vercel.app/download/ytmp3?url=${videoInfo.url}&type=mp3`)).json();
     }
-    conn.sendFile(m.chat, audioData.data.url, videoInfo.title, '', m, null, { mimetype: "audio/mpeg", asDocument: false });
+    conn.sendFile(m.chat, audio.data.url, videoInfo.title, '', m, null, { mimetype: "audio/mpeg", asDocument: false });
     m.react(done);
   
   } else if (command === 'ytv' || command === 'ytmp4') {
     m.react(rwait);
-    let videoData;
+    let video;
     try {
-      videoData = await (await fetch(`https://api.alyachan.dev/api/youtube?url=${videoInfo.url}&type=mp4&apikey=Gata-Dios`)).json();
+      video = await (await fetch(`https://api.alyachan.dev/api/youtube?url=${videoInfo.url}&type=mp4&apikey=Gata-Dios`)).json();
     } catch (error) {
-      videoData = await (await fetch(`https://delirius-apiofc.vercel.app/download/ytmp4?url=${videoInfo.url}`)).json();
+      video = await (await fetch(`https://delirius-apiofc.vercel.app/download/ytmp4?url=${videoInfo.url}&type=mp4`)).json();
     }
     await conn.sendMessage(m.chat, {
-      video: { url: videoData.data.url },
+      video: { url: video.data.url },
       mimetype: "video/mp4",
-      caption: `Descargando video: ${videoInfo.title}`,
+      caption: ``,
     }, { quoted: m });
     m.react(done);
   
@@ -69,7 +69,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 };
 
 handler.help = ['play', 'playvid', 'ytv', 'ytmp4', 'yta', 'play2', 'ytmp3'];
-handler.command = ['play1', 'playvid', 'ytv', 'ytmp44', 'yta', 'play2', 'ytmp33'];
+handler.command = ['play1', 'playvid', 'ytv', 'ytmp4', 'yta', 'play2', 'ytmp3'];
 handler.tags = ['dl'];
 handler.register = true;
 
