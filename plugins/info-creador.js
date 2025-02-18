3255af909747aece05a1349172d89d1a3f337b57
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-18 15:36:29 -0400

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,8 +11,8 @@ let handler = async (m, { conn }) => {
   let name = await conn.getName(who);
 
   await sendContactArray(conn, m.chat, [
-    [`${conn.user.jid.split('@')[0]}`, `Es Un Bot`, packname, dev, redes, `Sabra Dios 🫏`, md, bio],
-    [`${suittag}`, `Propietario`, botname, `No Hacer Spam`, correo, `Venezuela`, channel, biobot]
+    [`${conn.user.jid.split('@')[0]}`, `✦ Es Un Bot`, packname, dev, redes, `Sabra Dios 🫏`, md, bio],
+    [`${suittag}`, `ᰔᩚ Propietario`, botname, `❀ No Hacer Spam`, correo, `⊹˚• Venezuela •˚⊹`, channel, biobot]
   ], m);
 }
 
