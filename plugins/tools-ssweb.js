a679128efd83873e04e44a7f2b1671b74606e831
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-18 08:21:00 -0400

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,21 +1,19 @@
 import fetch from 'node-fetch'
+
 let handler = async (m, { conn, command, args }) => {
 if (!args[0]) return conn.reply(m.chat, `${emoji} Por favor, ingrese el Link de una página.`, m)
 try {
 await m.react(rwait)
-conn.reply(m.chat, `${emoji2} Buscando su información....`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons, 
-sourceUrl: channel }}})
+conn.reply(m.chat, `${emoji2} Buscando su información....`, m)
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
 await m.react(done)
 } catch {
 return conn.reply(m.chat, `${msm} Ocurrió un error.`, m)
 await m.react(error)}}
+
 handler.help = ['ssweb', 'ss']
 handler.tags = ['tools']
 handler.command = ['ssweb', 'ss']
+
 export default handler
