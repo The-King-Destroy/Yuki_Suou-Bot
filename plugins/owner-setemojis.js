41fd52db2b216d8e007a873f92b6c821bb297ff3
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-19 19:19:36 -0400

    Update owner-setemojis.js

--- plugins/owner-setemojis.js
+++ plugins/owner-setemojis.js
@@ -1,14 +1,14 @@
-let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply(`${emoji} Por favor, proporciona los nuevos emojis para el bot, separados por "/".\n> Ejemplo: #setemoji 🍬/🍭/🍫/🍩/🍪`);
+let handler = async (m, { conn, text }) => {
+  if (!text) return m.reply(`🍬 Por favor, proporciona los nuevos emojis para el bot, separados por "/".\n> Ejemplo: #setemoji 🍬/🍭/🍫/🍩/🍪`);
 
-  const welcomes = text.split('/').map(msg => msg.trim()).slice(0, 5); 
-  global.emoji = emojis[0];
-  global.emoji2 = emojis[1];
-  global.emoji3 = emojis[2];
-  global.emoji4 = emojis[3];
-  global.emoji5 = emojis[4];
+  const emojis = text.split('/').map(msg => msg.trim()).slice(0, 5);
+  global.emoji = emojis[0] || '';
+  global.emoji2 = emojis[1] || '';
+  global.emoji3 = emojis[2] || '';
+  global.emoji4 = emojis[3] || '';
+  global.emoji5 = emojis[4] || '';
 
-  m.reply(`✎ Los emojis del bot han sido cambiadas a:\n1. ${emoji}\n2. ${emoji2}\n3. ${emoji3}\n4. ${emoji4}\n5. ${emoji5}`);
+  m.reply(`✎ Los emojis del bot han sido cambiados a:\n1. ${global.emoji}\n2. ${global.emoji2}\n3. ${global.emoji3}\n4. ${global.emoji4}\n5. ${global.emoji5}`);
 };
 
 handler.help = ['setemojis'];
