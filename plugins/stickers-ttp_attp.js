be64031594129bef6a8b3945a604741891ea7294
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-20 18:00:01 -0400

    Update stickers-ttp_attp.js

--- plugins/stickers-ttp_attp.js
+++ plugins/stickers-ttp_attp.js
@@ -11,7 +11,7 @@ let handler = async(m, { conn, text, args, usedPrefix, command }) => {
         };
         const response = await axios.post('https://salism3api.pythonanywhere.com/text2gif', data);
         const x=response.data.image;
-        let stiker = await sticker(null,x,global.packname, global.nombre)
+        let stiker = await sticker(null,x,global.botname, global.nombre)
         conn.sendFile(m.chat, stiker, null, { asSticker: true })
     }
     if (command == 'ttp') {
