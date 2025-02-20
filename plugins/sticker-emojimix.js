34d81788298e18850ef0fc7df535547db1350dde
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-20 18:03:19 -0400

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -16,7 +16,7 @@ if (!args[0]) return m.reply(`📌 Ejemplo: *${usedPrefix + command}* 😎+🤑`
 let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
-let stiker = await sticker(false, res.url, global.packsticker, global.packsticker2)
+let stiker = await sticker(false, res.url, global.botname, global.nombre)
 conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
 }}
 handler.help = ['emojimix *<emoji+emoji>*']
