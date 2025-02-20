a42ccc561c8417354385a92155ce069bef1be915
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-20 09:33:55 -0400

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,21 +4,21 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 
 let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad.....*
 
-${namegrupo}
-*❀* ${gp1}
+- ${namegrupo}
+> *❀* ${gp1}
 
 ${namecomu}
-*❀* ${comunidad1}
+> *❀* ${comunidad1}
 
 *ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ*
 
 ⚘ Enlace anulado? entre aquí! 
 
 - ${namechannel}
-*❀* ${channel}
+> *❀* ${channel}
 
 - ${namechannel2}
-*❀* ${channel2}
+> *❀* ${channel2}
 
 > ${dev}`
 
