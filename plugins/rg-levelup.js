4c849c076114b13010afb08702b46ada9ee57cda
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-21 13:26:42 -0400

    Update rg-levelup.js

--- plugins/rg-levelup.js
+++ plugins/rg-levelup.js
@@ -21,8 +21,8 @@ let handler = async (m, { conn }) => {
     if (before !== user.level) {
         let txt = `ᥫ᭡ Felicidades Has subido de nivel ❀\n\n`; 
         txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`;
-        txt += `• ★ *Nivel anterior* : ${before}\n`;
-        txt += `• ✰ *Nuevos niveles* : ${user.level}\n`;
+        txt += `• ✰ *Nivel anterior* : ${before}\n`;
+        txt += `• ✦ *Nuevos niveles* : ${user.level}\n`;
         txt += `• ❖ *Fecha* : ${new Date().toLocaleString('id-ID')}\n\n`;
         txt += `> ➨ Nota: *Cuanto más interactúes con el Bot, mayor será tu nivel.*`;
         await conn.sendMessage(m.chat, { text: txt }, { quoted: m });
@@ -35,8 +35,9 @@ let handler = async (m, { conn }) => {
         let rank = sortedLevel.findIndex(u => u.jid === who) + 1;
 
         let txt = `*「✿」Usuario* ◢ ${name} ◤\n\n`;
-        txt += `★ Experiencia » *${user.exp}*\n`;
-        txt += `❖ Nivel » *${user.level}*\n`;
+        txt += `✦ Nivel » *${user.level}*\n`;
+        txt += `✰ Experiencia » *${user.exp}*\n`;
+        txt += `❖ Rango » *${user.role}*\n`;
         txt += `➨ Progreso » *${user.exp - min} => ${xp}* _(${Math.floor(((user.exp - min) / xp) * 100)}%)_\n`;
         txt += `# Puesto » *${rank}* de *${sortedLevel.length}*\n`;
         txt += `❒ Comandos totales » *${user.commands || 0}*`;
