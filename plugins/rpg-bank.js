e17337fb3c8c9a65d21f3f3e0c653f33b725e1c7
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-21 11:50:08 -0400

    Update rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -1,25 +1,29 @@
-// Bank Editado Por Cuervo
-//★彡[ᴄʀᴇᴀᴛᴇ ʙʏ ᴄᴜᴇʀᴠᴏ-ᴛᴇᴀᴍ-ꜱᴜᴘʀᴇᴍᴇ]彡★
-// Respeten credito xddddd (ratas inmundas)
-
-import fetch from 'node-fetch'
 import db from '../lib/database.js'
 
-let handler = async (m, {conn, usedPrefix}) => {
-   let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-   let img = 'https://files.catbox.moe/al4kc8.jpg'
-   if (who == conn.user.jid) return m.react('✖️')
-   if (!(who in global.db.data.users)) return m.reply(`${emoji4} El usuario no se encuentra en mi base de datos.`)
-   let user = global.db.data.users[who]
-   let name = conn.getName(who);
-   let txt = (`${who == m.sender ? `╭━〔  ⪛ ʙᴀɴᴄᴏ ᴄᴇɴᴛʀᴀʟ ⪜  〕⬣\n┋ 👤 *Cliente:* ${name}\n┋ 💸 *${moneda} En Cartera*: ${user.coin}\n┋ 🏦 *${moneda} En Banco*: ${user.bank}\n┋ ✨ *Experiencia:* ${user.exp}\n┋ 📅 *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣` : `╭━〔  ⪛ ʙᴀɴᴄᴏ ᴄᴇɴᴛʀᴀʟ ⪜  〕⬣\n┋ 👤 *Cliente:* @${who.split('@')[0]}\n┋ 💸 *${moneda} En Cartera*: ${user.coin}\n┋ 🏦 *${moneda} En Banco*: ${user.bank}\n┋ *✨ Experiencia:* ${user.exp}\n┋ 📅 *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣`}`)
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, fkontak, null, {mentions: [who] })
+let handler = async (m, { conn, usedPrefix }) => {
+    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
+    if (who == conn.user.jid) return m.react('✖️')
+    if (!(who in global.db.data.users)) return m.reply(`${emoji} El usuario no se encuentra en mi base de datos*`)
+  
+    let user = global.db.data.users[who]
+    let total = (user.coin || 0) + (user.bank || 0);
+
+    const texto = `✿ 》》Informacion - Economia《《 ✿
+ 
+ᰔᩚ Usuario » *${conn.getName(who)}*   
+⛀ Dinero » *${user.coin} ${moneda}*
+⚿ Banco » *${user.bank} ${moneda}*
+⛁ Total » *${total} ${moneda}*
+
+> _Para proteger tu dinero, ¡depósitalo en el banco usando #deposit!_`;
+
+    await conn.reply(m.chat, texto, m)
 }
 
-handler.help = ['bank']
-handler.tags = ['economy']
-handler.command = ['bank', 'banco'] 
+handler.help = ['bal']
+handler.tags = ['rpg']
+handler.command = ['bal', 'balance'] 
 handler.register = true 
-handler.group = true
+handler.group = true 
 
 export default handler
