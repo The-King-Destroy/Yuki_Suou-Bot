bf6c9a86184f5c78ad62f5308a6d10fbd7e5a30a
OfcKing <argumedodie@gmail.com>
2025-02-21 09:53:47 -0500

    Fix?

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -60,13 +60,16 @@ yukiJBOptions.command = command
 yukiJadiBot(yukiJBOptions)
 global.db.data.users[m.sender].Subs = new Date * 1
 } 
-handler.help = ['serbot', 'serbot code']
+handler.help = ['qr', 'code']
 handler.tags = ['serbot']
-handler.command = ['jadibot', 'serbot']
+handler.command = ['qr', 'code']
 export default handler 
 
 export async function yukiJadiBot(options) {
 let { pathYukiJadiBot, m, conn, args, usedPrefix, command } = options
+if (command === 'code') {
+command = 'qr'; 
+args.unshift('code')}
 const mcode = args[0] && /(--code|code)/.test(args[0].trim()) ? true : args[1] && /(--code|code)/.test(args[1].trim()) ? true : false
 let txtCode, codeBot, txtQR
 if (mcode) {
