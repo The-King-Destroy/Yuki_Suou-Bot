579ee8a0b07123fc8baf726e4401ed67b3488c1c
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-23 00:07:39 -0400

    Hola me llamo Juan Carlos Gutiérrez

--- plugins/descargas-mediafire.js
+++ plugins/descargas-mediafire.js
@@ -1,54 +1,19 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) {
-    return conn.reply(m.chat, `${emoji} por favor, ingresa un enlace de *Mediafire*.`, m);
-  }
-  
-  await m.react('🕓');
-  
-  let url = args[0];
-  if (!url.includes('mediafire.com')) {
-    return conn.reply(m.chat, `${emoji2} El enlace proporcionado no parece ser de MediaFire.`, m);
-  }
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-  try {
-    const apiUrl = `https://api.siputzx.my.id/api/d/mediafire?url=${encodeURIComponent(url)}`;
-    const response = await axios.get(apiUrl);
-    
-    if (!response.data.status || !response.data.data) {
-      throw new Error('No se pudo obtener la información del archivo.');
-    }
+if (!text) throw m.reply(`${emoji} Por favor, ingresa un link de mediafire.`);
+conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
+        let ouh = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`)
+  let gyh = await ouh.json() 
+        await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `乂  *¡MEDIAFIRE - DESCARGAS!*  乂\n\n✩ *Nombre* : ${gyh.data[0].nama}\n✩ *Peso* : ${gyh.data[0].size}\n✩ *MimeType* : ${gyh.data[0].mime}\n> ↻ El archivo se esta enviando espera un momento,..`, m)       
+        await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
+}
+handler.help = ['mediafire']
+handler.tags = ['descargas']
+handler.command = ['mf', 'mediafire']
+handler.coin = 10
+handler.register = true
+handler.group = true
 
-    const { fileName, downloadLink, fileSize, meta } = response.data.data;
-    
-    let text = '`乂  M E D I A F I R E - D O W N L O A D`\n\n';
-    text += `📄 *Título* » ${fileName}\n`;
-    text += `🗂️ *Tamaño* » ${fileSize}\n`;
-    text += `🔗 *Enlace* » ${downloadLink}\n`;
-
-    await conn.reply(m.chat, text, m);
-
-    const fileBuffer = (await axios.get(downloadLink, { responseType: 'arraybuffer' })).data;
-    await conn.sendMessage(
-      m.chat,
-      { document: fileBuffer, fileName: fileName, mimetype: 'application/octet-stream' },
-      { quoted: m }
-    );
-    
-    await m.react('✅');
-  } catch (error) {
-    console.error(error);
-    await m.react('❌');
-  }
-};
-
-handler.help = ['mediafire *<url>*'];
-handler.tags = ['dl'];
-handler.command = ['mediafire'];
-handler.group = true;
-handler.register = true;
-handler.coin = 10;
-
-export default handler;
+export default handler
