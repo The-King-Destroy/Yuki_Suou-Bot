ccbd52ea4e8e2c80c554c90ac7776ecbc0ebe169
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-02-24 15:10:15 -0400

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,4 +1,7 @@
 import { addExif } from '../lib/sticker.js';
+import { sticker } from '../lib/sticker.js';
+import fetch from 'node-fetch';
+import axios from 'axios';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!m.quoted) return m.reply(`${emoji} Por favor, responde a un sticker con el comando *${usedPrefix + command}* seguido del nuevo nombre.\nEjemplo: *${usedPrefix + command} Nuevo Nombre*`);
@@ -7,8 +10,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!sticker) return m.reply(`${emoji2} No se pudo descargar el sticker.`);
 
   const textoParts = text.split(/[\u2022|]/).map(part => part.trim());
-  const texto1 = textoParts[0] || `✦ ${botname}`;
-  const texto2 = textoParts[1] || `${nombre}`
+  const userId = m.sender;
+  let packstickers = global.db.data.users[userId] || {};
+  let texto1 = textoParts[0] || packstickers.text1 || global.packsticker;
+  let texto2 = textoParts[1] || packstickers.text2 || global.packsticker2;
 
   const exif = await addExif(sticker, texto1, texto2);
 
