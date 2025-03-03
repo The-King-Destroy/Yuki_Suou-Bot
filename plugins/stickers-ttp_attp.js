d0372a2dbfe05f0a1b01d2d3fe62feb86c5072aa
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-03-03 11:35:32 -0400

    Update stickers-ttp_attp.js

--- plugins/stickers-ttp_attp.js
+++ plugins/stickers-ttp_attp.js
@@ -1,22 +1,29 @@
 import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
-import axios from 'axios';
+import axios from 'axios'
 
-let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-    if (!text) return m.reply(`${emoji} Por Favor, Ingresa Un Texto Para Realizar Tu Sticker\n> *Ejemplo:* ${usedPrefix + command} Yuki_Suou`)
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+    if (!text) return m.reply(`❀ Por Favor, Ingresa Un Texto Para Realizar Tu Sticker.`)
+
     let teks = encodeURI(text)
+    let userId = m.sender
+    let packstickers = global.db.data.users[userId] || {}
+    let texto1 = packstickers.text1 || global.packsticker
+    let texto2 = packstickers.text2 || global.packsticker2
+
     if (command == 'attp') {
-let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/attp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
-conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: channel, thumbnail: imagen1 }}}, { quoted: m })}
+        let stiker = await sticker(null, `https://api.fgmods.xyz/api/maker/attp?text=${text}&apikey=elrebelde21`, texto1, texto2)
+        conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true)
+    }
 
     if (command == 'ttp') {
-let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/ttp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
-conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: imagen1 }}}, { quoted: m })
-}
+        let stiker = await sticker(null, `https://api.fgmods.xyz/api/maker/ttp?text=${text}&apikey=elrebelde21`, texto1, texto2)
+        conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true)
+    }
 }
 
 handler.tags = ['sticker']
 handler.help = ['ttp', 'attp']
 handler.command = ['ttp', 'attp']
 
-export default handler;
+export default handler
