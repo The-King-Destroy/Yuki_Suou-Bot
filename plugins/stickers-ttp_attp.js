d56f0ff4517de081ac31643b2c2f02bcf6ecf6ec
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-02 23:54:07 -0400

    🔥🔥

--- plugins/stickers-ttp_attp.js
+++ plugins/stickers-ttp_attp.js
@@ -3,33 +3,16 @@ import fetch from 'node-fetch'
 import axios from 'axios';
 
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-    if (!text) return m.reply(`${emoji} Por favor, ingresa un texto para crear un sticker.`);
-    let teks = encodeURI(text);
-    let userId = m.sender;
-    let packstickers = global.db.data.users[userId] || {};
-    let texto1 = packstickers.text1 || global.packsticker;
-    let texto2 = packstickers.text2 || global.packsticker2;
-
+    if (!text) return m.reply(`🍭 Ingresa Un Texto Para Realizar Tu Sticker\n> *Ejemplo:* ${usedPrefix + command} CrowBot`)
+    let teks = encodeURI(text)
     if (command == 'attp') {
-        const data = {
-            text: `${text}`
-        };
-        const response = await axios.post('https://salism3api.pythonanywhere.com/text2gif', data);
-        const x = response.data.image;
-        let stiker = await sticker(null, x, texto1, texto2);
-        conn.sendFile(m.chat, stiker, null, { asSticker: true });
-    }
+let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/attp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: channel, thumbnail: imagen1 }}}, { quoted: m })}
+
     if (command == 'ttp') {
-        const data = {
-            text: `${text}`,
-            "outlineColor": "255,0,0,255",
-            "textColor": "0,0,0,255"
-        };
-        const response = await axios.post('https://salism3api.pythonanywhere.com/text2img', data);
-        const x = response.data.image;
-        let stiker = await sticker(null, x, texto1, texto2);
-        conn.sendFile(m.chat, stiker, null, { asSticker: true });
-    }
+let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/ttp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: imagen1 }}}, { quoted: m })
+}
 }
 
 handler.tags = ['sticker']
