29b78bcd7a4043141e0f4c780680459d734948c2
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-03-31 21:49:04 -0400

    Update grupo-infogrupo.js

--- plugins/grupo-infogrupo.js
+++ plugins/grupo-infogrupo.js
@@ -1,9 +1,10 @@
 const handler = async (m, {conn, participants, groupMetadata}) => {
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || `${icono}`;
-  const {antiLink, detect, welcome, modoadmin, antiPrivate, autoRechazar, nsfw, autoAceptar, restrict, antiSpam, reaction, antiviewonce, antiTraba, antiToxic} = global.db.data.chats[m.chat];
-  const groupAdmins = participants.filter((p) => p.admin);
-  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
-  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => global.icono);
+  const {antiLink, detect, welcome, modoadmin, autoRechazar, nsfw, autoAceptar, reaction, primaryBot, isBanned} = global.db.data.chats[m.chat]
+  const groupAdmins = participants.filter((p) => p.admin)
+  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
+  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+  const primary = primaryBot ? `@${primaryBot.split('@')[0]}` : 'Aleatorio',
   const text = `*✧･ﾟ INFO GRUPO ﾟ･✧*
 ❀ *ID:*
 → ${groupMetadata.id}
@@ -15,27 +16,23 @@ const handler = async (m, {conn, participants, groupMetadata}) => {
 → ${participants.length} Participantes
 ✰ *Creador del Grupo:*
 → @${owner.split('@')[0]}
+❒ *Bot primario:*
+→ ${primary}
 ✥ *Administradores:*
 ${listAdmin}
 
 ˚₊· ͟͟͞͞➳❥ *CONFIGURACIÓN*
 
-◈ *Welcome:* ${welcome ? '✅' : ''}
-◈ *Detect:* ${detect ? '✅' : '❌'}  
-◈ *Antilink:* ${antiLink ? '✅' : '❌'} 
-◈ *Autoaceptar:* ${autoAceptar ? '✅' : '❌'} 
-◈ *Autorechazar:* ${autoRechazar ? '✅' : '❌'} 
-◈ *Nfsw:* ${nsfw ? '✅' : '❌'} 
-◈ *Antiprivado:* ${antiPrivate ? '✅' : '❌'} 
-◈ *Modoadmin:* ${modoadmin ? '✅' : '❌'} 
-◈ *Antiver:* ${antiviewonce ? '✅' : '❌'} 
-◈ *Reacción* ${reaction ? "✅️" : "❌️"}
-◈ *Antispam:* ${antiSpam ? '✅' : '❌'} 
-◈ *Restrict:* ${restrict ? '✅' : '❌'} 
-◈ *Antitoxic:* ${antiToxic ? '✅' : '❌'} 
-◈ *Antitraba:* ${antiTraba ? '✅' : '❌'} 
-`.trim();
-  conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
+◈ *${botname}* » ${isBanned ? 'Desactivado' : 'Activado'}
+◈ *Welcome:* ${welcome ? 'Activado' : 'Desactivado'}
+◈ *Detect:* ${detect ? 'Activado' : 'Desactivado'}  
+◈ *Antilink:* ${antiLink ? 'Activado' : 'Desactivado'} 
+◈ *Autoaceptar:* ${autoAceptar ? 'Activado' : 'Desactivado'}
+◈ *Autorechazar:* ${autoRechazar ? 'Activado' : 'Desactivado'}
+◈ *NSFW:* ${nsfw ? 'Activado' : 'Desactivado'}
+◈ *Modoadmin:* ${modoadmin ? 'Activado' : 'Desactivado'}
+◈ *Reacción:* ${reaction ? 'Activado' : 'Desactivado'}`.trim();
+  conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner, primary]});
 };
 handler.help = ['infogrupo'];
 handler.tags = ['grupo'];
