abbc9c79b444714015ca484aef346c88233d26ff
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-04-01 06:33:14 -0400

    Update _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -16,7 +16,13 @@ await conn.groupRequestParticipantsUpdate(m.chat, [participant.jid], "approve")
 } if (m.messageStubType === 172 && m.messageStubParameters?.[0]?.includes('@s.whatsapp.net')) {
 const jid = m.messageStubParameters[0]
 if (jid.split('@')[0].startsWith('5')) {
-await conn.groupRequestParticipantsUpdate(m.chat, [jid], "approve")}}}
+await conn.groupRequestParticipantsUpdate(m.chat, [jid], "approve")}}
+
+} if (isBotAdmin && chat.antifake) {
+const antiFakePrefixes = ['6', '90', '212', '92', '93', '94', '7', '49', '2', '91', '48']
+if (antiFakePrefixes.some(prefix => m.sender.startsWith(prefix))) {
+global.db.data.users[m.sender].block = true
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}}
 
 }
 export default handler
