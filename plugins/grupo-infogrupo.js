5e5abbc8e1688584768d59132b3e82c7d46e8ae6
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-04-01 07:56:55 -0400

    Update grupo-infogrupo.js

--- plugins/grupo-infogrupo.js
+++ plugins/grupo-infogrupo.js
@@ -1,18 +1,14 @@
 const handler = async (m, {conn, participants, groupMetadata}) => {
   const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => global.icono);
-  const { antiLink, detect, welcome, modoadmin, autoRechazar, nsfw, autoAceptar, reaction, isBanned } = global.db.data.chats[m.chat]
+  const { antiLink, detect, welcome, modoadmin, autoRechazar, nsfw, autoAceptar, reaction, isBanned, antifake } = global.db.data.chats[m.chat]
   const groupAdmins = participants.filter((p) => p.admin)
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
   const text = `*✧･ﾟ INFO GRUPO ﾟ･✧*
-❀ *ID:*
-→ ${groupMetadata.id}
-⚘ *Nombre:*
-→ ${groupMetadata.subject}
-❖ *Miembros:*
-→ ${participants.length} Participantes
-✰ *Creador del Grupo:*
-→ @${owner.split('@')[0]}
+❀ *ID:* ${groupMetadata.id}
+⚘ *Nombre:* ${groupMetadata.subject}
+❖ *Miembros:* ${participants.length} Participantes
+✰ *Creador:* @${owner.split('@')[0]}
 ✥ *Administradores:*
 ${listAdmin}
 
@@ -27,9 +23,10 @@ ${listAdmin}
 ◈ *NSFW:* ${nsfw ? 'Activado' : 'Desactivado'}
 ◈ *Modoadmin:* ${modoadmin ? 'Activado' : 'Desactivado'}
 ◈ *Reacción:* ${reaction ? 'Activado' : 'Desactivado'}
+◈ *Antifake:* ${antifake ? 'Activado' : 'Desactivado'}
 
 ✦ *Descripción:*
-→ ${groupMetadata.desc?.toString() || 'Sin Descripción'}`.trim();
+${groupMetadata.desc?.toString() || 'Sin Descripción'}`.trim();
   conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
 };
 handler.help = ['infogrupo'];
