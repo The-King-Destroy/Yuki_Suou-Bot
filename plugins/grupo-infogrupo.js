5544b230f379af25cc4e0db091f988606daecd88
Alex-X <argumedodie@gmail.com>
2025-03-31 21:34:30 -0500

    Update zzz function

--- plugins/grupo-infogrupo.js
+++ plugins/grupo-infogrupo.js
@@ -1,10 +1,9 @@
 const handler = async (m, {conn, participants, groupMetadata}) => {
   const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => global.icono);
-  const {antiLink, detect, welcome, modoadmin, autoRechazar, nsfw, autoAceptar, reaction, isBanned, primaryBot} = global.db.data.chats[m.chat]
+  const { antiLink, detect, welcome, modoadmin, autoRechazar, nsfw, autoAceptar, reaction, isBanned } = global.db.data.chats[m.chat]
   const groupAdmins = participants.filter((p) => p.admin)
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-  const primary = primaryBot ? `@${primaryBot.split('@')[0]}` : 'Aleatorio'
   const text = `*✧･ﾟ INFO GRUPO ﾟ･✧*
 ❀ *ID:*
 → ${groupMetadata.id}
@@ -14,8 +13,6 @@ const handler = async (m, {conn, participants, groupMetadata}) => {
 → ${participants.length} Participantes
 ✰ *Creador del Grupo:*
 → @${owner.split('@')[0]}
-❒ *Bot primario:*
-→ ${primary}
 ✥ *Administradores:*
 ${listAdmin}
 
@@ -33,7 +30,7 @@ ${listAdmin}
 
 ✦ *Descripción:*
 → ${groupMetadata.desc?.toString() || 'Sin Descripción'}`.trim();
-  conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner, primary]});
+  conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
 };
 handler.help = ['infogrupo'];
 handler.tags = ['grupo'];
