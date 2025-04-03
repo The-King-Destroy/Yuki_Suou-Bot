cc369a006420811162f72f3cbbdb71975bf4c577
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-04-03 10:03:42 -0400

    Update descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -1,92 +1,31 @@
-import fetch from "node-fetch";
-import yts from 'yt-search';
-import axios from "axios";
-
-const formatAudio = ['mp3', 'm4a', 'webm', 'acc', 'flac', 'opus', 'ogg', 'wav'];
-const formatVideo = ['360', '480', '720', '1080', '1440', '4k'];
-
-const ddownr = {
-  download: async (url, format) => {
-    if (!formatAudio.includes(format) && !formatVideo.includes(format)) {
-      throw new Error('Formato no soportado, verifica la lista de formatos disponibles.');
-    }
-
-    const config = {
-      method: 'GET',
-      url: `https://p.oceansaver.in/ajax/download.php?format=${format}&url=${encodeURIComponent(url)}&api=dfcb6d76f2f6a9894gjkege8a4ab232222`,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      }
-    };
-
-    try {
-      const response = await axios.request(config);
-
-      if (response.data && response.data.success) {
-        const { id, title, info } = response.data;
-        const { image } = info;
-        const downloadUrl = await ddownr.cekProgress(id);
-
-        return {
-          id: id,
-          image: image,
-          title: title,
-          downloadUrl: downloadUrl
-        };
-      } else {
-        throw new Error('Fallo al obtener los detalles del video.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
-  },
-  cekProgress: async (id) => {
-    const config = {
-      method: 'GET',
-      url: `https://p.oceansaver.in/ajax/progress.php?id=${id}`,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      }
-    };
-
-    try {
-      while (true) {
-        const response = await axios.request(config);
-
-        if (response.data && response.data.success && response.data.progress === 1000) {
-          return response.data.download_url;
-        }
-        await new Promise(resolve => setTimeout(resolve, 5000));
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
-  }
-};
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+import axios from 'axios'
+const MAX_SIZE_MB = 100
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
+
+  if (!text.trim()) {
+    return conn.reply(m.chat, `❀ Por favor, ingresa el nombre de la música a descargar.`, m)
+  }
   try {
-    if (!text.trim()) {
-      return conn.reply(m.chat, `❀ Por favor, ingresa el nombre de la música a descargar.`, m);
+    const search = await yts(text)
+    if (!search.all.length) {
+      return m.reply('✧ No se encontraron resultados para tu búsqueda.')
     }
 
-    const search = await yts(text);
-    if (!search.all || search.all.length === 0) {
-      return m.reply('No se encontraron resultados para tu búsqueda.');
-    }
+    const videoInfo = search.all[0]
+    const { title, thumbnail, timestamp, views, ago, url, author } = videoInfo
+    const vistas = formatViews(views)
+    const canal = author.name || 'Desconocido'
+    const infoMessage = `「✦」Descargando *<${title}>*\n\n> ✦ Canal » *${videoInfo.author.name || 'Desconocido'}*\n> ✰ Vistas » *${views}*\n> ⴵ Duración » *${timestamp}*\n> ✐ Publicación » *${ago}*\n> 🜸 Link » ${url}`
 
-    const videoInfo = search.all[0];
-    const { title, thumbnail, timestamp, views, ago, url } = videoInfo;
-    const vistas = formatViews(views);
-    const infoMessage = `「✦」Descargando *<${title}>*\n\n> ✦ Canal » *${videoInfo.author.name || 'Desconocido'}*\n> ✰ Vistas » *${views}*\n> ⴵ Duración » *${timestamp}*\n> ✐ Publicación » *${ago}*\n> 🜸 Link » ${url}\n`;
-    const thumb = (await conn.getFile(thumbnail))?.data;
+    const thumb = (await conn.getFile(thumbnail)).data
 
     const JT = {
       contextInfo: {
         externalAdReply: {
-          title: botname,
+          title: '✧ Youtube • Music ✧',
           body: dev,
           mediaType: 1,
           previewType: 0,
@@ -96,50 +35,68 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
           renderLargerThumbnail: true,
         },
       },
-    };
-
-    await conn.reply(m.chat, infoMessage, m, JT);
-
-    if (command === 'play' || command === 'yta' || command === 'ytmp3') {
-      const api = await (await fetch(`https://api.neoxr.eu/api/youtube?url=${url}&type=audio&quality=128kbps&apikey=GataDios`)).json()
-      const result = api.data.url
-      await conn.sendMessage(m.chat, { audio: { url: result }, mimetype: "audio/mpeg" }, { quoted: m });
-
-    } else if (command === 'play2' || command === 'ytv' || command === 'ytmp4') {
-      
-      const response = await fetch(`https://api.neoxr.eu/api/youtube?url=${url}&type=video&quality=480p&apikey=GataDios`)
-      const json = await response.json()
-
-      try {
-        await conn.sendMessage(m.chat, {
-          video: { url: json.data.url },
-          fileName: json.data.filename,
-          mimetype: 'video/mp4',
-          caption: '',
-          thumbnail: json.thumbnail
-        }, { quoted: m });
-      } catch (e) {
-        console.error(`Error con la fuente de descarga:`, e.message);
-      }
-
-    } else {
-      throw "Comando no reconocido.";
     }
+
+    await conn.reply(m.chat, infoMessage, m, JT)
+
+    let api, result, fileSizeMB
+    if (command === 'audio' || command === 'playaudio') {
+      api = await fetchAPI(url, 'audio')
+      result = api.download || api.data.url
+      fileSizeMB = await getFileSize(result)
+
+      if (fileSizeMB > MAX_SIZE_MB) {
+        await conn.sendMessage(m.chat, { document: { url: result }, fileName: `${api.title || api.data.filename}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
+      } else {
+        await conn.sendMessage(m.chat, { audio: { url: result }, fileName: `${api.title || api.data.filename}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m })
+      }
+    } else if (command === 'video' || command === 'playvideo') {
+      api = await fetchAPI(url, 'video')
+      result = api.download || api.data.url
+      fileSizeMB = await getFileSize(result)
+
+      if (fileSizeMB > MAX_SIZE_MB) {
+        await conn.sendMessage(m.chat, { document: { url: result }, fileName: `${api.title || api.data.filename}.mp4`, mimetype: 'video/mp4' }, { quoted: m })
+      } else {
+        await conn.sendMessage(m.chat, { video: { url: result }, fileName: api.title || api.data.filename, mimetype: 'video/mp4', caption: title, thumbnail: api.thumbnail || thumb }, { quoted: m })
+      }
+    } else {
+      throw new Error("✧ Comando no reconocido.")
+    }
+
   } catch (error) {
-    return m.reply(`⚠︎ Ocurrió un error: ${error.message}`);
+    return m.reply(`⚠︎ Ocurrió un error: ${error.message}`)
   }
-};
+}
 
-handler.command = handler.help = ['play', 'play2', 'ytmp3', 'yta', 'ytmp4', 'ytv'];
-handler.tags = ['downloader'];
-handler.group = true;
+const fetchAPI = async (url, type) => {
+    const fallbackEndpoints = {
+      audio: `https://api.neoxr.eu/api/youtube?url=${url}&type=audio&quality=128kbps&apikey=Paimon`,
+      video: `https://api.neoxr.eu/api/youtube?url=${url}&type=video&quality=720p&apikey=Paimon`,
+    }
+    const response = await fetch(fallbackEndpoints[type])
+    return await response.json()
+}
 
-export default handler;
+const getFileSize = async (url) => {
+  try {
+    const response = await axios.head(url)
+    const sizeInBytes = response.headers['content-length'] || 0
+    return parseFloat((sizeInBytes / (1024 * 1024)).toFixed(2))
+  } catch (error) {
+    return 0
+  }
+}
+handler.command = handler.help = ['playaudio', 'audio', 'playvideo', 'video']
+handler.tags = ['descargas']
+handler.group = true
+
+export default handler
 
 function formatViews(views) {
-  if (views >= 1000) {
-    return (views / 1000).toFixed(1) + 'k (' + views.toLocaleString() + ')';
-  } else {
-    return views.toString();
-  }
+  if (views === undefined) return "No disponible"
+  if (views >= 1_000_000_000) return `${(views / 1_000_000_000).toFixed(1)}B (${views.toLocaleString()})`
+  if (views >= 1_000_000) return `${(views / 1_000_000).toFixed(1)}M (${views.toLocaleString()})`
+  if (views >= 1_000) return `${(views / 1_000).toFixed(1)}k (${views.toLocaleString()})`
+  return views.toString()
 }
