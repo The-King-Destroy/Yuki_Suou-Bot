5a7ecc42daee55a75ec33deecdaf8cbf00462f82
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-04-15 17:23:13 -0700

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -30,8 +30,8 @@ console.log(`Error : ${e}`)
 global.creador = 'Wa.me/528211111111'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
 global.asistencia = 'Wa.me/528211111111'
-global.namechannel = '=͟͟͞❀ 𝐘𝐮𝐤𝐢'𝐬 𝐒𝐮𝐨𝐮 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞★'
-global.namechannel2 = '=͟͟͞❀ 𝐘𝐮𝐤𝐢'𝐬 𝐒𝐮𝐨𝐮 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞★'
+global.namechannel = "=͟͟͞❀ 𝐘𝐮𝐤𝐢'𝐬 𝐒𝐮𝐨𝐮 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞★"
+global.namechannel2 = "=͟͟͞❀ 𝐘𝐮𝐤𝐢'𝐬 𝐒𝐮𝐨𝐮 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞★"
 global.namegrupo = 'ᰔᩚ ᥡᥙkі sᥙ᥆ᥙ • ᥆𝖿іᥴіᥲᥣ ❀'
 global.namecomu = 'ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥴ᥆mᥙᥒі𝗍ᥡ ❀'
 global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
