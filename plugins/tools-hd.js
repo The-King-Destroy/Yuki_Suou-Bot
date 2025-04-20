6bace5b24d153a3063d15c3dadd8e85403fe672d
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-04-20 15:42:24 -0400

    Update tools-hd.js

--- plugins/tools-hd.js
+++ plugins/tools-hd.js
@@ -34,7 +34,6 @@ const handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ["hd"]
 handler.tags = ["tools"]
 handler.command = ["remini", "hd", "enhance"]
-handler.group = true
 
 export default handler
 
