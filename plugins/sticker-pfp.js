55606b376c0afcf6612cedc1e53fe8314cbfe30c
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-04-25 00:48:07 -0400

    Update sticker-pfp.js

--- plugins/sticker-pfp.js
+++ plugins/sticker-pfp.js
@@ -5,7 +5,7 @@
 let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     let name = conn.getName(who);
-    let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://files.catbox.moe/xr2m6u.jpg');
+    let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg');
     await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil de ${name}*`, m);
 }
 
