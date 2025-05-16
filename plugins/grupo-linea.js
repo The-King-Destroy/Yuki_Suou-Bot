09ae9b44da3981189411954d32d0ca4bcd6055cc
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-05-16 12:54:11 -0400

    Update grupo-linea.js

--- plugins/grupo-linea.js
+++ plugins/grupo-linea.js
@@ -1,35 +1,45 @@
+import axios from "axios"
+
 let handler = async (m, { conn, args }) => {
   try {
-    let id = args?.[0]?.match(/\d+\-\d+@g.us/) || m.chat;
+    let id = args?.[0]?.match(/\d+\-\d+@g.us/) || m.chat
 
     const participantesUnicos = Object.values(conn.chats[id]?.messages || {})
       .map((item) => item.key.participant)
-      .filter((value, index, self) => self.indexOf(value) === index);
+      .filter((value, index, self) => self.indexOf(value) === index)
+
+    const participantesOrdenados = participantesUnicos
+      .filter(participante => participante)
+      .sort((a, b) => {
+        if (a && b) {
+          return a.split("@")[0].localeCompare(b.split("@")[0])
+        }
+        return 0
+      })
 
     const listaEnLinea =
-      participantesUnicos
-        .map((k) => `@${k.split("@")[0]}`)
-        .join("\n") || "*✧ No hay usuarios en línea en este momento :c.*";
+      participantesOrdenados
+        .map((k) => `*●* @${k.split("@")[0]}`)
+        .join("\n") || "✧ No hay usuarios en línea en este momento."
 
-    const mensaje = `*♡ Lista de usuarios en línea:*\n\n${listaEnLinea}\n\n> ${dev}`;
+    await conn.sendMessage(
+      m.chat,
+      {
+        text: `*❀ Lista de usuarios en línea:*\n\n${listaEnLinea}\n\n> ${dev}`,
+        contextInfo: { mentionedJid: participantesOrdenados },
+      },
+      { quoted: m }
+    )
 
-    await conn.sendMessage(m.chat, {
-      text: mensaje,
-      mentions: participantesUnicos,
-    });
-
-    await m.react("✅");
+    await m.react("✅")
   } catch (error) {
-    console.error(error);
-    await m.reply(`${msm} Hubo un error al enviar la lista de usuarios.`);
+    await m.reply(`⚠︎ Ocurrió un error: ${error.message}`)
   }
-};
+}
 
-handler.help = ["listonline"];
-handler.tags = ["grupo"];
-handler.command = ["listonline", "online", "linea", "enlinea"];
-handler.group = true;
-handler.fail = null;
-handler.register = true;
+handler.help = ["listonline"]
+handler.tags = ["owner"]
+handler.command = ["listonline", "online", "linea", "enlinea"]
+handler.group = true
 
-export default handler;
+export default handler
