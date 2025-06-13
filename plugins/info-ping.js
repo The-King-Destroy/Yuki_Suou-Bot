edde817500ad059ed33327b794b06cddb10d7acd
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-06-13 12:00:16 -0400

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -2,18 +2,19 @@ import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
-         let timestamp = speed();
-         let latensi = speed() - timestamp;
-         exec(`neofetch --stdout`, (error, stdout, stderr) => {
-          let child = stdout.toString("utf-8");
-          let ssd = child.replace(/Memory:/, "Ram:");
+let timestamp = speed()
+let sentMsg = await conn.reply(m.chat, '❀ Calculando ping...', m)
+let latency = speed() - timestamp
+exec(`neofetch --stdout`, (error, stdout, stderr) => {
+let child = stdout.toString("utf-8");
+let ssd = child.replace(/Memory:/, "Ram:")
 
-          conn.reply(m.chat, `✰ *¡Pong!*\n> Tiempo ⴵ ${latensi.toFixed(4)}ms`, m);
-            });
+let result = `✰ *¡Pong!*\n> Tiempo ⴵ ${latency.toFixed(4).split(".")[0]}ms\n${ssd}`
+conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
+})
 }
 handler.help = ['ping']
 handler.tags = ['info']
 handler.command = ['ping', 'p']
-handler.register = true
 
 export default handler
