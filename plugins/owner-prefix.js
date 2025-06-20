5c4a59ecdf7c303c2efd98e26748ffce6b219f69
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-06-20 00:01:38 -0700

    Actualizar owner-prefix.js

--- plugins/owner-prefix.js
+++ plugins/owner-prefix.js
@@ -1,13 +1,13 @@
-let handler = async function (m, { conn, text, usedPrefix, command }) {
+async function handler(m, { conn, text, usedPrefix, command }) {
   if (!text) throw `❌ No se encontró ningún prefijo. Por favor escribe un prefijo.\n> *Ejemplo: ${usedPrefix + command} !*`;
 
-  global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+  global.prefix = new RegExp('^[' + text.replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
 
   await conn.sendMessage(m.chat, {
     text: `✅ Prefijo actualizado a: *${text}*`,
     mentions: [m.sender]
   }, { quoted: m });
-};
+}
 
 handler.help = ['prefix [nuevo prefijo]'];
 handler.tags = ['owner'];
