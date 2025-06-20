a723f1458bd914108e123190bc76c30c6dd620da
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-06-20 13:25:53 -0700

    Actualizar anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -8,7 +8,7 @@ let res = await fetch('https://api.waifu.pics/sfw/waifu')
 if (!res.ok) return
 let json = await res.json()
 if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', `${emoji} Aqui tienes tu *Waifu* ฅ^•ﻌ•^ฅ.`, fkontak)
+await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', `${emoji} Aqui tienes tu *Waifu* ฅ^•ﻌ•^ฅ.`, m)
 } catch {
 }}
 handler.help = ['waifu']
