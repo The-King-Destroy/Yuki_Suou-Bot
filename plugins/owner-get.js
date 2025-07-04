f0fc99f5def1233e4d61896a02a89949d5eb71a0
ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜 <moisesmusic04@gmail.com>
2025-07-03 22:35:03 -0400

    Update owner-get.js

--- plugins/owner-get.js
+++ plugins/owner-get.js
@@ -1,27 +1,29 @@
-import fetch from 'node-fetch';
-import {format} from 'util';
-const handler = async (m, {text}) => {
-    if (!/^https?:\/\//.test(text)) return m.reply(`${emoji} Te faltó el *url* de la pagina.`);
-  const _url = new URL(text);
-  const url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY');
-  const res = await fetch(url);
-  if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
-    // delete res
-    throw `Content-Length: ${res.headers.get('content-length')}`;
-  }
-  if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m);
-  let txt = await res.buffer();
-  try {
-    txt = format(JSON.parse(txt + ''));
-  } catch (e) {
-    txt = txt + '';
-  } finally {
-    m.reply(txt.slice(0, 65536) + '');
-  }
-};
-handler.help = ['fetch', 'get'];
-handler.tags = ['owner'];
-handler.command = ['fetch', 'get'];
-handler.rowner = true;
+import fetch from 'node-fetch'
+import { format } from 'util'
 
-export default handler;
+let handler = async (m, { conn, text }) => {
+if (m.fromMe) return
+if (!/^https?:\/\//.test(text)) return m.reply(`❀ Por favor, ingresa la *url* de la pagina.`)
+let url = text
+await m.react('🕒')
+let res = await fetch(url)
+if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+throw `Content-Length: ${res.headers.get('content-length')}`
+}
+if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
+let txt = await res.buffer()
+try {
+txt = format(JSON.parse(txt + ''))
+} catch (e) {
+txt = txt + ''
+} finally {
+m.reply(txt.slice(0, 65536) + '')
+await m.react('✔️')
+}}
+
+handler.help = ['get']
+handler.tags = ['tools']
+handler.command = ['fetch', 'get']
+handler.rowner = true
+
+export default handler
