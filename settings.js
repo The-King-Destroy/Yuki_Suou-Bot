c08347c1cc2a6b13f202fd2fa4d176c1ed58abc3
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-07-25 17:03:49 -0700

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -78,7 +78,7 @@ global.correo = 'thekingdestroy507@gmail.com'
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363416409380841@newsletter',
+ch1: '120363401404146384@newsletter',
 }
 global.multiplier = 60
 
