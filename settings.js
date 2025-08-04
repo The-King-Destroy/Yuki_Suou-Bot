aa6b3454a93b8f36bbd2d576e14fe58942702c1d
Ell Contrerax <argumedodie@gmail.com>
2025-08-04 08:18:17 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -79,6 +79,8 @@ global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
 ch1: '120363401404146384@newsletter',
+ch2: "120363420992828502@newsletter",
+ch3: "120363419171116775@newsletter"
 }
 global.multiplier = 60
 
